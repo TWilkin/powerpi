ae02a64b2a74b28de1fdc4d5622793e3b38a53e3
TWilkin <tom@xzi-xzone.com>
2019-11-24 00:29:01 +0000

    Added handling of expected device state change messages

--- power-starter/src/main.py
+++ power-starter/src/main.py
@@ -1,14 +1,41 @@
+import json
 import paho.mqtt.client as mqtt
 import os
+
+# the MQTT topic we're reading/writing to
+topic = 'home'
+
+# change the power state of a device
+def power(client, device, state):
+    print('Turning device {:s} {:s}'.format(device, state))
+
+    # now publish that the state was changed
+    message = {
+        'type': 'update',
+        'device': device,
+        'state': state
+    }
+    client.publish(topic, json.dumps(message))
 
 # MQTT connect callback
 def on_connect(client, user_data, flags, result_code):
     print('MQTT Connect {:d}'.format(result_code))
-    client.subscribe('home')
+    client.subscribe(topic)
 
 # MQTT message callback
 def on_message(client, user_data, message):
-    print('{:s} {:s}'.format(message.topic, str(message.payload)))
+    # read the JSON
+    event = json.loads(message.payload)
+    print(event)
+    
+    # check if we should respond to this message
+    if event['type'] == 'power':
+        if event['state'] != 'on' and event['state'] != 'off':
+            print('Unrecognisable state {:s}'.format(event['state']))
+            return
+        
+        # attempt to power the device on/off
+        power(client, event['device'], event['state'])
 
 # initialise and connect to MQTT
 client = mqtt.Client()
