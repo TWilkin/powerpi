07271bb76fff426b622137066771bf3057a1e4c1
TWilkin <4322355+TWilkin@users.noreply.github.com>
2021-02-06 21:15:14 +0000

    Fix for broken on/off commands

--- power-starter/power_starter/devices/energenie/ener314.py
+++ power-starter/power_starter/devices/energenie/ener314.py
@@ -8,17 +8,16 @@ from .. devices import Device, DeviceManager
 @Device(device_type='socket')
 class SocketDevice(object):
 
-    def __init__(self, home_id, device_id=0, retries=4, delay=0.5):
-        self.__home_id = int(home_id)
+    def __init__(self, home_id=0, device_id=0, retries=4, delay=0.5):
         self.__device_id = int(device_id)
         self.__retries = retries
         self.__delay = delay
 
     def turn_on(self):
-        self.__run(switch_on, self.__home_id)
+        self.__run(switch_on, self.__device_id)
 
     def turn_off(self):
-        self.__run(switch_off, self.__home_id)
+        self.__run(switch_off, self.__device_id)
 
     def __run(self, func, *params):
         for i in range(0, self.__retries):
@@ -29,8 +28,7 @@ class SocketDevice(object):
 @Device(device_type='socket_group')
 class SocketGroupDevice(object):
 
-    def __init__(self, home_id, devices, retries=4, delay=0.5):
-        self.__home_id = int(home_id)
+    def __init__(self, devices, home_id=None, retries=4, delay=0.5):
         self.__retries = retries
         self.__delay = delay
 
@@ -40,10 +38,10 @@ class SocketGroupDevice(object):
             self.__devices.append(d)
 
     def turn_on(self):
-        self.__run(switch_on, self.__home_id)
+        self.__run(switch_on, 'on')
 
     def turn_off(self):
-        self.__run(switch_off, self.__home_id)
+        self.__run(switch_off, 'off')
 
     def __run(self, func, status, *params):
         for i in range(0, self.__retries):
