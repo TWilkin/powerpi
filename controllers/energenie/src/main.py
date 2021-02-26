ef877732266d45fd5b09e61bc8e5babab725ca3e
TWilkin <4322355+TWilkin@users.noreply.github.com>
2021-02-26 19:17:37 +0000

    Add common python library with config

--- controllers/energenie/src/main.py
+++ controllers/energenie/src/main.py
@@ -1,5 +1,20 @@
+import logging
+
+from common.config import Config
+
 def main():
-    print('test')
+    handler = logging.StreamHandler()
+    handler.setLevel(logging.INFO)
+    handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s', datefmt='%Y-%m-d %H:%M:%S'))
+
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+    logger.addHandler(handler)
+
+    logger.info('PowerPi Energenie Controller')
+    
+    config = Config.instance()
+    logger.info('Using Energenie module {module}'.format(module = config.energenie_device))
 
 if __name__ == '__main__':
     main()
