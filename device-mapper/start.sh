584e6e5fc0b5e0d8e7f900e983b921f952fb4ef3
TWilkin <4322355+TWilkin@users.noreply.github.com>
2021-03-05 20:33:05 +0000

    Switch device-mapper to host Energenie controller

--- device-mapper/start.sh
+++ device-mapper/start.sh
@@ -2,10 +2,9 @@
 
 # export the config to variables
 DEVICES=`tr "\n" " " < $DEVICES_FILE | sed s/\"/\\\\\"/g`
-EVENTS=`tr "\n" " " < $EVENTS_FILE | sed s/\"/\\\\\"/g`
 
 # generate a name for the container
-NAME=powerpi_power-starter.1.`hostname`
+NAME=powerpi_$CONTROLLER_NAME.1.`hostname`
 
 # function to ensure the spawned container is stopped
 function stop() {
@@ -15,8 +14,8 @@ function stop() {
 }
 
 # ensure we have the latest version of the image
-echo "Updating power-starter image"
-docker pull $POWER_STARTER_IMAGE
+echo "Updating image"
+docker pull $IMAGE
 
 # trap so if this container is stopped, it will stop the spawned container
 echo "Setting exit trap condition"
@@ -24,7 +23,7 @@ trap 'kill ${!}; stop' SIGINT
 trap 'kill ${!}; stop' SIGTERM
 
 # start the container running on the node hosting this
-echo "Starting power-starter"
+echo "Starting $CONTROLLER_NAME"
 docker run \
     --privileged \
     --name $NAME \
@@ -34,8 +33,6 @@ docker run \
     --env "ENERGENIE_DEVICE=$ENERGENIE_DEVICE" \
     --env "MQTT_ADDRESS=$MQTT_ADDRESS" \
     --env "TOPIC_BASE=$TOPIC_BASE" \
-    --env "POLL_FREQUENCY=$POLL_FREQUENCY" \
     --env "DEVICES=$DEVICES" \
-    --env "EVENTS=$EVENTS" \
-    $POWER_STARTER_IMAGE \
+    $IMAGE \
     & wait ${!}
