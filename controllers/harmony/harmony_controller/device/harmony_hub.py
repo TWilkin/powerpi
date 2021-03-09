df372b6d3fd327487b2bafeffa36ee5b3d44d537
TWilkin <4322355+TWilkin@users.noreply.github.com>
2021-03-09 21:33:50 +0000

    Add start_activity to HarmonyHubDevice

--- controllers/harmony/harmony_controller/device/harmony_hub.py
+++ controllers/harmony/harmony_controller/device/harmony_hub.py
@@ -36,9 +36,41 @@ class HarmonyHubDevice(Device):
             if client:
                 client.power_off()
 
+    def start_activity(self, name: str):
+        activities = self.__activities()
+
+        if name in activities:
+            with self.__client as client:
+                if client:
+                    client.start_activity(activities[name])
+        else:
+            self._logger.error(
+                'Activity "{}" for {} not found'.format(name, self)
+            )
+
     @cached(cache=TTLCache(maxsize=1, ttl=10 * 60))
-    def config(self):
+    def __config(self):
+        self._logger.info(
+            'Loading config for {}'.format(self)
+        )
+
         with self.__client as client:
             if client:
                 with self.__cache_lock:
                     return client.get_config()
+
+    @cached(cache=TTLCache(maxsize=1, ttl=10 * 60))
+    def __activities(self):
+        activities = {}
+
+        for activity in self.__config()['activity']:
+            activities[activity['label']] = int(activity['id'])
+
+        self._logger.info(
+            'Found {} activities for {}'.format(len(activities), self)
+        )
+
+        return activities
+
+    def __str__(self):
+        return 'HarmonyHubDevice({})'.format(self.name)
