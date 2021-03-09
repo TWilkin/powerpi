259f5487bb2e634c39ef2d0096f47cac038841b3
TWilkin <4322355+TWilkin@users.noreply.github.com>
2021-03-09 18:32:29 +0000

    Use DeviceTestBase in Energenie controller

--- controllers/energenie/tests/device/test_socket.py
+++ controllers/energenie/tests/device/test_socket.py
@@ -2,6 +2,7 @@ from datetime import datetime
 
 from pytest_mock import MockerFixture
 
+from powerpi_common_test.device import DeviceTestBase
 from energenie_controller.device.socket import SocketDevice
 
 
@@ -16,7 +17,7 @@ class SocketDeviceImpl(SocketDevice):
         )
 
 
-class TestSocketDevice(object):
+class TestSocketDevice(DeviceTestBase):
     def get_subject(self, mocker: MockerFixture):
         return SocketDeviceImpl(mocker)
 
@@ -33,73 +34,3 @@ class TestSocketDevice(object):
         subject._run(func, 'blah', 1, 2)
 
         assert self.counter == 4
-
-    def test_turn_on(self, mocker: MockerFixture):
-        subject = self.get_subject(mocker)
-
-        assert subject.state == 'unknown'
-        subject.turn_on()
-        assert subject.state == 'on'
-
-    def test_turn_off(self, mocker: MockerFixture):
-        subject = self.get_subject(mocker)
-
-        assert subject.state == 'unknown'
-        subject.turn_off()
-        assert subject.state == 'off'
-
-    def test_change_message(self, mocker: MockerFixture):
-        subject = self.get_subject(mocker)
-
-        mocker.patch.object(subject.config, 'message_age_cutoff', 120)
-
-        message = {
-            'state': 'on',
-            'timestamp': int(datetime.utcnow().timestamp() * 1000)
-        }
-
-        assert subject.state == 'unknown'
-        subject.on_message(None, None, message, subject.name, 'change')
-        assert subject.state == 'on'
-
-    def test_old_change_message(self, mocker: MockerFixture):
-        subject = self.get_subject(mocker)
-
-        mocker.patch.object(subject.config, 'message_age_cutoff', 120)
-
-        message = {
-            'state': 'on',
-            'timestamp': 0
-        }
-
-        assert subject.state == 'unknown'
-        subject.on_message(None, None, message, subject.name, 'change')
-        assert subject.state == 'unknown'
-
-    def test_wrong_change_message(self, mocker: MockerFixture):
-        subject = self.get_subject(mocker)
-
-        mocker.patch.object(subject.config, 'message_age_cutoff', 120)
-
-        message = {
-            'state': 'on',
-            'timestamp': int(datetime.utcnow().timestamp() * 1000)
-        }
-
-        assert subject.state == 'unknown'
-        subject.on_message(None, None, message, 'other', 'change')
-        assert subject.state == 'unknown'
-
-    def test_bad_state_change_message(self, mocker: MockerFixture):
-        subject = self.get_subject(mocker)
-
-        mocker.patch.object(subject.config, 'message_age_cutoff', 120)
-
-        message = {
-            'state': 'notastate',
-            'timestamp': int(datetime.utcnow().timestamp() * 1000)
-        }
-
-        assert subject.state == 'unknown'
-        subject.on_message(None, None, message, subject.name, 'change')
-        assert subject.state == 'unknown'
