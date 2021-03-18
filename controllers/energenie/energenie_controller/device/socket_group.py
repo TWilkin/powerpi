c1276e5e68f0e9454960f1cc58f8d96e4795e750
TWilkin <4322355+TWilkin@users.noreply.github.com>
2021-03-18 21:28:25 +0000

    Split SocketDevice and SocketGroupDevice

--- controllers/energenie/energenie_controller/device/socket_group.py
+++ controllers/energenie/energenie_controller/device/socket_group.py
@@ -2,25 +2,28 @@ import time
 
 from powerpi_common.config import Config
 from powerpi_common.logger import Logger
-from powerpi_common.device import Device
+from powerpi_common.device import Device, DeviceManager
 from powerpi_common.mqtt import MQTTClient
 
 
-class SocketDevice(Device):
+class SocketGroupDevice(Device):
 
     def __init__(
         self,
         config: Config,
         logger: Logger,
         mqtt_client: MQTTClient,
+        device_manager: DeviceManager,
         name: str,
-        home_id=0,
-        device_id=0,
+        devices: list(str),
+        home_id: int = None,
         retries=4,
-        delay=0.5,
+        delay=0.5
     ):
         Device.__init__(self, config, logger, mqtt_client, name)
 
+        self.__device_manager = device_manager
+        self.__devices = devices
         self.__retries = retries
         self.__delay = delay
 
@@ -30,7 +33,10 @@ class SocketDevice(Device):
     def _turn_off(self):
         pass
 
-    def _run(self, func, new_state: str, *params):
+    def _run(self, func, new_state, *params):
         for _ in range(0, self.__retries):
             func(*params)
             time.sleep(self.__delay)
+
+        for device in self.__devices:
+            self.__device_manager.get_device(device).state = new_state
