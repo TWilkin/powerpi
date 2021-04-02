d3ae5a56c905f5fa01822096e811f769968c7c7f
TWilkin <4322355+TWilkin@users.noreply.github.com>
2021-04-02 16:29:42 +0100

    Add generation of consumers to EventManager

--- common/python/powerpi_common/event/manager.py
+++ common/python/powerpi_common/event/manager.py
@@ -1,7 +1,7 @@
 from powerpi_common.config import Config
 from powerpi_common.logger import Logger
-from powerpi_common.device import DeviceManager
-from powerpi_common.mqtt import MQTTClient
+from powerpi_common.device import Device, DeviceManager
+from powerpi_common.mqtt import DeviceStateEventConsumer, MQTTClient
 
 
 class EventManager(object):
@@ -16,6 +16,49 @@ class EventManager(object):
         self.__logger = logger
         self.__device_manager = device_manager
 
+        self.__consumers = []
+
     def load(self):
         events = self.__config.events['events']
-        self.__logger.info(events)
+
+        # iterate over the configuration events and create listeners
+        for event in events:
+            # see if we can find the device
+            try:
+                device = self.__device_manager.get_device(
+                    event['action']['device']
+                )
+            except:
+                # not a problem, we didn't know if this is the correct controller
+                continue
+
+            topic = '{}/event/{}'.format(
+                self.__config.topic_base,
+                event['topic']
+            )
+
+            self.__consumers.append(EventConsumer(
+                self.__config, self.__logger, topic, device
+            ))
+
+            self.__logger.info(
+                f'Found event on topic "{topic}" for device {device}'
+            )
+
+        self.__logger.info(
+            f'Found {len(self.__consumers)} matching events'
+        )
+
+
+class EventConsumer(DeviceStateEventConsumer):
+    def __init__(
+        self,
+        config: Config,
+        logger: Logger,
+        topic: str,
+        device: Device
+    ):
+        DeviceStateEventConsumer.__init__(self, topic, device, config, logger)
+
+    def on_message(self, client, user_data, message, entity, action):
+        pass
--- common/python/powerpi_common/mqtt/__init__.py
+++ common/python/powerpi_common/mqtt/__init__.py
@@ -1,4 +1,5 @@
 from .client import MQTTClient
 from .consumer import MQTTConsumer
+from .device_state_event_consumer import DeviceStateEventConsumer
 from .power_event_consumer import PowerEventConsumer
 from .status_event_consumer import StatusEventConsumer
