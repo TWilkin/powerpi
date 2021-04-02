63cd286f3de971586b02f80fa0934a32f9ad3ca3
TWilkin <4322355+TWilkin@users.noreply.github.com>
2021-04-02 17:28:52 +0100

    power-starter was loading events file

--- power-starter/power_starter/util/config.py
+++ power-starter/power_starter/util/config.py
@@ -7,23 +7,23 @@ class Config(object):
     __instance = None
 
     def __init__(self):
-        self.__devices = Config.__load(os.getenv('DEVICES_FILE'), os.getenv('DEVICES'))
-        self.__events = Config.__load(os.getenv('EVENTS_FILE'), os.getenv('EVENTS'))
+        self.__devices = Config.__load(
+            os.getenv('DEVICES_FILE'), os.getenv('DEVICES'))
 
     @property
     def device_fatal(self):
         value = os.getenv('DEVICE_FATAL')
         return value.lower() == 'true' if value is not None else False
-    
+
     @property
     def energenie_device(self):
         value = os.getenv('ENERGENIE_DEVICE')
 
         if value is not None:
             value = value.upper()
-        
+
         return value if value is not None and value == 'ENER314' else 'ENER314-RT'
-    
+
     @property
     def is_ener314_rt(self):
         return self.energenie_device == 'ENER314-RT'
@@ -31,7 +31,7 @@ class Config(object):
     @property
     def mqtt_address(self):
         return os.getenv('MQTT_ADDRESS')
-    
+
     @property
     def topic_base(self):
         return os.getenv('TOPIC_BASE')
@@ -45,20 +45,20 @@ class Config(object):
     def message_age_cutoff(self):
         cutoff = as_int(os.getenv('MESSAGE_AGE_CUTOFF'))
         return cutoff if cutoff is not None else 120
-    
+
     @property
     def devices(self):
         return self.__devices
-    
+
     @property
     def events(self):
         return self.__events
-    
+
     @classmethod
     def instance(cls):
         if cls.__instance is None:
             cls.__instance = Config()
-        
+
         return cls.__instance
 
     @classmethod
@@ -73,7 +73,7 @@ class Config(object):
 def as_int(value):
     if value is None:
         return None
-    
+
     try:
         return int(value)
     except ValueError:
