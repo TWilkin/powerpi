5ef5e3f3aae12fad694d313bfa04ac29946221c7
TWilkin <4322355+TWilkin@users.noreply.github.com>
2021-05-30 14:33:45 +0100

    Add long press for normal power buttons

--- nginx/ui/src/components/DevicePowerButton.tsx
+++ nginx/ui/src/components/DevicePowerButton.tsx
@@ -1,6 +1,9 @@
+import { faPowerOff } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import classNames from "classnames";
 import { DeviceState, PowerPiApi } from "powerpi-common-api";
 import React, { MouseEvent, useEffect, useState } from "react";
+import useLongPress from "../hooks/useLongPress";
 
 interface DevicePowerButtonProps {
   api: PowerPiApi;
@@ -11,12 +14,23 @@ interface DevicePowerButtonProps {
 const DevicePowerButton = ({ api, device, state }: DevicePowerButtonProps) => {
   const [changeState, setChangeState] = useState(DeviceState.Unknown);
   const [loading, setLoading] = useState(false);
+  const [toggle, setToggle] = useState(true);
 
   useEffect(() => {
     setLoading(false);
   }, [state]);
 
-  const handleClick = async (event: MouseEvent<HTMLDivElement>) => {
+  // set to loading and make the state change API call
+  const updateState = async (newState: DeviceState) => {
+    setLoading(true);
+
+    setChangeState(newState);
+
+    await api.postMessage(device, newState);
+  };
+
+  // handle a click on the slider to toggle
+  const handleSliderClick = async (event: MouseEvent<HTMLDivElement>) => {
     event.preventDefault();
 
     let newState = DeviceState.On;
@@ -30,22 +44,58 @@ const DevicePowerButton = ({ api, device, state }: DevicePowerButtonProps) => {
       }
     }
 
-    setLoading(true);
-    setChangeState(newState);
-    await api.postMessage(device, newState);
+    await updateState(newState);
   };
 
+  // handle a click on a button
+  const handleButtonClick = async (
+    event: MouseEvent<HTMLButtonElement>,
+    newState: DeviceState
+  ) => {
+    event.preventDefault();
+
+    await updateState(newState);
+
+    setToggle(true);
+  };
+
+  // support long press to change button type
+  const longPress = useLongPress(() => {
+    setToggle(false);
+  });
+
+  // show the power toggle control
+  if (toggle) {
+    return (
+      <div className="slider" onClick={handleSliderClick} {...longPress}>
+        <span
+          className={classNames(
+            "slider-bar",
+            { on: state === DeviceState.On },
+            { off: state === DeviceState.Off },
+            { unknown: state === DeviceState.Unknown },
+            { loading }
+          )}
+        />
+      </div>
+    );
+  }
+
+  // show an on/off button
   return (
-    <div className="slider" onClick={handleClick}>
-      <span
-        className={classNames(
-          "slider-bar",
-          { on: state === DeviceState.On },
-          { off: state === DeviceState.Off },
-          { unknown: state === DeviceState.Unknown },
-          { loading }
-        )}
-      />
+    <div className="buttons">
+      <button
+        className="on"
+        onClick={(event) => handleButtonClick(event, DeviceState.On)}
+      >
+        <FontAwesomeIcon icon={faPowerOff} />
+      </button>
+      <button
+        className="off"
+        onClick={(event) => handleButtonClick(event, DeviceState.Off)}
+      >
+        <FontAwesomeIcon icon={faPowerOff} />
+      </button>
     </div>
   );
 };
