c123eeec8b7bb9bb12b5ef67f89b93fd960a3263
TWilkin <4322355+TWilkin@users.noreply.github.com>
2021-06-09 20:42:08 +0100

    Add support for verified Alexa request

--- skill/src/index.ts
+++ skill/src/index.ts
@@ -1,8 +1,7 @@
 import { Request, Response } from "express";
-import { ExpressJS, Lambda, Webhook } from "jovo-framework";
+import { ExpressJS, WebhookVerified as Webhook } from "jovo-framework";
 import app from "./app";
 
-// ExpressJS (Jovo Webhook)
 if (process.argv.indexOf("--webhook") > -1) {
   const port = process.env.JOVO_PORT || 3000;
   Webhook.jovoApp = app;
@@ -11,12 +10,10 @@ if (process.argv.indexOf("--webhook") > -1) {
     console.info(`Local server listening on port ${port}.`);
   });
 
-  Webhook.post("/webhook", async (request: Request, response: Response) => {
-    await app.handle(new ExpressJS(request, response));
-  });
+  Webhook.post(
+    ["/webhook", "/webhook_alexa"],
+    async (request: Request, response: Response) => {
+      await app.handle(new ExpressJS(request, response));
+    }
+  );
 }
-
-// AWS Lambda
-export const handler = async (event: any, context: any, callback: Function) => {
-  await app.handle(new Lambda(event, context, callback));
-};
