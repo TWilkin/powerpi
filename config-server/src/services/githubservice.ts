2799acaa377ac414f1a65153623a468b3f613d33
TWilkin <4322355+TWilkin@users.noreply.github.com>
2021-10-23 18:34:54 +0100

    Move getFile to a method

--- config-server/src/services/githubservice.ts
+++ config-server/src/services/githubservice.ts
@@ -5,6 +5,8 @@ import { Service } from "typedi";
 import Container from "../container";
 import ConfigService from "./config";
 
+const fileTypes = ["devices", "events", "schedules"];
+
 @Service()
 export default class GitHubConfigService {
   private mqtt: MqttService;
@@ -22,18 +24,31 @@ export default class GitHubConfigService {
   private async checkForChanges() {
     const octokit = await this.login();
 
-    const getFile = async (file: string) => {
-      const filePath = path.join(this.config.path, file);
+    for (const type of fileTypes) {
+      const file = await this.getFile(octokit, type);
 
-      this.logger.info(
-        `Attempting to retrieve ${path.basename(
-          file,
-          ".json"
-        )} file from github://${this.config.gitHubUser}/${
-          this.config.repo
-        }/${filePath}@${this.config.branch}`
-      );
+      if (file) {
+        this.logger.info(JSON.stringify(file));
+      }
+    }
+  }
 
+  private async login() {
+    const token = await this.config.gitHubToken;
+
+    return new Octokit({
+      auth: token
+    });
+  }
+
+  private async getFile(octokit: Octokit, fileType: string) {
+    const filePath = path.join(this.config.path, `${fileType}.json`);
+
+    this.logger.info(
+      `Attempting to retrieve ${fileType} file from github://${this.config.gitHubUser}/${this.config.repo}/${filePath}@${this.config.branch}`
+    );
+
+    try {
       const { data } = await octokit.rest.repos.getContent({
         owner: this.config.gitHubUser!,
         repo: this.config.repo,
@@ -45,17 +60,10 @@ export default class GitHubConfigService {
       const buffer = Buffer.from(content, "base64");
 
       return JSON.parse(buffer.toString());
-    };
+    } catch {
+      this.logger.error(`Could not retrieve ${fileType} file from GitHub`);
+    }
 
-    const devices = await getFile("devices.json");
-    this.logger.info(JSON.stringify(devices));
-  }
-
-  private async login() {
-    const token = await this.config.gitHubToken;
-
-    return new Octokit({
-      auth: token
-    });
+    return undefined;
   }
 }
