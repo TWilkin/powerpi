c3a26c99d5fac0e96c813f61ed56f5efa35cbac8
TWilkin <4322355+TWilkin@users.noreply.github.com>
2021-10-27 18:26:48 +0100

    Make common MqttService async

--- common/node/common/src/services/mqtt.ts
+++ common/node/common/src/services/mqtt.ts
@@ -1,60 +1,58 @@
-import { connect, IClientPublishOptions, MqttClient } from "mqtt";
+import { AsyncMqttClient, connectAsync, IClientPublishOptions } from "async-mqtt";
 import os from "os";
 import { Service } from "typedi";
 import { ConfigService } from "./config";
 import { LoggerService } from "./logger";
 
 export interface Message {
-  [key: string]: any;
-  timestamp?: number;
+    [key: string]: any;
+    timestamp?: number;
 }
 
 @Service()
 export class MqttService {
-  private client: MqttClient | undefined;
+    private client: AsyncMqttClient | undefined;
 
-  constructor(private config: ConfigService, private logger: LoggerService) {}
+    constructor(private config: ConfigService, private logger: LoggerService) {}
 
-  public connect() {
-    const options = {
-      clientId: `${this.config.service}-${os.hostname}`
-    };
-
-    this.logger.info("MQTT connecting to", this.config.mqttAddress);
-    this.client = connect(this.config.mqttAddress, options);
-
-    this.client.on("connect", () => {
-      this.logger.info("MQTT client", options.clientId, "connected.");
-    });
-
-    this.client.on("error", (error) => {
-      this.logger.error("MQTT client error:", error);
-      process.exit(1);
-    });
-  }
-
-  public publish(
-    type: string,
-    entity: string,
-    action: string,
-    message: Message
-  ) {
-    if (!message.timestamp) {
-      message.timestamp = new Date().getTime();
+    private get clientId() {
+        return `${this.config.service}-${os.hostname}`;
     }
 
-    const options: IClientPublishOptions = {
-      qos: 2,
-      retain: true
-    };
+    public async connect() {
+        const options = {
+            clientId: this.clientId,
+        };
 
-    const topicName = this.topicName(type, entity, action);
+        this.logger.info("MQTT connecting to", this.config.mqttAddress);
+        this.client = await connectAsync(this.config.mqttAddress, options);
+        this.logger.info("MQTT client", options.clientId, "connected.");
+    }
 
-    this.logger.debug("Publishing to", topicName);
+    public async disconnect() {
+        if (this.client) {
+            await this.client.end();
+            this.logger.info("MQTT client", this.clientId, "disconnected.");
+        }
+    }
 
-    this.client?.publish(topicName, JSON.stringify(message), options);
-  }
+    public async publish(type: string, entity: string, action: string, message: Message) {
+        if (!message.timestamp) {
+            message.timestamp = new Date().getTime();
+        }
 
-  private topicName = (type: string, entity: string, action: string) =>
-    `${this.config.topicNameBase}/${type}/${entity}/${action}`;
+        const options: IClientPublishOptions = {
+            qos: 2,
+            retain: true,
+        };
+
+        const topicName = this.topicName(type, entity, action);
+
+        this.logger.debug("Publishing to", topicName);
+
+        await this.client?.publish(topicName, JSON.stringify(message), options);
+    }
+
+    private topicName = (type: string, entity: string, action: string) =>
+        `${this.config.topicNameBase}/${type}/${entity}/${action}`;
 }
