790ab8699cb925907b93f846557f1959776c74a2
TWilkin <4322355+TWilkin@users.noreply.github.com>
2021-11-27 17:07:48 +0000

    Upgrade macro controller

--- controllers/macro/macro_controller/__main__.py
+++ controllers/macro/macro_controller/__main__.py
@@ -1,7 +1,9 @@
+import asyncio
 import sys
 
 from dependency_injector.wiring import inject, Provide
 
+from powerpi_common.config import ConfigRetriever
 from powerpi_common.device import DeviceManager, DeviceStatusChecker
 from powerpi_common.logger import Logger
 from powerpi_common.mqtt import MQTTClient
@@ -11,8 +13,9 @@ from macro_controller.device.container import add_devices
 
 
 @inject
-def main(
+async def main(
     logger: Logger = Provide[ApplicationContainer.common.logger],
+    config_retriever: ConfigRetriever = Provide[ApplicationContainer.common.config_retriever],
     device_manager: DeviceManager = Provide[ApplicationContainer.common.device.device_manager],
     mqtt_client: MQTTClient = Provide[ApplicationContainer.common.mqtt_client],
     device_status_checker: DeviceStatusChecker = Provide[
@@ -20,6 +23,12 @@ def main(
     ]
 ):
     logger.info(f'PowerPi Macro Controller v{__version__}')
+
+    # intially connect to MQTT
+    mqtt_client.connect()
+
+    # retrieve any config from the queue
+    await config_retriever.start()
 
     # load the devices from the config
     device_manager.load()
@@ -37,4 +46,6 @@ if __name__ == '__main__':
     container.wire(modules=[sys.modules[__name__]])
     add_devices(container)
 
-    main()
+    coro = main()
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(coro)
