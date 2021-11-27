be4dcbb87a5c70842fe108a69ecafac62e4f3a1f
TWilkin <4322355+TWilkin@users.noreply.github.com>
2021-11-27 16:59:38 +0000

    Upgrade energenie controller

--- controllers/energenie/energenie_controller/__main__.py
+++ controllers/energenie/energenie_controller/__main__.py
@@ -1,7 +1,9 @@
+import asyncio
 import sys
 
 from dependency_injector.wiring import inject, Provide
 
+from powerpi_common.config import ConfigRetriever
 from powerpi_common.device import DeviceManager
 from powerpi_common.event import EventManager
 from powerpi_common.logger import Logger
@@ -13,17 +15,23 @@ from energenie_controller.device.container import add_devices
 
 
 @inject
-def main(
+async def main(
     config: EnergenieConfig = Provide[ApplicationContainer.config],
     logger: Logger = Provide[ApplicationContainer.common.logger],
+    config_retriever: ConfigRetriever = Provide[ApplicationContainer.common.config_retriever],
     device_manager: DeviceManager = Provide[ApplicationContainer.common.device.device_manager],
     event_manager: EventManager = Provide[ApplicationContainer.common.event_manager],
     mqtt_client: MQTTClient = Provide[ApplicationContainer.common.mqtt_client]
 ):
-    logger.info('PowerPi Energenie Controller v{}'.format(__version__))
+    logger.info(f'PowerPi Energenie Controller v{__version__}')
 
-    logger.info('Using Energenie module {module}'
-                .format(module=config.energenie_device))
+    logger.info(f'Using Energenie module {config.energenie_device}')
+    
+    # intially connect to MQTT
+    mqtt_client.connect()
+
+    # retrieve any config from the queue
+    await config_retriever.start()
 
     # load the devices from the config
     device_manager.load()
@@ -41,4 +49,6 @@ if __name__ == '__main__':
     container.wire(modules=[sys.modules[__name__]])
     add_devices(container)
 
-    main()
+    coro = main()
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(coro)
