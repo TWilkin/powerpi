b9b58f4901813663436ddab79f71b2bb5136782d
TWilkin <4322355+TWilkin@users.noreply.github.com>
2021-12-12 20:59:00 +0000

    Externals library was not needed

--- nginx/ui/webpack.common.js
+++ nginx/ui/webpack.common.js
@@ -1,5 +1,4 @@
 const path = require("path");
-const nodeExternals = require("webpack-node-externals");
 
 module.exports = {
     entry: {
@@ -30,9 +29,4 @@ module.exports = {
         filename: "bundle.js",
         path: path.resolve(__dirname, "dist"),
     },
-    externals: [
-        nodeExternals({
-            modulesFromFile: true,
-        }),
-    ],
 };
