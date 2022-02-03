2aaa011fa013acd51ac2de1314370d0462661f11
TWilkin <4322355+TWilkin@users.noreply.github.com>
2022-02-03 21:17:39 +0000

    Add setup to zigbee controller main

--- controllers/zigbee/zigbee_controller/__main__.py
+++ controllers/zigbee/zigbee_controller/__main__.py
@@ -1,15 +1,31 @@
 import asyncio
 import sys
 
-from dependency_injector.wiring import inject
+from dependency_injector.wiring import inject, Provide
 
+from powerpi_common.config import ConfigRetriever
+from powerpi_common.logger import Logger
+from powerpi_common.mqtt import MQTTClient
 from zigbee_controller.__version import __version__
 from zigbee_controller.container import ApplicationContainer
 
 
 @inject
-async def main():
-    pass
+async def main(
+    logger: Logger = Provide[ApplicationContainer.common.logger],
+    config_retriever: ConfigRetriever = Provide[ApplicationContainer.common.config_retriever],
+    mqtt_client: MQTTClient = Provide[ApplicationContainer.common.mqtt_client],
+):
+    logger.info(f'PowerPi Zigbee Controller v{__version__}')
+
+    # intially connect to MQTT
+    mqtt_client.connect()
+
+    # retrieve any config from the queue
+    await config_retriever.start()
+
+    # use MQTT loop to handle messages
+    mqtt_client.loop()
 
 
 if __name__ == '__main__':
