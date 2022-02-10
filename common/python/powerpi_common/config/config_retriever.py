c3adb4d17e75440a8a1b77eebdca22182f4bbebc
TWilkin <4322355+TWilkin@users.noreply.github.com>
2022-02-10 19:54:23 +0000

    Fix bug where ZigBee and Harmony controllers won't restart on config

--- common/python/powerpi_common/config/config_retriever.py
+++ common/python/powerpi_common/config/config_retriever.py
@@ -1,5 +1,5 @@
 import asyncio
-import sys
+import os
 
 from powerpi_common.logger import Logger
 from powerpi_common.mqtt import MQTTClient, MQTTConsumer
@@ -13,9 +13,9 @@ class ConfigRetriever(object):
         self.__mqtt_client = mqtt_client
 
     async def start(self):
-        # subscribe to change topic for each device type
-        for type in self.__config.used_config:
-            topic = f'config/{type}/change'
+        # subscribe to change topic for each file type
+        for file_type in self.__config.used_config:
+            topic = f'config/{file_type}/change'
             consumer = ConfigConsumer(topic, self.__config, self.__logger)
             self.__mqtt_client.add_consumer(consumer)
         
@@ -58,7 +58,7 @@ class ConfigConsumer(MQTTConsumer):
             and self._config.get_config(entity) is not None:
                 # this is a changed config and used, so we should restart the service
                 self._logger.info(f'Restarting service due to changed {entity} config')
-                sys.exit(0)
+                os._exit(0)
         else:
             # this is a new or unused config, so just set it
             self._config.set_config(entity, message.get('payload'))
