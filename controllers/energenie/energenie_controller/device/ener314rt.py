086dd08494ece2cdb107702b33192ffeb204f51a
TWilkin <4322355+TWilkin@users.noreply.github.com>
2022-02-18 17:43:11 +0000

    Ensure Energenie can't try and start/stop 2 devices at once

--- controllers/energenie/energenie_controller/device/ener314rt.py
+++ controllers/energenie/energenie_controller/device/ener314rt.py
@@ -1,18 +1,27 @@
 import atexit
 
-from pyenergenie import energenie
+from pyenergenie.energenie import init, finished
+from pyenergenie.energenie.Devices import ENER002
 
 from .energenie import EnergenieInterface
 
 
-energenie.init()
-atexit.register(energenie.finished)
+init()
+atexit.register(finished)
 
 
-class EnergenieInterfaceImpl(energenie.Devices.ENER002, EnergenieInterface):
+class EnergenieInterfaceImpl(EnergenieInterface):
     def __init__(self):
-        energenie.Devices.ENER002.__init__(self, (0, 0))
+        EnergenieInterface.__init__(self)
+
+        self.__device = ENER002((0, 0))
 
     def set_ids(self, home_id: int, device_id: int):
         EnergenieInterface.set_ids(self, home_id, device_id)
-        self.device_id = [home_id, device_id]
+        self.__device.device_id = [home_id, device_id]
+    
+    def _turn_on(self):
+        self.__device.turn_on(self)
+    
+    def _turn_off(self):
+        self.__device.turn_off(self)
