998cc4c07ac71e84121f4f0b901aff613c57575b
TWilkin <4322355+TWilkin@users.noreply.github.com>
2022-02-25 18:01:32 +0000

    Put test files into matching directories

--- common/pytest/setup.py
+++ common/pytest/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='powerpi-common-test',
-    version='0.1.1',
+    version='0.1.2',
     description='PowerPi Common Python Test Library',
     author='TWilkin',
     classifiers=[
