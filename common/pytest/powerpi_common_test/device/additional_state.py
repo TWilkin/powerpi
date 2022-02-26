5bd12f9606ee3bd8fdac05be2744dfd61d279ddc
TWilkin <4322355+TWilkin@users.noreply.github.com>
2022-02-26 16:52:35 +0000

    Add test to check additional state is rebroadcast on state change

--- common/pytest/powerpi_common_test/device/additional_state.py
+++ common/pytest/powerpi_common_test/device/additional_state.py
@@ -1,6 +1,8 @@
 from datetime import datetime
 from pytest_mock import MockerFixture
+from typing import Any, Dict
 
+from powerpi_common_test.mqtt import mock_producer
 from .device import DeviceTestBase
 
 
@@ -40,6 +42,41 @@ class AdditionalStateDeviceTestBase(DeviceTestBase):
         assert subject.state == 'on'
         assert subject.additional_state.get('something', None) is None
         assert subject.additional_state.get(key, None) == 1
+    
+    async def test_state_change_outputs_additional_state(self, mocker: MockerFixture):
+        # capture the published messages
+        self.messages = []
+        def mock_add_producer():
+            def add_producer():
+                def publish(_: str, message: Dict[str, Any]):
+                    self.messages.append(message)
+                
+                return publish
+            
+            self.mqtt_client.add_producer = add_producer
+        
+        subject = self.create_subject(mocker, mock_add_producer)
+
+        mocker.patch.object(self.config, 'message_age_cutoff', 120)
+
+        assert subject.state == 'unknown'
+        assert subject.additional_state == {}
+
+        key = subject._additional_state_keys()[0]
+        additional_state = {}
+        additional_state[key] = 1
+        subject.additional_state = additional_state
+        assert subject.additional_state.get(key, None) == 1
+
+        # turn the device on, then off
+        for state in ['on', 'off']:
+            await subject.change_power(state)
+            assert subject.state == state
+            assert subject.additional_state.get(key, None) == 1
+
+        # ensure all message are present and contain the additional state
+        assert len(self.messages) == 3
+        assert all([key in message for message in self.messages])
     
     def test_initial_additional_state_message(self, mocker: MockerFixture):
         self.initial_state_consumer = None
