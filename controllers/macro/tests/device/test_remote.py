da0705c711d89b2f6c46c0d064651e0a741a8c54
TWilkin <4322355+TWilkin@users.noreply.github.com>
2022-02-26 21:11:12 +0000

    #94 Upgrade RemoteDevice to support additional state

--- controllers/macro/tests/device/test_remote.py
+++ controllers/macro/tests/device/test_remote.py
@@ -2,6 +2,7 @@ import asyncio
 import pytest
 
 from pytest_mock import MockerFixture
+from typing import Union
 
 from macro_controller.device.remote import RemoteDevice
 from powerpi_common_test.mqtt import mock_producer
@@ -21,7 +22,7 @@ class TestRemoteDevice(object):
             self.config, self.logger, self.mqtt_client, 'remote', timeout
         )
     
-    @pytest.mark.parametrize('state', [('on'), ('off')])
+    @pytest.mark.parametrize('state', ['on', 'off'])
     async def test_turn_x(self, mocker: MockerFixture, state: str):
         subject = self.get_subject(mocker, 0.2)
 
@@ -40,7 +41,7 @@ class TestRemoteDevice(object):
 
         assert subject.state == state
 
-    @pytest.mark.parametrize('state', [('on'), ('off')])
+    @pytest.mark.parametrize('state', ['on', 'off'])
     async def test_turn_x_timeout(self, mocker: MockerFixture, state: str):
         subject = self.get_subject(mocker, 0.1)
 
@@ -56,11 +57,56 @@ class TestRemoteDevice(object):
         self.publish.assert_any_call(topic, message)
 
         assert subject.state == 'unknown'
+    
+    @pytest.mark.parametrize('state', [None, 'on', 'off'])
+    @pytest.mark.parametrize('use_additional_state', [True, False])
+    async def test_change_power_and_additional_state(self, mocker: MockerFixture, state: Union[str, None], use_additional_state: bool):
+        subject = self.get_subject(mocker, 0.2)
 
-    def __schedule_state_change(self, subject: RemoteDevice, state: str):
+        assert subject.state == 'unknown'
+        assert subject.additional_state == {}
+
+        additional_state = {'something': 'else'} if use_additional_state else None
+        self.__schedule_state_change(subject, state, additional_state)
+
+        await subject.change_power_and_additional_state(state, additional_state)
+
+        topic = 'device/remote/change'
+        message = {}
+        if state is not None:
+            message['state'] = state
+        if use_additional_state:
+            message['something'] = 'else'
+
+        self.publish.assert_any_call(topic, message)
+
+        assert subject.state == state if state is not None else 'unknown'
+        
+        if use_additional_state:
+            assert subject.additional_state.get('something', None) == 'else'
+    
+    async def test_change_power_and_additional_state_timeout(self, mocker: MockerFixture):
+        subject = self.get_subject(mocker, 0.1)
+
+        assert subject.state == 'unknown'
+        assert subject.additional_state == {}
+
+        await subject.change_power_and_additional_state('on', {'something': 'else'})
+
+        topic = 'device/remote/change'
+        message = {
+            'state': 'on',
+            'something': 'else'
+        }
+        self.publish.assert_any_call(topic, message)
+
+        assert subject.state == 'unknown'
+        assert subject.additional_state == {}
+
+    def __schedule_state_change(self, subject: RemoteDevice, state: str, additional_state={}):
         async def update():
             await asyncio.sleep(0.1)
-            subject.state = state
+            subject.set_state_and_additional(state, additional_state)
         
         loop = asyncio.get_event_loop()
         loop.create_task(update())
