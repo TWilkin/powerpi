7197677d59e9f32fe09bda7f9e2099d0ec07f489
TWilkin <4322355+TWilkin@users.noreply.github.com>
2022-03-10 19:46:42 +0000

    Upgrade powerpi-common to poetry

--- common/pytest/powerpi_common_test/device/mixin/initialisable.py
+++ common/pytest/powerpi_common_test/device/mixin/initialisable.py
@@ -1,9 +1,13 @@
+import pytest
+
 from abc import ABC
 from pytest_mock import MockerFixture
 
 
 class InitialisableMixinTestBase(ABC):
-    def test_initialise_implemented(self, mocker: MockerFixture):
+    pytestmark = pytest.mark.asyncio
+    
+    async def test_initialise_implemented(self, mocker: MockerFixture):
         subject = self.create_subject(mocker)
 
-        subject.initialise()
+        await subject.initialise()
