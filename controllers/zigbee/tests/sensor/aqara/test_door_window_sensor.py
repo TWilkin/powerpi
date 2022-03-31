92abfaf67a4adbbf11fb89525b7017c260e609fe
TWilkin <4322355+TWilkin@users.noreply.github.com>
2022-03-31 19:01:21 +0100

    Add test for on_attribute_updated

--- controllers/zigbee/tests/sensor/aqara/test_door_window_sensor.py
+++ controllers/zigbee/tests/sensor/aqara/test_door_window_sensor.py
@@ -56,6 +56,51 @@ class TestAqaraDoorWindowSensor(
 
         self.publish.assert_not_called()
 
+    @pytest.mark.parametrize('values', [
+        (3100, 100),
+        (3101, 100),
+        (2820, 0),
+        (2819, 0),
+        (2819, 0),
+        (3075, 91)
+    ])
+    def test_on_attribute_updated(self, mocker: MockerFixture, values: Tuple[int, int]):
+        (data, percent) = values
+
+        def mock():
+            self.publish = mock_producer(mocker, self.mqtt_client)
+
+        subject = self.create_subject(mocker, mock)
+
+        # data is attribute id (0x01), type value (0x21), data in little endian
+        value = bytearray(b'\x01\x21')
+        value += data.to_bytes(2, 'little')
+        value = value.decode('utf8')
+
+        subject.on_attribute_updated(0xFF01, value)
+
+        topic = 'event/test/battery'
+        message = {'value': percent, 'unit': '%'}
+        self.publish.assert_called_once_with(topic, message)
+
+    @pytest.mark.parametrize('values', [(0xFF00, 0x01), (0xFF01, 0xBD)])
+    def test_on_attribute_updated_other_data(self, mocker: MockerFixture, values: Tuple[int, int]):
+        (attribute_id, data_id) = values
+
+        def mock():
+            self.publish = mock_producer(mocker, self.mqtt_client)
+
+        subject = self.create_subject(mocker, mock)
+
+        # data is attribute id (0x02), type value (0x21), data in little endian
+        value = bytearray(data_id)
+        value += b'\x21\x00\x00'
+        value = value.decode('utf8')
+
+        subject.on_attribute_updated(attribute_id, value)
+
+        self.publish.assert_not_called()
+
     def __verify_publish(self, state: str):
         topic = 'event/test/change'
 
