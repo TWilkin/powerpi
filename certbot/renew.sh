b8555deb777e0ae3e86fb4b9dbab55f7c0db970c
TWilkin <4322355+TWilkin@users.noreply.github.com>
2022-06-12 15:57:12 +0100

    #125 Make certbot write the output to the correct directory

--- certbot/renew.sh
+++ certbot/renew.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-PWD=`pwd`
+OUT_DIR=/etc/letsencrypt
 
 # renew (or request a new) certificate
 certbot \
@@ -10,10 +10,10 @@ certbot \
     --renew-by-default \
     --non-interactive \
     -d $EXTERNAL_HOST_NAME \
-    --config-dir $PWD \
-    --work-dir $PWD \
-    --logs-dir $PWD
+    --config-dir $OUT_DIR \
+    --work-dir $OUT_DIR \
+    --logs-dir $OUT_DIR
 
 # create a symlink as NGINX doesn't support environment variables in the config files
-rm -f /etc/letsencrypt/live/default
-ln -s /etc/letsencrypt/live/$EXTERNAL_HOST_NAME /etc/letsencrypt/live/default
+rm -f $OUT_DIR/live/default
+ln -s $OUT_DIR/live/$EXTERNAL_HOST_NAME $OUT_DIR/live/default
