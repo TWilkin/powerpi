937d47cc8fd70072974c1389e3bde4d0f8904fec
TWilkin <TWilkin@users.noreply.github.com>
2022-07-31 14:58:44 +0000

    #94 Get build working with Alexa

--- services/babel-fish/jovo.project.js
+++ services/babel-fish/jovo.project.js
@@ -1,18 +1,10 @@
 const { ProjectConfig } = require('@jovotech/cli-core');
+const { AlexaCli } = require('@jovotech/platform-alexa');
 
-/*
-|--------------------------------------------------------------------------
-| JOVO PROJECT CONFIGURATION
-|--------------------------------------------------------------------------
-|
-| Information used by the Jovo CLI to build and deploy projects
-| Learn more here: www.jovo.tech/docs/project-config
-|
-*/
 const project = new ProjectConfig({
   endpoint: '${JOVO_WEBHOOK_URL}',
   plugins: [
-    // Add Jovo CLI plugins here
+    new AlexaCli(),
   ],
 });
 
