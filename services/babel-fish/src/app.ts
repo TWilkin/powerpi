5dc62a3483a0905b81f7742f61bb51422ab6d865
TWilkin <TWilkin@users.noreply.github.com>
2022-08-07 14:20:32 +0000

    #94 Actually make the request to the API

--- services/babel-fish/src/app.ts
+++ services/babel-fish/src/app.ts
@@ -1,13 +1,15 @@
 import { App } from '@jovotech/framework';
 import { AlexaPlatform } from '@jovotech/platform-alexa';
 import DevicePowerComponent from './components/DevicePowerComponent';
+import ErrorComponent from './components/ErrorComponent';
 import GlobalComponent from './components/GlobalComponent';
 import LoginComponent from './components/LoginComponent';
 
 const app = new App({
   components: [
-    GlobalComponent, 
+    ErrorComponent,
     DevicePowerComponent,
+    GlobalComponent, 
     LoginComponent
   ],
   plugins: [
--- services/babel-fish/src/components/DevicePowerComponent.ts
+++ services/babel-fish/src/components/DevicePowerComponent.ts
@@ -1,6 +1,8 @@
 import { BaseComponent, Component, Intents } from "@jovotech/framework";
+import { DeviceState, PowerPiApi } from "@powerpi/api";
 import Container from "../container";
 import DeviceService from "../services/DeviceService";
+import ErrorComponent from "./ErrorComponent";
 
 @Component()
 export default class DevicePowerComponent extends BaseComponent {
@@ -27,12 +29,21 @@ export default class DevicePowerComponent extends BaseComponent {
     }
 
     @Intents(["DevicePowerIntent"])
-    action() {
+    async action() {
         const deviceService = Container.get(DeviceService);
         const device = deviceService.find(this.$entities.deviceName?.id);
         const status = this.$entities.status?.id;
 
         if(device && status) {
+            const success = await makeRequest(
+                this.$alexa?.$user.accessToken, 
+                api => api.postMessage(device.name, status as DeviceState)
+            );
+
+            if(!success) {
+                return this.$redirect(ErrorComponent, "apiError");
+            }
+
             return this.$send({ message: `Turning ${device?.displayName} ${status}`, listen: false });
         }
 
@@ -44,4 +55,19 @@ export default class DevicePowerComponent extends BaseComponent {
     UNHANDLED() {
         return this.START();
     }
+}
+
+async function makeRequest(token: string | undefined, action: (api: PowerPiApi) => Promise<unknown>) {
+    const api = new PowerPiApi("http://deep-thought:3000/api");
+
+    if (token) {
+        api.setCredentials(token);
+    }
+
+    let success = true;
+    api.setErrorHandler(() => (success = false));
+
+    await action(api);
+
+    return success;
 }
