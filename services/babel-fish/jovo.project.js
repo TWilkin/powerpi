d8b2f18593bbd2a244833e7387f6df1dda09aa97
TWilkin <TWilkin@users.noreply.github.com>
2022-08-07 16:25:16 +0000

    Apply prettier to json and js files

--- services/babel-fish/jovo.project.js
+++ services/babel-fish/jovo.project.js
@@ -1,40 +1,38 @@
-const { ProjectConfig } = require('@jovotech/cli-core');
+const { ProjectConfig } = require("@jovotech/cli-core");
 const { AlexaCli } = require("@jovotech/platform-alexa");
 
 const project = new ProjectConfig({
-  endpoint: '${JOVO_WEBHOOK_URL}',
-  plugins: [
-    new AlexaCli({
-      files: {
-        "skill-package/skill.json": {
-          manifest: {
-            publishingInformation: {
-              locales: {
-                "en-GB": {
-                  summary: "PowerPi Alexa Integration",
-                  examplePhrases: [
-                    "Alexa start PowerPi"
-                  ],
-                  keywords: [
-                    "powerpi"
-                  ],
-                  name: "PowerPI babel-fish",
-                  description: "PowerPi Alexa Integration",
-                  smallIconUri: "https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@5.15/svgs/solid/plug.svg",
-                  largeconUri: "https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@5.15/svgs/solid/plug.svg"
-                }
-              }
+    endpoint: "${JOVO_WEBHOOK_URL}",
+    plugins: [
+        new AlexaCli({
+            files: {
+                "skill-package/skill.json": {
+                    manifest: {
+                        publishingInformation: {
+                            locales: {
+                                "en-GB": {
+                                    summary: "PowerPi Alexa Integration",
+                                    examplePhrases: ["Alexa start PowerPi"],
+                                    keywords: ["powerpi"],
+                                    name: "PowerPI babel-fish",
+                                    description: "PowerPi Alexa Integration",
+                                    smallIconUri:
+                                        "https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@5.15/svgs/solid/plug.svg",
+                                    largeconUri:
+                                        "https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@5.15/svgs/solid/plug.svg",
+                                },
+                            },
+                        },
+                        privacyAndCompliance: {
+                            locales: {
+                                "en-GB": {},
+                            },
+                        },
+                    },
+                },
             },
-            privacyAndCompliance: {
-              locales: {
-                "en-GB": {}
-              }
-            }
-          }
-        }
-      }
-    }),
-  ],
+        }),
+    ],
 });
 
 module.exports = project;
