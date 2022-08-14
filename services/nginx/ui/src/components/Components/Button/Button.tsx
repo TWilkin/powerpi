9fbeb5ee393b67062b36156ef4d03ee5bb0d759e
TWilkin <TWilkin@users.noreply.github.com>
2022-08-14 21:44:18 +0100

    Use the new Button component for long press

--- services/nginx/ui/src/components/Components/Button/Button.tsx
+++ services/nginx/ui/src/components/Components/Button/Button.tsx
@@ -1,17 +1,28 @@
 import { IconProp } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import classNames from "classnames";
 import { MouseEvent } from "react";
 import styles from "./Button.module.scss";
 
 interface ButtonProps {
-    text: string;
+    text?: string;
     icon?: IconProp;
     onClick: (event: MouseEvent<HTMLButtonElement>) => void;
+    className?: string;
 }
 
-const Button = ({ text, icon, onClick }: ButtonProps) => {
+const Button = ({ text, icon, onClick, className }: ButtonProps) => {
     return (
-        <button className={styles.button} onClick={onClick}>
+        <button
+            className={classNames(
+                styles.button,
+                {
+                    [styles.text]: text !== undefined,
+                },
+                className
+            )}
+            onClick={onClick}
+        >
             {icon && <FontAwesomeIcon icon={icon} />}
 
             {text}
