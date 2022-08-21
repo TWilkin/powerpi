0231cf6dbfbc7b76935a078ad63c49efbba2f29e
TWilkin <TWilkin@users.noreply.github.com>
2022-08-21 22:25:50 +0100

    #93 Add validation of the schedules file

--- services/clacks-config/src/schema/index.ts
+++ services/clacks-config/src/schema/index.ts
@@ -1,9 +1,11 @@
 import * as floorplan from "./floorplan.schema.json";
+import * as schedules from "./schedules.schema.json";
 import * as users from "./users.schema.json";
 
 export default function loadSchema() {
     return {
         floorplan,
+        schedules,
         users,
     };
 }
