d0b718f0851538ed51b32a13bddc626517d1ca6f
TWilkin <TWilkin@users.noreply.github.com>
2022-08-21 21:56:33 +0100

    #93 Add validation of the floorplan file

--- services/clacks-config/src/schema/index.ts
+++ services/clacks-config/src/schema/index.ts
@@ -1,7 +1,9 @@
+import * as floorplan from "./floorplan.schema.json";
 import * as users from "./users.schema.json";
 
 export default function loadSchema() {
     return {
+        floorplan,
         users,
     };
 }
