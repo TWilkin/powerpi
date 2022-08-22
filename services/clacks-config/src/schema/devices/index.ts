80649ce85605e8060e6e58df8b485bfed4541b2a
TWilkin <TWilkin@users.noreply.github.com>
2022-08-22 18:34:12 +0100

    #93 Add the composed harmony_hub schema

--- services/clacks-config/src/schema/devices/index.ts
+++ services/clacks-config/src/schema/devices/index.ts
@@ -1,7 +1,13 @@
 import * as BaseDevice from "./BaseDevice.schema.json";
+import * as HarmonyHub from "./harmony/HarmonyHub.schema.json";
+import * as PollableDevice from "./PollableDevice.schema.json";
 
 export default function loadDevicesSchema() {
     return {
         BaseDevice,
+        PollableDevice,
+
+        // the harmony controller devices
+        HarmonyHub,
     };
 }
