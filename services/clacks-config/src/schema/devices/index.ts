d3edfb1dfeb978573224893f3e97c16632e57c07
TWilkin <TWilkin@users.noreply.github.com>
2022-08-22 18:51:46 +0100

    #93 Add the composed Energenie socket schema

--- services/clacks-config/src/schema/devices/index.ts
+++ services/clacks-config/src/schema/devices/index.ts
@@ -1,4 +1,7 @@
 import * as BaseDevice from "./BaseDevice.schema.json";
+import * as EnergenieCommon from "./energenie/EnergenieCommon.schema.json";
+import * as EnergenieSocket from "./energenie/EnergenieSocket.schema.json";
+import * as EnergenieSocketGroup from "./energenie/EnergenieSocketGroup.schema.json";
 import * as HarmonyHub from "./harmony/HarmonyHub.schema.json";
 import * as PollableDevice from "./PollableDevice.schema.json";
 
@@ -7,7 +10,12 @@ export default function loadDevicesSchema() {
         BaseDevice,
         PollableDevice,
 
-        // the harmony controller devices
+        // the Energenie controller devices
+        EnergenieCommon,
+        EnergenieSocket,
+        EnergenieSocketGroup,
+
+        // the Harmony controller devices
         HarmonyHub,
     };
 }
