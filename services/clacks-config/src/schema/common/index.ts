78a1b1a3e425a4744f14d7f4e6480f9420905ab8
TWilkin <TWilkin@users.noreply.github.com>
2022-08-22 18:05:57 +0100

    #93 Add types for Time and WeekDay

--- services/clacks-config/src/schema/common/index.ts
+++ services/clacks-config/src/schema/common/index.ts
@@ -1,7 +1,11 @@
 import * as DeviceName from "./DeviceName.schema.json";
+import * as Time from "./Time.schema.json";
+import * as WeekDay from "./WeekDay.schema.json";
 
 export default function loadCommonSchema() {
     return {
         DeviceName,
+        Time,
+        WeekDay,
     };
 }
