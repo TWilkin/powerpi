ed107495d46dc02c8ca6bb1b930da02f1731ca86
TWilkin <TWilkin@users.noreply.github.com>
2022-08-22 20:59:41 +0100

    #93 Add the macro devices

--- services/clacks-config/src/schema/devices/macro/index.ts
+++ services/clacks-config/src/schema/devices/macro/index.ts
@@ -1,7 +1,13 @@
+import * as Composite from "./Composite.schema.json";
 import * as Delay from "./Delay.schema.json";
+import * as Log from "./Log.schema.json";
+import * as Mutex from "./Mutex.schema.json";
 
 export default function loadMacroSchema() {
     return {
+        Composite,
         Delay,
+        Log,
+        Mutex,
     };
 }
