38d62ed743b45e39ed5e7c0d641b214cec989887
TWilkin <TWilkin@users.noreply.github.com>
2022-09-19 13:14:47 +0100

    #183 Make MAC address use a re-usable schema

--- services/clacks-config/src/schema/common/index.ts
+++ services/clacks-config/src/schema/common/index.ts
@@ -2,6 +2,7 @@ import * as Condition from "./Condition.schema.json";
 import * as DeviceState from "./DeviceState.schema.json";
 import * as Identifier from "./Identifier.schema.json";
 import * as JsonPath from "./JsonPatch.schema.json";
+import * as Mac from "./MAC.schema.json";
 import * as Time from "./Time.schema.json";
 import * as WeekDay from "./WeekDay.schema.json";
 
@@ -11,6 +12,7 @@ export default function loadCommonSchema() {
         DeviceState,
         Identifier,
         JsonPath,
+        Mac,
         Time,
         WeekDay,
     };
