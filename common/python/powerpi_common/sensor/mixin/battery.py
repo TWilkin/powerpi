867a8e45a0cbf819ddf310fba27f693d59aa0b35
TWilkin <TWilkin@users.noreply.github.com>
2022-10-13 21:03:19 +0100

    #221 Make it pollable and provide battery level

--- common/python/powerpi_common/sensor/mixin/battery.py
+++ common/python/powerpi_common/sensor/mixin/battery.py
@@ -3,12 +3,12 @@ from abc import ABC
 
 class BatteryMixin(ABC):
     '''
-    Mixing to add battery message broadcast functionality to a sensor.
+    Mixing to add battery message broadcast functionality to a device/sensor.
     '''
 
     def on_battery_change(self, level: int):
         '''
-        Call this method to broadcast a new battery level for this sensor.
+        Call this method to broadcast a new battery level for this device/sensor.
         '''
         message = {
             'value': level,
--- controllers/node/node_controller/devices/node.py
+++ controllers/node/node_controller/devices/node.py
@@ -1,11 +1,13 @@
 from node_controller.pijuice import PiJuiceInterface
 from powerpi_common.config import Config
 from powerpi_common.device import Device
+from powerpi_common.device.mixin import PollableMixin
 from powerpi_common.logger import Logger
 from powerpi_common.mqtt import MQTTClient
+from powerpi_common.sensor.mixin import BatteryMixin
 
 
-class NodeDevice(Device):
+class NodeDevice(Device, PollableMixin, BatteryMixin):
     def __init__(
         self,
         config: Config,
@@ -15,8 +17,12 @@ class NodeDevice(Device):
         **kwargs
     ):
         Device.__init__(self, config, logger, mqtt_client, **kwargs)
+        PollableMixin.__init__(self, config, **kwargs)
 
         self.__pijuice = pijuice
+
+    async def poll(self):
+        self.on_battery_change(21)
 
     async def _turn_on(self):
         raise NotImplementedError
