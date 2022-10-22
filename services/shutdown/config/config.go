b06dfb6b4a61a746892d89fe7f5925f0f81a2fcd
TWilkin <TWilkin@users.noreply.github.com>
2022-10-22 15:18:32 +0100

    #107 Setup to receive the message

--- services/shutdown/config/config.go
+++ services/shutdown/config/config.go
@@ -5,17 +5,32 @@ import (
 	"os"
 )
 
+func Hostname() (hostname string) {
+	hostname, err := os.Hostname()
+
+	if err != nil {
+		panic(err)
+	}
+
+	return
+}
+
 func MqttAddress() (mqtt_address string) {
 	mqtt_address = os.Getenv("MQTT_ADDRESS")
 	return
 }
 
 func MqttClientId() (client_id string) {
-	hostname, err := os.Hostname()
-	if err != nil {
-		panic(err)
+	client_id = fmt.Sprintf("shutdown-%s", Hostname())
+	return
+}
+
+func MqttTopicBase() (topic_base string) {
+	topic_base = os.Getenv("TOPIC_BASE")
+
+	if topic_base == "" {
+		topic_base = "powerpi"
 	}
-	
-	client_id = fmt.Sprintf("shutdown-%s", hostname)
+
 	return
 }
--- services/shutdown/shutdown.go
+++ services/shutdown/shutdown.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/eclipse/paho.mqtt.golang"
 
@@ -11,15 +14,43 @@ import (
 func main() {
 	fmt.Println("PowerPi Shutdown Service")
 
-	// connect to MQTT
-	fmt.Printf("Connecting to MQTT at %s as %s", config.MqttAddress(), config.MqttClientId())
-	fmt.Println()
-	mqtt_options := mqtt.NewClientOptions().AddBroker(config.MqttAddress()).SetClientID(config.MqttClientId())
-	mqtt_client := mqtt.NewClient(mqtt_options)
+	// make the channel
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
 
-	if token := mqtt_client.Connect(); token.Wait() && token.Error() != nil {
+	// set the MQTT options
+	mqtt_options := mqtt.NewClientOptions()
+	mqtt_options.AddBroker(config.MqttAddress())
+	mqtt_options.SetClientID(config.MqttClientId())
+	mqtt_options.SetCleanSession(true)
+	mqtt_options.OnConnect = onConnect
+
+	// connect to MQTT
+	fmt.Printf("Connecting to MQTT at %s as %s\n", config.MqttAddress(), config.MqttClientId())
+	mqttClient := mqtt.NewClient(mqtt_options)
+
+	if token := mqttClient.Connect(); token.Wait() && token.Error() != nil {
 		panic(token.Error())
 	}
 
-	mqtt_client.Disconnect(250)
+	// join the channel
+	<-c
+
+	mqttClient.Disconnect(250)
+}
+
+func onConnect(mqttClient mqtt.Client) {
+	fmt.Println("Connected to MQTT")
+
+	// subscribe to the shutdown event for this device
+	topic := fmt.Sprintf("%s/device/%s/change", config.MqttTopicBase(), config.Hostname())
+	fmt.Printf("Subscribing to %s\n", topic)
+
+	if token := mqttClient.Subscribe(topic, 2, onMessageReceived); token.Wait() && token.Error() != nil {
+		panic(token.Error())
+	}
+}
+
+func onMessageReceived(mqttClient mqtt.Client, message mqtt.Message) {
+	fmt.Printf("Received %s: %s\n", message.Topic(), message.Payload())
 }
