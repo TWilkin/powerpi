363728b51bc5bfe91ab1f7515138a74783a06b92
TWilkin <TWilkin@users.noreply.github.com>
2022-10-23 10:45:09 +0100

    #107 Wrap the MQTT actions in a class

--- services/shutdown/main.go
+++ services/shutdown/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"encoding/json"
 	"flag"
 	"fmt"
 	"os"
@@ -10,28 +9,17 @@ import (
 	"syscall"
 	"time"
 
-	"github.com/eclipse/paho.mqtt.golang"
+	"powerpi/shutdown/mqtt"
 )
-
-type DeviceState string
-const (
-	On DeviceState = "on"
-	Off = "off"
-)
-
-type DeviceMessage struct {
-	State DeviceState `json:"state"`
-	Timestamp int64 `json:"timestamp"`
-}
 
 
 func main() {
 	fmt.Println("PowerPi Shutdown Service")
 
 	// use command line args
-	mqttHost := flag.String("host", "localhost", "The hostname of the MQTT broker")
-	mqttPort := flag.Int("port", 1883, "The port number for the MQTT broker")
-	mqttTopicBase := flag.String("topic", "powerpi", "The topic base for the MQTT broker")
+	host := flag.String("host", "localhost", "The hostname of the MQTT broker")
+	port := flag.Int("port", 1883, "The port number for the MQTT broker")
+	topicBase := flag.String("topic", "powerpi", "The topic base for the MQTT broker")
 	mock := flag.Bool("mock", false, "Whether to actually shutdown or not")
 	flag.Parse()
 
@@ -44,94 +32,32 @@ func main() {
 	channel := make(chan os.Signal, 1)
 	signal.Notify(channel, os.Interrupt, syscall.SIGTERM)
 
-	// set the MQTT options
-	mqttAddress := fmt.Sprintf("tcp://%s:%d", *mqttHost, *mqttPort)
-	mqttClientId := fmt.Sprintf("shutdown-%s", hostname)
-	mqtt_options := mqtt.NewClientOptions()
-	mqtt_options.AddBroker(mqttAddress)
-	mqtt_options.SetClientID(mqttClientId)
-	mqtt_options.SetCleanSession(true)
-	mqtt_options.OnConnect = func(mqttClient mqtt.Client) { onConnect(mqttClient, hostname, *mqttTopicBase, *mock) }
-
 	// connect to MQTT
-	fmt.Printf("Connecting to MQTT at %s as %s\n", mqttAddress, mqttClientId)
-	mqttClient := mqtt.NewClient(mqtt_options)
-
-	if token := mqttClient.Connect(); token.Wait() && token.Error() != nil {
-		panic(token.Error())
-	}
+	callback := func(client mqtt.MqttClient, state mqtt.DeviceState) {
+		shutdown(client, state, *mock)
+	} 
+	client := mqtt.New(hostname, *topicBase, callback)
+	client.Connect(*host, *port)
 
 	// join the channel
 	<-channel
-
-	mqttClient.Disconnect(250)
 }
 
-func onConnect(mqttClient mqtt.Client, hostname string, topicBase string, mock bool) {
-	fmt.Println("Connected to MQTT")
+func shutdown(client mqtt.MqttClient, state mqtt.DeviceState, mock bool) {
+	fmt.Println("Initiating shutdown")
 
-	// publish that this device is now on
-	publishState(mqttClient, hostname, topicBase, On)
+	// publish the off message and wait to make sure it's sent
+	client.PublishState(mqtt.Off)
+	time.Sleep(time.Second)
 
-	// subscribe to the shutdown event for this device
-	topic := fmt.Sprintf("%s/device/%s/change", topicBase, hostname)
-	fmt.Printf("Subscribing to %s\n", topic)
-
-	callback := func(mqttClient mqtt.Client, message mqtt.Message) {
-		onMessageReceived(mqttClient, message, hostname, topicBase, mock)
-	}
-
-	if token := mqttClient.Subscribe(topic, 2, callback); token.Wait() && token.Error() != nil {
-		panic(token.Error())
-	}
-}
-
-func onMessageReceived(mqttClient mqtt.Client, message mqtt.Message, hostname string, topicBase string, mock bool) {
-	fmt.Printf("Received %s: %s\n", message.Topic(), message.Payload())
-
-	data := []byte(message.Payload())
-	var payload DeviceMessage
-	err := json.Unmarshal(data, &payload)
-	if err != nil {
-		fmt.Println("Could not decode JSON message")
-		return
-	}
-
-	// check if the message is old
-	twoMinsAgo := time.Now().Unix() - 2 * 60
-	if twoMinsAgo >= (payload.Timestamp / 1000) {
-		fmt.Println("Ignoring old message")
-		return
-	}
-
-	// if it's not old and an off command, shutdown
-	if payload.State == Off {
-		fmt.Println("Initiating shutdown")
-
-		if !mock {
-			err := exec.Command("shutdown").Run()
-			if err != nil {
-				fmt.Println("Failed to shutdown:", err)
-			}
+	// turn off the computer if we're not mocking
+	if !mock {
+		err := exec.Command("shutdown").Run()
+		if err != nil {
+			fmt.Println("Failed to shutdown:", err)
 		}
-
-		publishState(mqttClient, hostname, topicBase, Off)
-
-		os.Exit(0)
-	}
-}
-
-func publishState(mqttClient mqtt.Client, hostname string, topicBase string, state DeviceState) {
-	topic := fmt.Sprintf("%s/device/%s/status", topicBase, hostname)
-	message := &DeviceMessage{state, time.Now().Unix() * 1000}
-
-	payload, err := json.Marshal(message)
-	if err != nil {
-		fmt.Println("Could not encode JSON message")
-		return
 	}
 
-	fmt.Printf("Publishing %s: %s\n", topic, payload)
-
-	mqttClient.Publish(topic, 2, true, payload)
+	// nothing else to do so quit now
+	os.Exit(0)
 }
---
+++ services/shutdown/mqtt/mqtt.go
@@ -0,0 +1,116 @@
+package mqtt
+
+import (
+	"encoding/json"
+	"fmt"
+	"time"
+
+	MQTT "github.com/eclipse/paho.mqtt.golang"
+)
+
+type DeviceState string
+const (
+	On DeviceState = "on"
+	Off = "off"
+)
+
+type MqttMessageAction func(MqttClient, DeviceState)
+
+type MqttClient struct {
+	client MQTT.Client
+	hostname string
+	topicBase string
+	action MqttMessageAction
+}
+
+type DeviceMessage struct {
+	State DeviceState `json:"state"`
+	Timestamp int64 `json:"timestamp"`
+}
+
+func New(hostname string, topicBase string, action MqttMessageAction) MqttClient {
+	client := MqttClient{nil, hostname, topicBase, action}
+	return client
+}
+
+func (client MqttClient) Connect(host string, port int) {
+	address := fmt.Sprintf("tcp://%s:%d", host, port)
+	clientId := fmt.Sprintf("shutdown-%s", client.hostname)
+
+	options := MQTT.NewClientOptions()
+	options.AddBroker(address)
+	options.SetClientID(clientId)
+	options.SetCleanSession(true)
+
+	options.OnConnect = func(_ MQTT.Client) {
+		client.onConnect()
+	}
+
+	fmt.Printf("Connecting to MQTT at %s as %s\n", address, clientId)
+	client.client = MQTT.NewClient(options)
+
+	if token := client.client.Connect(); token.Wait() && token.Error() != nil {
+		panic(token.Error())
+	}
+}
+
+func (client MqttClient) PublishState(state DeviceState) {
+	topic := client.topic("status")
+	message := &DeviceMessage{state, time.Now().Unix() * 1000}
+
+	payload, err := json.Marshal(message)
+	if err != nil {
+		fmt.Println("Could not encode JSON message")
+		return
+	}
+
+	fmt.Printf("Publishing %s: %s\n", topic, payload)
+
+	client.client.Publish(topic, 2, true, payload)
+}
+
+func (client MqttClient) topic(action string) string {
+	return fmt.Sprintf("%s/device/%s/%s", client.topicBase, client.hostname, action)
+}
+
+
+func (client MqttClient) onConnect() {
+	fmt.Println("Connected to MQTT")
+
+	// publish that this device is now on
+	client.PublishState(On)
+
+	// subscribe to the shutdown event for this device
+	topic := client.topic("change")
+	fmt.Printf("Subscribing to %s\n", topic)
+
+	callback := func(_ MQTT.Client, message MQTT.Message) {
+		client.onMessageReceived(message)
+	}
+
+	if token := client.client.Subscribe(topic, 2, callback); token.Wait() && token.Error() != nil {
+		panic(token.Error())
+	}
+}
+
+func (client MqttClient) onMessageReceived(message MQTT.Message) {
+	fmt.Printf("Received %s: %s\n", message.Topic(), message.Payload())
+
+	data := []byte(message.Payload())
+	var payload DeviceMessage
+	err := json.Unmarshal(data, &payload)
+	if err != nil {
+		fmt.Println("Could not decode JSON message")
+		return
+	}
+
+	// check if the message is old
+	twoMinsAgo := time.Now().Unix() - 2 * 60
+	if twoMinsAgo >= (payload.Timestamp / 1000) {
+		fmt.Println("Ignoring old message")
+		return
+	}
+
+	// call the action
+	client.action(client, payload.State)
+}
