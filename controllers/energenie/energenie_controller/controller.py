b4ebbeb3ac1dd428f50802504609a80c669c2f4c
TWilkin <TWilkin@users.noreply.github.com>
2022-11-02 12:12:51 +0000

    #221 Bump energenie-controller to v0.1.10 and fix custom controller mains

--- controllers/energenie/energenie_controller/controller.py
+++ controllers/energenie/energenie_controller/controller.py
@@ -1,9 +1,11 @@
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from powerpi_common.config.config_retriever import ConfigRetriever
 from powerpi_common.controller import Controller as CommonController
 from powerpi_common.device import DeviceManager, DeviceStatusChecker
 from powerpi_common.event import EventManager
 from powerpi_common.logger import Logger
 from powerpi_common.mqtt import MQTTClient
+
 from energenie_controller.__version__ import __app_name__, __version__
 from energenie_controller.config import EnergenieConfig
 
@@ -18,11 +20,13 @@ class Controller(CommonController):
         device_manager: DeviceManager,
         event_manager: EventManager,
         mqtt_client: MQTTClient,
-        device_status_checker: DeviceStatusChecker
+        device_status_checker: DeviceStatusChecker,
+        scheduler: AsyncIOScheduler,
     ):
         CommonController.__init__(
             self, logger, config_retriever, device_manager,
             event_manager, mqtt_client, device_status_checker,
+            scheduler,
             __app_name__, __version__
         )
 
--- controllers/zigbee/zigbee_controller/controller.py
+++ controllers/zigbee/zigbee_controller/controller.py
@@ -1,9 +1,11 @@
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from powerpi_common.config.config_retriever import ConfigRetriever
 from powerpi_common.controller import Controller as CommonController
 from powerpi_common.device import DeviceManager, DeviceStatusChecker
 from powerpi_common.event import EventManager
 from powerpi_common.logger import Logger
 from powerpi_common.mqtt import MQTTClient
+
 from zigbee_controller.__version__ import __app_name__, __version__
 from zigbee_controller.config import ZigbeeConfig
 from zigbee_controller.device import ZigbeeController
@@ -20,11 +22,13 @@ class Controller(CommonController):
         event_manager: EventManager,
         mqtt_client: MQTTClient,
         device_status_checker: DeviceStatusChecker,
+        scheduler: AsyncIOScheduler,
         zigbee_controller: ZigbeeController
     ):
         CommonController.__init__(
             self, logger, config_retriever, device_manager,
             event_manager, mqtt_client, device_status_checker,
+            scheduler,
             __app_name__, __version__
         )
 
