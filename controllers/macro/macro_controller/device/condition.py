901cef7e4a29f89fd4a1ad9eede2f23ff94be79b
TWilkin <TWilkin@users.noreply.github.com>
2022-11-24 19:07:15 +0000

    #103 Repeat the condition check every interval until it passes or the timeout elapses

--- controllers/macro/macro_controller/device/condition.py
+++ controllers/macro/macro_controller/device/condition.py
@@ -1,3 +1,7 @@
+from asyncio import get_event_loop, sleep, wait_for
+from asyncio.exceptions import CancelledError as AsyncCancelledError
+from asyncio.exceptions import TimeoutError as AsyncTimeoutError
+from contextlib import suppress
 from typing import Union
 
 from powerpi_common.condition import (ConditionParser, Expression,
@@ -23,6 +27,8 @@ class ConditionDevice(Device, DeviceOrchestratorMixin, PollableMixin):
         device: str,
         on_condition: Union[Expression, None] = None,
         off_condition: Union[Expression, None] = None,
+        timeout=30,
+        interval=1,
         **kwargs
     ):
         Device.__init__(
@@ -37,6 +43,9 @@ class ConditionDevice(Device, DeviceOrchestratorMixin, PollableMixin):
 
         self.__on_condition = on_condition
         self.__off_condition = off_condition
+
+        self.__timeout = timeout
+        self.__interval = interval
 
     @property
     def device(self):
@@ -62,28 +71,51 @@ class ConditionDevice(Device, DeviceOrchestratorMixin, PollableMixin):
             self.device.turn_off()
 
     async def __check_condition(self, status: DeviceStatus):
-        try:
-            return self.__execute_parser(status)
-        except ParseException as ex:
-            self.__logger.exception(ex)
-
-        return False
-
-    def __execute_parser(self, status: DeviceStatus):
         condition = self.__on_condition if status == DeviceStatus.ON else self.__off_condition
-
         parser = ConditionParser(self.__variable_manager, {})
-        return parser.conditional_expression(condition)
+
+        # this will be set to True if the condition evaluates to true before the timeout
+        success = False
+
+        # use the condition to decide when to perform the action (if at all)
+        def attempt():
+            try:
+                return parser.conditional_expression(condition)
+            except ParseException as ex:
+                self.__logger.exception(ex)
+
+            return False
+
+        # repeat the condition check every interval seconds until the timeout seconds
+        # with early termination if the condition evaluates to true
+        async def repeat_condition_check():
+            nonlocal success
+
+            while True:
+                if attempt():
+                    success = True
+                    break
+
+                await sleep(self.__interval)
+
+        # schedule the condition check in a task and wait for it to pass or timeout
+        with suppress(AsyncCancelledError) and suppress(AsyncTimeoutError):
+            task = get_event_loop().create_task(repeat_condition_check)
+            await wait_for(task, self.__timeout)
+
+        return success
 
     def __validate(self):
         '''
         Try and run the conditions to verify they're valid
         '''
+        parser = ConditionParser(self.__variable_manager, {})
+
         try:
             if self.__on_condition is not None:
-                self.__execute_parser(DeviceStatus.ON)
+                parser.conditional_expression(self.__on_condition)
 
             if self.__off_condition is not None:
-                self.__execute_parser(DeviceStatus.OFF)
+                parser.conditional_expression(self.__off_condition)
         except ParseException as ex:
             self.log_exception(ex)
