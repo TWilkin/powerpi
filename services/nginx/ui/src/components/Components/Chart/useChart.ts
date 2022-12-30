ce32ec5dd5c5122eb4a9ec5dc20f9bd619080456
TWilkin <TWilkin@users.noreply.github.com>
2022-12-30 12:54:05 +0000

    Fix bug where previous scale would still appear when changing dataset

--- services/nginx/ui/src/components/Components/Chart/useChart.ts
+++ services/nginx/ui/src/components/Components/Chart/useChart.ts
@@ -5,50 +5,32 @@ import { getFormattedUnit, getFormattedValue } from "../FormattedValue";
 import useChartColours from "./useChartColours";
 import { Dataset } from "./useHistoryDatasets";
 
+type DatasetChartScale = {
+    [key: string]: {
+        [key: string]: unknown;
+        min?: number;
+        max?: number;
+    };
+};
+
 export default function useChart(datasets?: Dataset[]) {
     const { isLandscape } = useOrientation();
 
     const { textColour, lineColour, tooltipColour, lineColours } = useChartColours();
-
-    // add the time axis by default
-    const scales: {
-        [key: string]: {
-            [key: string]: unknown;
-            min?: number;
-            max?: number;
-        };
-    } = useMemo(
-        () => ({
-            time: {
-                axis: isLandscape ? "x" : "y",
-                type: "time",
-                time: {
-                    minUnit: "minute",
-                },
-                reverse: !isLandscape,
-                grid: {
-                    color: lineColour,
-                    borderColor: lineColour,
-                },
-                ticks: {
-                    color: textColour,
-                },
-            },
-        }),
-        [isLandscape, lineColour, textColour]
-    );
 
     // generate the chart options
     const options: ChartOptions<"line"> = useMemo(
         () => ({
             responsive: true,
             maintainAspectRatio: false,
+
             plugins: {
                 legend: {
                     labels: {
                         color: textColour,
                     },
                 },
+
                 tooltip: {
                     titleColor: textColour,
                     bodyColor: textColour,
@@ -67,55 +49,76 @@ export default function useChart(datasets?: Dataset[]) {
                     },
                 },
             },
-            scales: datasets?.reduce((scales, dataset, i) => {
-                const key = `${dataset.action}-${dataset.unit}`.toLowerCase();
-                const formattedUnit = dataset.unit ? getFormattedUnit(dataset.unit) : undefined;
 
-                if (!scales[key]) {
-                    scales[key] = {
-                        axis: isLandscape ? "y" : "x",
-                        title: {
-                            display: true,
-                            text: dataset.unit
-                                ? `${dataset.action} (${formattedUnit})`
-                                : dataset.action,
-                            color: textColour,
-                        },
-                        type: "linear" as const,
-                        position: isLandscape
-                            ? Object.keys(scales).length % 2 === 1
-                                ? ("left" as const)
-                                : ("right" as const)
-                            : "bottom",
-                        grid: {
-                            drawOnChartArea: i === 0,
-                            color: lineColour,
-                            borderColor: lineColour,
-                        },
-                        ticks: {
-                            includeBounds: false,
-                            color: textColour,
-                        },
-                    };
-                }
+            scales: {
+                // add the time axis by default
+                time: {
+                    axis: isLandscape ? "x" : "y",
+                    type: "time",
+                    time: {
+                        minUnit: "minute",
+                    },
+                    reverse: !isLandscape,
+                    grid: {
+                        color: lineColour,
+                        borderColor: lineColour,
+                    },
+                    ticks: {
+                        color: textColour,
+                    },
+                },
 
-                // ensure the min/max still applies with this dataset
-                const points = dataset.data.map((point) => point.value);
-                let min = Math.min(...points, Number.MAX_VALUE);
-                let max = Math.max(...points, Number.MIN_VALUE);
+                // plus the axis for each dataset
+                ...datasets?.reduce((scales, dataset, i) => {
+                    const key = `${dataset.action}-${dataset.unit}`.toLowerCase();
+                    const formattedUnit = dataset.unit ? getFormattedUnit(dataset.unit) : undefined;
 
-                // add a bit of padding to the range
-                const padding = max === min ? Math.max(max, min) : (max - min) / 5;
-                min -= padding / 5;
-                max += padding / 5;
+                    if (!scales[key]) {
+                        scales[key] = {
+                            axis: isLandscape ? "y" : "x",
+                            title: {
+                                display: true,
+                                text: dataset.unit
+                                    ? `${dataset.action} (${formattedUnit})`
+                                    : dataset.action,
+                                color: textColour,
+                            },
+                            type: "linear" as const,
+                            position: isLandscape
+                                ? Object.keys(scales).length % 2 === 0
+                                    ? ("left" as const)
+                                    : ("right" as const)
+                                : "bottom",
+                            grid: {
+                                drawOnChartArea: i === 0,
+                                color: lineColour,
+                                borderColor: lineColour,
+                            },
+                            ticks: {
+                                includeBounds: false,
+                                color: textColour,
+                            },
+                        };
+                    }
 
-                scales[key].min = Math.min(min, scales[key].min ?? max);
-                scales[key].max = Math.max(max, scales[key].max ?? min);
+                    // ensure the min/max still applies with this dataset
+                    const points = dataset.data.map((point) => point.value);
+                    let min = Math.min(...points, Number.MAX_VALUE);
+                    let max = Math.max(...points, Number.MIN_VALUE);
 
-                return scales;
-            }, scales),
+                    // add a bit of padding to the range
+                    const padding = max === min ? Math.max(max, min) : (max - min) / 5;
+                    min -= padding / 5;
+                    max += padding / 5;
+
+                    scales[key].min = Math.min(min, scales[key].min ?? max);
+                    scales[key].max = Math.max(max, scales[key].max ?? min);
+
+                    return scales;
+                }, {} as DatasetChartScale),
+            },
         }),
-        [datasets, isLandscape, lineColour, scales, textColour, tooltipColour]
+        [datasets, isLandscape, lineColour, textColour, tooltipColour]
     );
 
     // extract the data points
