957af0a0a4eb7bb5be709215311ee18738eeff64
TWilkin <TWilkin@users.noreply.github.com>
2023-01-03 19:09:40 +0000

    #250 Add Innr light boilerplate for additional state, polling and on/off

--- controllers/zigbee/zigbee_controller/device/innr/light.py
+++ controllers/zigbee/zigbee_controller/device/innr/light.py
@@ -1,5 +1,6 @@
 from powerpi_common.config import Config
-from powerpi_common.device import Device
+from powerpi_common.device import AdditionalStateDevice
+from powerpi_common.device.mixin import AdditionalState, PollableMixin
 from powerpi_common.logger import Logger
 from powerpi_common.mqtt import MQTTClient
 from zigbee_controller.device.zigbee_controller import ZigbeeController
@@ -7,7 +8,11 @@ from zigbee_controller.zigbee import ZigbeeMixin
 
 
 #pylint: disable=too-many-ancestors
-class InnrLight(Device, ZigbeeMixin):
+class InnrLight(AdditionalStateDevice, PollableMixin, ZigbeeMixin):
+    '''
+    Adds support for Innr Smart RGB bulb.
+    '''
+
     def __init__(
         self,
         config: Config,
@@ -16,7 +21,24 @@ class InnrLight(Device, ZigbeeMixin):
         controller: ZigbeeController,
         **kwargs
     ):
-        Device.__init__(
+        AdditionalStateDevice.__init__(
             self, config, logger, mqtt_client, **kwargs
         )
+        PollableMixin.__init__(self, config, **kwargs)
         ZigbeeMixin.__init__(self, controller, **kwargs)
+
+    async def poll(self):
+        pass
+
+    async def on_additional_state_change(self, new_additional_state: AdditionalState):
+        pass
+
+    def _additional_state_keys(self):
+        # TODO find a way to dynamically identify which are supported by the bulb
+        return ['brightness', 'temperature', 'hue', 'saturation']
+
+    async def _turn_on(self):
+        pass
+
+    async def _turn_off(self):
+        pass
