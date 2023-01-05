285221baa281f3b40f4abed2bc902264d9c48af8
TWilkin <TWilkin@users.noreply.github.com>
2023-01-05 19:00:04 +0000

    #250 Add a utility to convert a standardised value to whatever the device uses

--- common/python/powerpi_common/util/data.py
+++ common/python/powerpi_common/util/data.py
@@ -1,7 +1,60 @@
-from typing import Tuple
+from enum import Enum
+from typing import Callable, Dict, Tuple
 
 
 def restrict(value: int, value_range: Tuple[int, int]):
     min_value, max_value = value_range
 
     return max(min_value, min(max_value, value))
+
+
+class DataType(str, Enum):
+    BRIGHTNESS = 'brightness'
+    DURATION = 'duration'
+    HUE = 'hue'
+    SATURATION = 'saturation'
+    TEMPERATURE = 'temperature'
+
+
+class Standardiser:
+    '''
+    Standardiser provides a simple interface to convert from a local value to a standardised value
+    and vice versa.
+
+    The dictionary of converters should be formatted as follows:
+    {
+        DURATION: (
+            lambda value: value / 1000,
+            lambda value: value * 1000
+        )
+    }
+    In this example the local value is seconds and the standard value is milliseconds.
+    '''
+
+    def __init__(
+            self,
+            converters: Dict[
+                DataType,
+                Tuple[Callable[[int], int], Callable[[int], int]]
+            ]):
+        self.__converters = converters
+
+    def convert(self, data_type: DataType, value: int):
+        '''
+        Convert the value from the standard value to the internal value for the device.
+        '''
+
+        if data_type in self.__converters:
+            return self.__converters[data_type][0](value)
+
+        return value
+
+    def revert(self, data_type: DataType, value: int):
+        '''
+        Convert the value from the internal value to the standard value for the device.
+        '''
+
+        if data_type in self.__converters:
+            return self.__converters[data_type][1](value)
+
+        return value
