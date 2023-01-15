e47583f0d25a695682cf02ddcf01f61455c5a34f
TWilkin <TWilkin@users.noreply.github.com>
2023-01-15 16:53:06 +0000

    #256 Retrieve and write the battery % of the switch mini

--- controllers/zigbee/zigbee_controller/sensor/osram/switch_mini.py
+++ controllers/zigbee/zigbee_controller/sensor/osram/switch_mini.py
@@ -1,3 +1,4 @@
+from asyncio import ensure_future
 from enum import Enum
 from typing import Any, List
 
@@ -63,6 +64,12 @@ class OsramSwitchMiniSensor(Sensor, ZigbeeMixin, BatteryMixin):
     /event/NAME/press:{"button": "down", "type": "release"}
     '''
 
+    # the minimum expected voltage in mV
+    MIN_VOLTAGE = 2820
+
+    # the maximum expected voltage in mV
+    MAX_VOLTAGE = 3100
+
     #pylint: disable=too-many-arguments
     def __init__(
         self,
@@ -86,6 +93,12 @@ class OsramSwitchMiniSensor(Sensor, ZigbeeMixin, BatteryMixin):
         }
 
         self._broadcast('press', message)
+
+        # we want to get the updated battery
+        ensure_future(self._zigbee_device[1]
+                      .in_clusters[PowerConfigurationCluster.cluster_id]
+                      .read_attributes(['battery_voltage'])
+                      )
 
     def long_button_press_handler(self, button: Button, args: List[List[int]]):
         if len(args) == 1:
@@ -151,14 +164,37 @@ class OsramSwitchMiniSensor(Sensor, ZigbeeMixin, BatteryMixin):
         )
 
         # battery level
-        device[1].in_clusters[PowerConfigurationCluster.cluster_id].add_listener(
+        cluster: PowerConfigurationCluster = device[1] \
+            .in_clusters[PowerConfigurationCluster.cluster_id]
+        attribute = cluster.find_attribute('battery_voltage')
+        cluster.add_listener(
             ClusterAttributeListener(self.__on_attribute_updated)
         )
+        await cluster.configure_reporting(attribute.id, 1, 10, 1)
+
+        cluster: PowerConfigurationCluster = self._zigbee_device[
+            1].in_clusters[PowerConfigurationCluster.cluster_id]
+        await cluster.read_attributes(['battery_voltage'])
 
     def __on_attribute_updated(self, attribute_id: int, value: Any):
-        if attribute_id == 0x0021:
-            # battery level
-            self.on_battery_change(value)
+        device = self._zigbee_device
+        cluster: PowerConfigurationCluster = device[1] \
+            .in_clusters[PowerConfigurationCluster.cluster_id]
+        attribute = cluster.find_attribute('battery_voltage')
+
+        if attribute_id == attribute.id:
+            voltage = value * 100
+            self.log_info(f'Received battery value of {voltage}mV')
+
+            # ensure voltage is in the expected range
+            voltage = max(voltage, self.MIN_VOLTAGE)
+            voltage = min(voltage, self.MAX_VOLTAGE)
+
+            # convert to a percentage
+            multiplier = 100 / (self.MAX_VOLTAGE - self.MIN_VOLTAGE)
+            percentage = round((voltage - self.MIN_VOLTAGE) * multiplier)
+
+            self.on_battery_change(percentage)
 
     def __str__(self):
         return ZigbeeMixin.__str__(self)
