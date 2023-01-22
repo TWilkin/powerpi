42b3e7bea6796b703bdfd154a2be06ea84356b4b
TWilkin <TWilkin@users.noreply.github.com>
2023-01-22 15:55:12 +0000

    #250 Start writing the ZigbeeLight tests

--- common/pytest/powerpi_common_test/device/mixin/pollable.py
+++ common/pytest/powerpi_common_test/device/mixin/pollable.py
@@ -1,7 +1,9 @@
 from abc import ABC
+from unittest.mock import PropertyMock
 
 import pytest
-
+from powerpi_common.config import Config
+from powerpi_common.device.mixin import PollableMixin
 from pytest_mock import MockerFixture
 
 
@@ -22,3 +24,19 @@ class PollableMixinTestBase(ABC):
         subject = self.create_subject(mocker)
 
         assert subject.poll_frequency >= 10
+
+
+class PollableMixingTestBaseNew(ABC):
+    @pytest.mark.asyncio
+    async def test_poll_implemented(self, subject: PollableMixin):
+        await subject.poll()
+
+    def test_polling_enabled(self, subject: PollableMixin):
+        assert subject.polling_enabled is True
+
+    def test_poll_frequency(self, subject: PollableMixin):
+        assert subject.poll_frequency >= 10
+
+    @pytest.fixture(autouse=True)
+    def poll_frequency(self, powerpi_config: Config):
+        type(powerpi_config).poll_frequency = PropertyMock(return_value=120)
---
+++ controllers/zigbee/tests/device/test_zigbee_light.py
@@ -0,0 +1,53 @@
+from asyncio import Future
+from typing import List
+
+import pytest
+from powerpi_common_test.device import DeviceTestBaseNew
+from powerpi_common_test.device.mixin import (InitialisableMixinTestBaseNew,
+                                              PollableMixingTestBaseNew)
+from pytest_mock import MockerFixture
+from zigbee_controller.device.zigbee_light import ZigbeeLight
+from zigpy.zcl import Cluster
+from zigpy.zcl.foundation import Status
+
+
+class TestZigbeeeLight(DeviceTestBaseNew, InitialisableMixinTestBaseNew, PollableMixingTestBaseNew):
+    @pytest.fixture
+    def subject(
+        self,
+        powerpi_config,
+        powerpi_logger,
+        powerpi_mqtt_client,
+        zigbee_controller
+    ):
+        return ZigbeeLight(
+            powerpi_config, powerpi_logger, powerpi_mqtt_client, zigbee_controller,
+            ieee='00:00:00:00:00:00:00:00', nwk='0xAAAA',
+            name='Light'
+        )
+
+    @pytest.fixture(autouse=True)
+    def cluster(self, zigbee_in_cluster: Cluster, mocker: MockerFixture):
+        future = Future()
+        future.set_result({'status': Status.SUCCESS})
+        zigbee_in_cluster.command.return_value = future
+
+        async def read_attributes(attributes: List[str]):
+            return (dict(zip(attributes, [0 for _ in attributes])), None)
+
+        mocker.patch.object(
+            zigbee_in_cluster,
+            'read_attributes',
+            read_attributes
+        )
+
+        async def write_attributes(_):
+            pass
+
+        mocker.patch.object(
+            zigbee_in_cluster,
+            'write_attributes',
+            write_attributes
+        )
+
+        return zigbee_in_cluster
