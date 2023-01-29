c02e1dd9490a28a9c81d9fe4cdc609eff48d62c8
TWilkin <TWilkin@users.noreply.github.com>
2023-01-29 14:49:36 +0000

    #258 Move all the re-usable slider code into Slider

--- services/nginx/ui/src/components/Components/Controls/BrightnessSlider/BrightnessSlider.tsx
+++ services/nginx/ui/src/components/Components/Controls/BrightnessSlider/BrightnessSlider.tsx
@@ -1,5 +1,4 @@
 import { faMoon, faSun } from "@fortawesome/free-solid-svg-icons";
-import { MouseEvent, TouchEvent, useCallback } from "react";
 import AdditionalStateControlsProps from "../AdditionalStateControlProps";
 import Slider from "../Slider";
 
@@ -7,32 +6,18 @@ type BrightnessSliderProps = {
     brightness?: number;
 } & AdditionalStateControlsProps;
 
-const BrightnessSlider = ({ brightness = 65535, disabled, onChange }: BrightnessSliderProps) => {
-    const onBrightnessChange = useCallback(
-        (event: MouseEvent<HTMLInputElement> | TouchEvent<HTMLInputElement>) => {
-            event.preventDefault();
+const BrightnessSlider = ({ brightness = 65535, disabled, onChange }: BrightnessSliderProps) => (
+    <Slider
+        title="Set the brightness for this device"
+        lowIcon={faMoon}
+        highIcon={faSun}
+        additionalStateName="brightness"
+        value={brightness}
+        min={0}
+        max={65535}
+        disabled={disabled}
+        onChange={onChange}
+    />
+);
 
-            const target = event.target as HTMLInputElement;
-            const newBrightness = parseInt(target.value);
-            if (newBrightness !== brightness) {
-                onChange({ brightness: newBrightness });
-            }
-        },
-        [brightness, onChange]
-    );
-
-    return (
-        <Slider title="Set the brightness for this device" lowIcon={faMoon} highIcon={faSun}>
-            <input
-                type="range"
-                min={0}
-                max={65535}
-                defaultValue={brightness}
-                disabled={disabled}
-                onMouseOut={onBrightnessChange}
-                onTouchEnd={onBrightnessChange}
-            />
-        </Slider>
-    );
-};
 export default BrightnessSlider;
--- services/nginx/ui/src/components/Components/Controls/Slider/Slider.tsx
+++ services/nginx/ui/src/components/Components/Controls/Slider/Slider.tsx
@@ -1,21 +1,60 @@
 import { IconProp } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { PropsWithChildren } from "react";
+import { MouseEvent, TouchEvent, useCallback } from "react";
+import AdditionalStateControlsProps from "../AdditionalStateControlProps";
 import styles from "./Slider.module.scss";
 
-type SliderProps = PropsWithChildren<{
+type SliderProps = {
     title: string;
     lowIcon: IconProp;
     highIcon: IconProp;
-}>;
+    additionalStateName: string;
+    value: number;
+    min: number;
+    max: number;
+} & AdditionalStateControlsProps;
 
-const Slider = ({ title, lowIcon, highIcon, children }: SliderProps) => (
-    <div className={styles.container} title={title}>
-        <FontAwesomeIcon icon={lowIcon} />
+const Slider = ({
+    title,
+    lowIcon,
+    highIcon,
+    additionalStateName,
+    value,
+    min,
+    max,
+    disabled,
+    onChange,
+}: SliderProps) => {
+    const onValueSettled = useCallback(
+        (event: MouseEvent<HTMLInputElement> | TouchEvent<HTMLInputElement>) => {
+            event.preventDefault();
 
-        {children}
+            const target = event.target as HTMLInputElement;
+            const newValue = parseInt(target.value);
+            if (newValue !== value) {
+                const message = { [additionalStateName]: newValue };
+                onChange(message);
+            }
+        },
+        [additionalStateName, onChange, value]
+    );
 
-        <FontAwesomeIcon icon={highIcon} />
-    </div>
-);
+    return (
+        <div className={styles.container} title={title}>
+            <FontAwesomeIcon icon={lowIcon} />
+
+            <input
+                type="range"
+                min={min}
+                max={max}
+                defaultValue={value}
+                disabled={disabled}
+                onMouseOut={onValueSettled}
+                onTouchEnd={onValueSettled}
+            />
+
+            <FontAwesomeIcon icon={highIcon} />
+        </div>
+    );
+};
 export default Slider;
