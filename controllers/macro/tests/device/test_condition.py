abf9e172aec64f9b4bb3b4aab586a474bfd675a0
TWilkin <TWilkin@users.noreply.github.com>
2023-02-20 19:56:14 +0000

    #237 Upgrade tests for condition device to fixtures

--- controllers/macro/tests/device/test_condition.py
+++ controllers/macro/tests/device/test_condition.py
@@ -1,57 +1,44 @@
 from asyncio import Future
-from unittest.mock import PropertyMock
+from unittest.mock import MagicMock, Mock, PropertyMock
 
 import pytest
 from macro_controller.device.condition import ConditionDevice
 from powerpi_common.device import DeviceStatus
-from powerpi_common_test.device import DeviceTestBase
-from powerpi_common_test.device.mixin import (DeviceOrchestratorMixinTestBase,
-                                              PollableMixinTestBase)
+from powerpi_common_test.device import DeviceTestBaseNew
+from powerpi_common_test.device.mixin import (
+    DeviceOrchestratorMixinTestBaseNew, PollableMixingTestBaseNew)
 from pytest_mock import MockerFixture
 
 
-class TestCondition(DeviceTestBase, DeviceOrchestratorMixinTestBase, PollableMixinTestBase):
-    def get_subject(self, mocker: MockerFixture):
-        # pylint: disable=attribute-defined-outside-init
-        self.device_manager = mocker.Mock()
-        self.variable_manager = mocker.Mock()
+class TestCondition(
+    DeviceTestBaseNew,
+    DeviceOrchestratorMixinTestBaseNew,
+    PollableMixingTestBaseNew
+):
 
-        self.device = mocker.Mock()
-        future = Future()
-        future.set_result(None)
-        self.device.turn_on.return_value = future
-        self.device.turn_off.return_value = future
-
-        self.device_manager.get_device = lambda name: self.device if name == 'test_device' else None
-
-        return ConditionDevice(
-            self.config, self.logger, self.mqtt_client, self.device_manager, self.variable_manager,
-            name='condition', device='test_device',
-            on_condition={
-                'when': [{'equals': [{'var': 'device.on.state'}, 'on']}]
-            },
-            off_condition={
-                'when': [{'equals': [{'var': 'device.off.state'}, 'off']}]
-            },
-            timeout=0.3,
-            interval=0.1,
-            poll_frequency=60
-        )
-
-    async def test_initialise(self, mocker: MockerFixture):
-        subject = self.create_subject(mocker)
-
+    @pytest.mark.asyncio
+    async def test_initialise(
+        self,
+        subject: ConditionDevice,
+        variable_manager: Mock,
+        mocker: MockerFixture
+    ):
         await subject.initialise()
 
-        self.variable_manager.get_device.assert_has_calls([
+        variable_manager.get_device.assert_has_calls([
             mocker.call('on'),
             mocker.call('off')
         ])
 
+    @pytest.mark.asyncio
     @pytest.mark.parametrize('state', [DeviceStatus.ON, DeviceStatus.OFF])
-    async def test_poll_updates_state(self, mocker: MockerFixture, state: DeviceStatus):
-        subject = self.create_subject(mocker)
-
+    async def test_poll_updates_state(
+        self,
+        subject: ConditionDevice,
+        variable_manager: Mock,
+        mocker: MockerFixture,
+        state: DeviceStatus
+    ):
         on_device_variable = mocker.Mock()
         type(on_device_variable).state = PropertyMock(
             return_value=state
@@ -68,7 +55,7 @@ class TestCondition(DeviceTestBase, DeviceOrchestratorMixinTestBase, PollableMix
             if name == DeviceStatus.OFF:
                 return off_device_variable
             return None
-        self.variable_manager.get_device = get_variable
+        variable_manager.get_device = get_variable
 
         assert subject.state == DeviceStatus.UNKNOWN
 
@@ -76,19 +63,33 @@ class TestCondition(DeviceTestBase, DeviceOrchestratorMixinTestBase, PollableMix
 
         assert subject.state == state
 
-    async def test_poll_no_condition(self, subject: ConditionDevice):
+    @pytest.mark.asyncio
+    async def test_poll_no_condition(
+        self,
+        no_condition_subject: ConditionDevice,
+        variable_manager: Mock
+    ):
+        subject = no_condition_subject
+
         assert subject.state == DeviceStatus.UNKNOWN
 
         await subject.poll()
 
         assert subject.state == DeviceStatus.UNKNOWN
 
-        self.variable_manager.get_device.assert_not_called()
+        variable_manager.get_device.assert_not_called()
 
+    @pytest.mark.asyncio
     @pytest.mark.parametrize('state', [DeviceStatus.ON, DeviceStatus.OFF])
-    async def test_turn_x_condition_true(self, mocker: MockerFixture, state: DeviceStatus):
-        subject = self.create_subject(mocker)
-
+    async def test_turn_x_condition_true(
+        self,
+        subject: ConditionDevice,
+        device: MagicMock,
+        variable_manager: Mock,
+        mocker: MockerFixture,
+        state: DeviceStatus
+    ):
+        #pylint: disable=too-many-arguments
         opposite = DeviceStatus.OFF if state == DeviceStatus.ON else DeviceStatus.ON
 
         device_variable = mocker.Mock()
@@ -96,7 +97,7 @@ class TestCondition(DeviceTestBase, DeviceOrchestratorMixinTestBase, PollableMix
             side_effect=[opposite, state]
         )
 
-        self.variable_manager.get_device = lambda name: device_variable if name == state else None
+        variable_manager.get_device = lambda name: device_variable if name == state else None
 
         assert subject.state == DeviceStatus.UNKNOWN
 
@@ -108,16 +109,23 @@ class TestCondition(DeviceTestBase, DeviceOrchestratorMixinTestBase, PollableMix
         assert subject.state == state
 
         if state == DeviceStatus.ON:
-            self.device.turn_on.assert_called_once()
-            self.device.turn_off.assert_not_called()
+            device.turn_on.assert_called_once()
+            device.turn_off.assert_not_called()
         else:
-            self.device.turn_off.assert_called_once()
-            self.device.turn_on.assert_not_called()
+            device.turn_off.assert_called_once()
+            device.turn_on.assert_not_called()
 
+    @pytest.mark.asyncio
     @pytest.mark.parametrize('state', [DeviceStatus.ON, DeviceStatus.OFF])
-    async def test_turn_x_condition_false(self, mocker: MockerFixture, state: DeviceStatus):
-        subject = self.create_subject(mocker)
-
+    async def test_turn_x_condition_false(
+        self,
+        subject: ConditionDevice,
+        device: MagicMock,
+        variable_manager: Mock,
+        mocker: MockerFixture,
+        state: DeviceStatus
+    ):
+        #pylint: disable=too-many-arguments
         opposite = DeviceStatus.OFF if state == DeviceStatus.ON else DeviceStatus.ON
 
         device_variable = mocker.Mock()
@@ -125,7 +133,7 @@ class TestCondition(DeviceTestBase, DeviceOrchestratorMixinTestBase, PollableMix
             side_effect=[opposite, opposite, opposite]
         )
 
-        self.variable_manager.get_device = lambda name: device_variable if name == state else None
+        variable_manager.get_device = lambda name: device_variable if name == state else None
 
         assert subject.state == DeviceStatus.UNKNOWN
 
@@ -136,11 +144,20 @@ class TestCondition(DeviceTestBase, DeviceOrchestratorMixinTestBase, PollableMix
 
         assert subject.state == state
 
-        self.device.turn_on.assert_not_called()
-        self.device.turn_off.assert_not_called()
+        device.turn_on.assert_not_called()
+        device.turn_off.assert_not_called()
 
+    @pytest.mark.asyncio
     @pytest.mark.parametrize('state', [DeviceStatus.ON, DeviceStatus.OFF])
-    async def test_turn_x_condition_none(self, subject: ConditionDevice, state: DeviceStatus):
+    async def test_turn_x_condition_none(
+        self,
+        no_condition_subject: ConditionDevice,
+        device: MagicMock,
+        variable_manager: Mock,
+        state: DeviceStatus
+    ):
+        subject = no_condition_subject
+
         assert subject.state == DeviceStatus.UNKNOWN
 
         if state == DeviceStatus.ON:
@@ -150,21 +167,76 @@ class TestCondition(DeviceTestBase, DeviceOrchestratorMixinTestBase, PollableMix
 
         assert subject.state == state
 
-        self.variable_manager.get_device.assert_not_called()
+        variable_manager.get_device.assert_not_called()
 
         if state == DeviceStatus.ON:
-            self.device.turn_on.assert_called_once()
-            self.device.turn_off.assert_not_called()
+            device.turn_on.assert_called_once()
+            device.turn_off.assert_not_called()
         else:
-            self.device.turn_off.assert_called_once()
-            self.device.turn_on.assert_not_called()
+            device.turn_off.assert_called_once()
+            device.turn_on.assert_not_called()
 
     @pytest.fixture
-    def subject(self, mocker: MockerFixture):
-        self.create_subject(mocker)
-
+    def subject(
+        self,
+        powerpi_config,
+        powerpi_logger,
+        powerpi_mqtt_client,
+        device_manager,
+        variable_manager
+    ):
+        #pylint: disable=too-many-arguments
         return ConditionDevice(
-            self.config, self.logger, self.mqtt_client, self.device_manager, self.variable_manager,
-            name='condition', device='test_device',
+            powerpi_config, powerpi_logger, powerpi_mqtt_client, device_manager, variable_manager,
+            name='condition',
+            device='test_device',
+            on_condition={
+                'when': [{'equals': [{'var': 'device.on.state'}, 'on']}]
+            },
+            off_condition={
+                'when': [{'equals': [{'var': 'device.off.state'}, 'off']}]
+            },
+            timeout=0.3,
+            interval=0.1,
             poll_frequency=60
         )
+
+    @pytest.fixture
+    def no_condition_subject(
+        self,
+        powerpi_config,
+        powerpi_logger,
+        powerpi_mqtt_client,
+        device_manager,
+        variable_manager
+    ):
+        #pylint: disable=too-many-arguments
+        return ConditionDevice(
+            powerpi_config, powerpi_logger, powerpi_mqtt_client, device_manager, variable_manager,
+            name='condition',
+            device='test_device',
+            poll_frequency=60
+        )
+
+    @pytest.fixture
+    def device(self, mocker: MockerFixture):
+        device = mocker.MagicMock()
+
+        future = Future()
+        future.set_result(None)
+        device.turn_on.return_value = future
+        device.turn_off.return_value = future
+
+        return device
+
+    @pytest.fixture
+    def device_manager(self, device: MagicMock, mocker: MockerFixture):
+        manager = mocker.MagicMock()
+
+        manager.get_device = lambda name: device if name == 'test_device' else None
+
+        return manager
+
+    @pytest.fixture
+    def variable_manager(self, mocker: MockerFixture):
+        return mocker.Mock()
