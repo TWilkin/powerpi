2761643345faa176e8d49d6b6c51fb65a67661d3
TWilkin <TWilkin@users.noreply.github.com>
2023-02-20 18:23:06 +0000

    #237 Update simple macro device tests to use fixtures

--- controllers/macro/tests/device/test_delay.py
+++ controllers/macro/tests/device/test_delay.py
@@ -1,12 +1,19 @@
-from pytest_mock import MockerFixture
-
+import pytest
 from macro_controller.device import DelayDevice
-from powerpi_common_test.device import DeviceTestBase
+from powerpi_common_test.device import DeviceTestBaseNew
 
 
-class TestDelayDevice(DeviceTestBase):
-    def get_subject(self, _: MockerFixture):
+class TestDelayDevice(DeviceTestBaseNew):
+    @pytest.fixture
+    def subject(
+        self,
+        powerpi_config,
+        powerpi_logger,
+        powerpi_mqtt_client
+    ):
         return DelayDevice(
-            self.config, self.logger, self.mqtt_client, 0.1, 0.1,
+            powerpi_config, powerpi_logger, powerpi_mqtt_client,
+            start=0.1,
+            end=0.1,
             name='delay'
         )
