a1c37ea8582093d2e9dcccfa8200b954e6aee0de
TWilkin <TWilkin@users.noreply.github.com>
2023-02-20 21:08:29 +0000

    #265 Add test cases for capability in DeviceOrchestratorMixin

--- common/python/tests/device/mixin/test_orchestrator.py
+++ common/python/tests/device/mixin/test_orchestrator.py
@@ -1,9 +1,10 @@
-from typing import Dict, List
+from typing import Dict, List, Union
 
 import pytest
 from powerpi_common.device import Device, DeviceStatus
 from powerpi_common.device.mixin import DeviceOrchestratorMixin
 from powerpi_common.mqtt.consumer import MQTTConsumer
+from powerpi_common.util.data import Range
 from powerpi_common_test.device.mixin import DeviceOrchestratorMixinTestBaseNew
 from pytest_mock import MockerFixture
 
@@ -62,6 +63,130 @@ class TestDeviceOrchestratorMixin(DeviceOrchestratorMixinTestBaseNew):
 
         assert subject.current_device == devices[2]
         assert subject.state == state
+
+    @pytest.mark.asyncio
+    @pytest.mark.parametrize('brightness', [True, False])
+    @pytest.mark.parametrize('temperature', [Range(1000, 2000), False])
+    @pytest.mark.parametrize('colour', [True, False])
+    async def test_on_referenced_device_capability_called(
+        self,
+        subject: DeviceImpl,
+        devices: List[str],
+        brightness: bool,
+        temperature: Union[Range, bool],
+        colour: bool
+    ):
+        #pylint: disable=too-many-arguments
+
+        assert subject.supports_brightness is False
+        assert subject.supports_colour_temperature is False
+        assert subject.supports_colour_hue_and_saturation is False
+
+        await subject.initialise()
+
+        message = {
+            'timestamp': 0
+        }
+
+        if brightness:
+            message['brightness'] = True
+
+        if temperature or colour:
+            message['colour'] = {}
+
+            if temperature:
+                message['colour']['temperature'] = {
+                    'min': 1000,
+                    'max': 2000,
+                }
+
+            if colour:
+                message['colour']['hue'] = True
+                message['colour']['saturation'] = True
+
+        await self.consumers['device/device0/capability'] \
+            .on_message(message, devices[0], 'capability')
+
+        assert subject.supports_brightness is brightness
+        assert subject.supports_colour_hue_and_saturation is colour
+
+        if temperature is False:
+            assert subject.supports_colour_temperature is False
+        else:
+            assert subject.supports_colour_temperature.min == temperature.min
+            assert subject.supports_colour_temperature.max == temperature.max
+
+        # now test an update
+        message = {'timestamp': 0}
+
+        await self.consumers['device/device0/capability'] \
+            .on_message(message, devices[0], 'capability')
+
+        assert subject.supports_brightness is False
+        assert subject.supports_colour_temperature is False
+        assert subject.supports_colour_hue_and_saturation is False
+
+    @pytest.mark.asyncio
+    async def test_on_referenced_device_capability_many_temperatures(
+        self,
+        subject: DeviceImpl,
+        devices: List[str],
+    ):
+        assert subject.supports_colour_temperature is False
+
+        await subject.initialise()
+
+        message = {
+            'timestamp': 0,
+            'colour': {
+                'temperature': {
+                    'min': 1000,
+                    'max': 2000
+                }
+            }
+        }
+
+        await self.consumers['device/device0/capability'] \
+            .on_message(message, devices[0], 'capability')
+
+        assert subject.supports_colour_temperature.min == 1000
+        assert subject.supports_colour_temperature.max == 2000
+
+        # now add another which should extend the range
+
+        message = {
+            'timestamp': 0,
+            'colour': {
+                'temperature': {
+                    'min': 999,
+                    'max': 2001
+                }
+            }
+        }
+
+        await self.consumers['device/device2/capability'] \
+            .on_message(message, devices[2], 'capability')
+
+        assert subject.supports_colour_temperature.min == 999
+        assert subject.supports_colour_temperature.max == 2001
+
+        # now add another which shouldn't change anything
+
+        message = {
+            'timestamp': 0,
+            'colour': {
+                'temperature': {
+                    'min': 1001,
+                    'max': 1999
+                }
+            }
+        }
+
+        await self.consumers['device/device1/capability'] \
+            .on_message(message, devices[1], 'capability')
+
+        assert subject.supports_colour_temperature.min == 999
+        assert subject.supports_colour_temperature.max == 2001
 
     @pytest.fixture
     def subject(
