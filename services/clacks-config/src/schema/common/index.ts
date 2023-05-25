0cb4ca0af16018ba4e9f085cf9e63ad01cf98692
TWilkin <TWilkin@users.noreply.github.com>
2023-05-25 17:51:50 +0100

    #253 Update clacks-config to take percentage for brightness and saturation

--- services/clacks-config/src/schema/common/index.ts
+++ services/clacks-config/src/schema/common/index.ts
@@ -2,6 +2,7 @@ import * as Condition from "./Condition.schema.json";
 import * as DeviceState from "./DeviceState.schema.json";
 import * as Identifier from "./Identifier.schema.json";
 import * as JsonPath from "./JsonPatch.schema.json";
+import * as Percentage from "./Percentage.schema.json";
 import * as Time from "./Time.schema.json";
 import * as WeekDay from "./WeekDay.schema.json";
 
@@ -11,6 +12,7 @@ export default function loadCommonSchema() {
         DeviceState,
         Identifier,
         JsonPath,
+        Percentage,
         Time,
         WeekDay,
     };
