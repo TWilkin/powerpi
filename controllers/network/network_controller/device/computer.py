69546ef3ace46da72b571a8a7e9f0470f3ba3990
TWilkin <TWilkin@users.noreply.github.com>
2023-06-04 09:49:55 +0100

    #91 Add turn_on and poll to ComputerDevice

--- controllers/network/network_controller/device/computer.py
+++ controllers/network/network_controller/device/computer.py
@@ -1,10 +1,15 @@
+from icmplib import async_ping
 from powerpi_common.config import Config
-from powerpi_common.device import Device
+from powerpi_common.device import Device, DeviceStatus
+from powerpi_common.device.mixin import PollableMixin
 from powerpi_common.logger import Logger
 from powerpi_common.mqtt import MQTTClient
+from wakeonlan import send_magic_packet
 
 
-class ComputerDevice(Device):
+class ComputerDevice(Device, PollableMixin):
+    # pylint: disable=too-many-ancestors
+
     '''
     Device for controlling networked computers.
     '''
@@ -21,8 +26,9 @@ class ComputerDevice(Device):
     ):
         # pylint: disable=too-many-arguments
         Device.__init__(self, config, logger, mqtt_client, **kwargs)
+        PollableMixin.__init__(self, config, **kwargs)
 
-        self.__mac_address = mac
+        self.__mac_address = mac.replace(':', '.')
         self.__network_address = ip if ip is not None else hostname
 
     @property
@@ -33,8 +39,23 @@ class ComputerDevice(Device):
     def network_address(self):
         return self.__network_address
 
+    async def poll(self):
+        result = await async_ping(
+            self.__network_address,
+            count=4, interval=0.2, timeout=2, privileged=False
+        )
+
+        new_state = DeviceStatus.ON if result.is_alive else DeviceStatus.OFF
+
+        if new_state != self.state:
+            self.state = new_state
+
     async def _turn_on(self):
-        pass
+        send_magic_packet(
+            self.__mac_address,
+            ip_address=self.__network_address
+        )
 
     async def _turn_off(self):
+        # do nothing as this will be handled by the shutdown service running on that computer
         pass
