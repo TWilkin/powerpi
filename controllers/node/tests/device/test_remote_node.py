b0a7a42fda8a64efe8c49ae43cca00b1f1d65dbf
TWilkin <TWilkin@users.noreply.github.com>
2023-06-04 22:43:01 +0100

    #297 Fix and upgrade test for RemoteNodeDevice

--- controllers/node/tests/device/test_remote_node.py
+++ controllers/node/tests/device/test_remote_node.py
@@ -1,37 +1,41 @@
 from datetime import datetime
-from typing import Tuple
 from unittest.mock import PropertyMock
 
 import pytest
-from node_controller.device.remote_node import RemoteNodeDevice
 from powerpi_common.device import DeviceStatus
-from powerpi_common_test.device import DeviceTestBase
-from powerpi_common_test.device.mixin import PollableMixinTestBase
+from powerpi_common_test.device import DeviceTestBaseNew
+from powerpi_common_test.device.mixin import PollableMixinTestBaseNew
 from pytest_mock import MockerFixture
 
+from node_controller.device.remote_node import RemoteNodeDevice
 
-class TestRemoteNodeDevice(DeviceTestBase, PollableMixinTestBase):
-    def get_subject(self, _: MockerFixture):
-        self.ip = '127.0.0.1'
 
-        return RemoteNodeDevice(
-            self.config,
-            self.logger,
-            self.mqtt_client,
-            self.ip,
-            name='remote',
-            poll_frequency=60
+class TestRemoteNodeDevice(DeviceTestBaseNew, PollableMixinTestBaseNew):
+    @pytest.mark.asyncio
+    async def test_poll_implemented(self, subject: RemoteNodeDevice, mocker: MockerFixture):
+        # pylint: disable=arguments-differ
+
+        host = mocker.MagicMock()
+        type(host).is_alive = PropertyMock(return_value=True)
+
+        mocker.patch(
+            'node_controller.device.remote_node.async_ping',
+            return_value=host
         )
 
-    @pytest.mark.parametrize('states', [
+        await subject.poll()
+
+    @pytest.mark.parametrize('is_alive,state', [
         (False, DeviceStatus.OFF),
         (True, DeviceStatus.ON)
     ])
-    async def test_poll_ping(self, mocker: MockerFixture, states: Tuple[bool, str]):
-        is_alive, state = states
-
-        subject = self.create_subject(mocker)
-
+    async def test_poll_ping(
+        self,
+        subject: RemoteNodeDevice,
+        mocker: MockerFixture,
+        is_alive: bool,
+        state: DeviceStatus
+    ):
         assert subject.state == DeviceStatus.UNKNOWN
 
         host = mocker.Mock()
@@ -47,9 +51,7 @@ class TestRemoteNodeDevice(DeviceTestBase, PollableMixinTestBase):
         assert subject.state == state
 
     @pytest.mark.asyncio
-    async def test_turn_on(self, mocker: MockerFixture):
-        subject = self.create_subject(mocker)
-
+    async def test_turn_on(self, subject: RemoteNodeDevice):
         assert subject.state == DeviceStatus.UNKNOWN
 
         await subject.turn_on()
@@ -57,9 +59,7 @@ class TestRemoteNodeDevice(DeviceTestBase, PollableMixinTestBase):
         assert subject.state == DeviceStatus.UNKNOWN
 
     @pytest.mark.asyncio
-    async def test_turn_off(self, mocker: MockerFixture):
-        subject = self.create_subject(mocker)
-
+    async def test_turn_off(self, subject: RemoteNodeDevice):
         assert subject.state == DeviceStatus.UNKNOWN
 
         await subject.turn_off()
@@ -68,12 +68,18 @@ class TestRemoteNodeDevice(DeviceTestBase, PollableMixinTestBase):
 
     @pytest.mark.asyncio
     @pytest.mark.parametrize('times', [1, 2])
-    async def test_change_message(self, mocker: MockerFixture, times: int):
-        subject = self.create_subject(mocker)
+    async def test_change_message(
+        self,
+        subject: RemoteNodeDevice,
+        powerpi_config,
+        mocker: MockerFixture,
+        times: int
+    ):
+        # pylint: disable=arguments-differ
 
         assert subject.state == DeviceStatus.UNKNOWN
 
-        mocker.patch.object(self.config, 'message_age_cutoff', 120)
+        mocker.patch.object(powerpi_config, 'message_age_cutoff', 120)
 
         message = {
             'state': 'on',
@@ -84,3 +90,21 @@ class TestRemoteNodeDevice(DeviceTestBase, PollableMixinTestBase):
             await subject.on_message(message, subject.name, 'change')
 
             assert subject.state == DeviceStatus.UNKNOWN
+            assert subject.state == DeviceStatus.UNKNOWN
+            assert subject.state == DeviceStatus.UNKNOWN
+
+    @pytest.fixture
+    def subject(
+        self,
+        powerpi_config,
+        powerpi_logger,
+        powerpi_mqtt_client
+    ):
+        return RemoteNodeDevice(
+            powerpi_config,
+            powerpi_logger,
+            powerpi_mqtt_client,
+            '127.0.0.1',
+            name='remote',
+            poll_frequency=60
+        )
