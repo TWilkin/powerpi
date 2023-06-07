ab0d2faaa2463e3b0884893b40767f65c6a58273
TWilkin <TWilkin@users.noreply.github.com>
2023-06-07 22:36:34 +0100

    #237 Update HarmonyActivityDevice tests

--- controllers/harmony/tests/device/test_harmony_activity.py
+++ controllers/harmony/tests/device/test_harmony_activity.py
@@ -1,53 +1,31 @@
 from asyncio import Future
+from unittest.mock import MagicMock
 
+import pytest
+from powerpi_common.device import DeviceManager
+from powerpi_common_test.device import DeviceTestBaseNew
 from pytest_mock import MockerFixture
 
 from harmony_controller.device.harmony_activity import HarmonyActivityDevice
-from powerpi_common_test.device import DeviceTestBase
 
 
-class TestHarmonyActivityDevice(DeviceTestBase):
-    def get_subject(self, mocker: MockerFixture):
-        self.device_manager = mocker.Mock()
-        self.harmony_hub = mocker.Mock()
+class TestHarmonyActivityDevice(DeviceTestBaseNew):
 
-        mocker.patch.object(
-            self.device_manager,
-            'get_device',
-            return_value=self.harmony_hub
-        )
-
-        future = Future()
-        future.set_result(None)
-        for method in ['start_activity', 'turn_on', 'turn_off']:
-            mocker.patch.object(
-                self.harmony_hub,
-                method,
-                return_value=future
-            )
-
-        return HarmonyActivityDevice(
-            self.config, self.logger, self.mqtt_client, self.device_manager, 'hub', 'my activity',
-            name='testactivity'
-        )
-
-    async def test_turn_on_hub(self, mocker: MockerFixture):
-        subject = self.create_subject(mocker)
-
+    @pytest.mark.asyncio
+    async def test_turn_on_hub(self, subject: HarmonyActivityDevice, harmony_hub: MagicMock):
         assert subject.state == 'unknown'
 
         await subject.turn_on()
 
         assert subject.state == 'on'
 
-        self.harmony_hub.start_activity.assert_called_once_with('my activity')
+        harmony_hub.start_activity.assert_called_once_with('my activity')
 
-    async def test_turn_on_hub_error(self, mocker: MockerFixture):
-        subject = self.create_subject(mocker)
-
+    @pytest.mark.asyncio
+    async def test_turn_on_hub_error(self, subject: HarmonyActivityDevice, harmony_hub: MagicMock):
         async def start_activity(_: str):
-            raise Exception('error')
-        self.harmony_hub.start_activity = start_activity
+            raise KeyError('error')
+        harmony_hub.start_activity = start_activity
 
         assert subject.state == 'unknown'
 
@@ -55,26 +33,66 @@ class TestHarmonyActivityDevice(DeviceTestBase):
 
         assert subject.state == 'unknown'
 
-    async def test_turn_off_hub(self, mocker: MockerFixture):
-        subject = self.create_subject(mocker)
-
+    @pytest.mark.asyncio
+    async def test_turn_off_hub(self, subject: HarmonyActivityDevice, harmony_hub: MagicMock):
         assert subject.state == 'unknown'
 
         await subject.turn_off()
 
         assert subject.state == 'off'
 
-        self.harmony_hub.turn_off.assert_called_once()
+        harmony_hub.turn_off.assert_called_once()
 
-    async def test_turn_off_hub_error(self, mocker: MockerFixture):
-        subject = self.create_subject(mocker)
-
+    @pytest.mark.asyncio
+    async def test_turn_off_hub_error(self, subject: HarmonyActivityDevice, harmony_hub: MagicMock):
         async def turn_off(_: str):
-            raise Exception('error')
-        self.harmony_hub.turn_off = turn_off
+            raise KeyError('error')
+        harmony_hub.turn_off = turn_off
 
         assert subject.state == 'unknown'
 
         await subject.turn_off()
 
         assert subject.state == 'unknown'
+
+    @pytest.fixture
+    def subject(
+        self,
+        powerpi_config,
+        powerpi_logger,
+        powerpi_mqtt_client,
+        powerpi_device_manager: DeviceManager,
+        harmony_hub,
+        mocker: MockerFixture
+    ):
+        # pylint: disable=too-many-arguments
+        mocker.patch.object(
+            powerpi_device_manager,
+            'get_device',
+            return_value=harmony_hub
+        )
+
+        return HarmonyActivityDevice(
+            powerpi_config,
+            powerpi_logger,
+            powerpi_mqtt_client,
+            powerpi_device_manager,
+            'hub',
+            'my activity',
+            name='testactivity'
+        )
+
+    @pytest.fixture
+    def harmony_hub(self, mocker: MockerFixture):
+        hub = mocker.MagicMock()
+
+        future = Future()
+        future.set_result(None)
+        for method in ['start_activity', 'turn_on', 'turn_off']:
+            mocker.patch.object(
+                hub,
+                method,
+                return_value=future
+            )
+
+        return hub
