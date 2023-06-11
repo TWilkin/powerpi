623eaa399f39c91db4c4c1bdb532939f629fd0e1
TWilkin <TWilkin@users.noreply.github.com>
2023-06-11 14:24:31 +0100

    #300 Make including python parameterisable

--- .github/scripts/check_modified.sh
+++ .github/scripts/check_modified.sh
@@ -2,20 +2,26 @@
 
 include_project() {
     local project=$1
+    local type=$2
 
     echo "Enabling tests for $project"
     echo "$project=true" >> $GITHUB_OUTPUT
-    echo "python=true" >> $GITHUB_OUTPUT
+
+    if [[ $type -eq "python" ]]
+    then
+        echo "python=true" >> $GITHUB_OUTPUT
+    fi
 }
 
 check_file() {
     local file=$1
     local path=$2
     local project=$3
+    local type=$4
 
     if [[ $file == $path/* ]]
     then
-        include_project $project
+        include_project $project $type
     fi
 }
 
@@ -28,31 +34,31 @@ do
     # updating common python/pytest requires retesting everything using python
     if [[ $file == common/python/* || $file == common/pytest/* ]]
     then
-        include_project python_common
+        include_project "python"_common "python"
 
-        include_project energenie_controller
-        include_project harmony_controller
-        include_project lifx_controller
-        include_project macro_controller
-        include_project network_controller
-        include_project node_controller
-        include_project zigbee_controller
+        include_project energenie_controller "python"
+        include_project harmony_controller "python"
+        include_project lifx_controller "python"
+        include_project macro_controller "python"
+        include_project network_controller "python"
+        include_project node_controller "python"
+        include_project zigbee_controller "python"
 
-        include_project scheduler
+        include_project scheduler "python"
     fi
 
     # for now just make it happen
-    check_file $file ".github" "lifx_controller"
+    check_file $file ".github" "lifx_controller" "python"
 
     # check the controllers
-    check_file $file "controllers/energenie" "energenie_controller"
-    check_file $file "controllers/harmony" "harmony_controller"
-    check_file $file "controllers/lifx" "lifx_controller"
-    check_file $file "controllers/macro" "macro_controller"
-    check_file $file "controllers/network" "network_controller"
-    check_file $file "controllers/node" "node_controller"
-    check_file $file "controllers/zigbee" "zigbee_controller"
+    check_file $file "controllers/energenie" "energenie_controller" "python"
+    check_file $file "controllers/harmony" "harmony_controller" "python"
+    check_file $file "controllers/lifx" "lifx_controller" "python"
+    check_file $file "controllers/macro" "macro_controller" "python"
+    check_file $file "controllers/network" "network_controller" "python"
+    check_file $file "controllers/node" "node_controller" "python"
+    check_file $file "controllers/zigbee" "zigbee_controller" "python"
 
     # check the services
-    check_file $file "services/scheduler" "scheduler"
+    check_file $file "services/scheduler" "scheduler" "python"
 done < files.txt
