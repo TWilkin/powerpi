e94e409aa3887a231d6f807241b69a12674d3458
TWilkin <TWilkin@users.noreply.github.com>
2023-06-20 23:07:48 +0100

    Extra arguments stop macro-controller tests from running

--- .github/scripts/test_python.sh
+++ .github/scripts/test_python.sh
@@ -7,6 +7,6 @@ echo Testing $project
 # required to allow node-controller to build
 export PIJUICE_BUILD_BASE=1
 
-poetry install --only main,powerpi,test --no-root --no-interaction --no-ansi
+poetry install --only main,powerpi,test
 
 poetry run pytest
