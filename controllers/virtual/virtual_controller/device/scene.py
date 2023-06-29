d630c701a435b86316b679f48f26eaa444e47f77
TWilkin <TWilkin@users.noreply.github.com>
2023-06-29 18:59:22 +0100

    #360 Add the poll method to SceneDevice

--- controllers/virtual/virtual_controller/device/scene.py
+++ controllers/virtual/virtual_controller/device/scene.py
@@ -1,16 +1,17 @@
 from typing import Awaitable, Callable, List, Optional
 
 from powerpi_common.config import Config
-from powerpi_common.device import Device, DeviceManager
+from powerpi_common.device import Device, DeviceManager, DeviceStatus
 from powerpi_common.device.mixin import (AdditionalState, AdditionalStateMixin,
-                                         DeviceOrchestratorMixin)
+                                         DeviceOrchestratorMixin,
+                                         PollableMixin)
 from powerpi_common.device.scene_state import ReservedScenes
 from powerpi_common.logger import Logger
 from powerpi_common.mqtt import MQTTClient
 from powerpi_common.util import ismixin
 
 
-class SceneDevice(Device, DeviceOrchestratorMixin):
+class SceneDevice(Device, DeviceOrchestratorMixin, PollableMixin):
     # pylint: disable=too-many-ancestors
     '''
     A device for applying a scene, and additional state to the supplied device.
@@ -36,6 +37,7 @@ class SceneDevice(Device, DeviceOrchestratorMixin):
             self, config, logger, mqtt_client, device_manager, devices,
             capability=False
         )
+        PollableMixin.__init__(self, config, **kwargs)
 
         self.__state = state
         self.__scene = scene
@@ -50,6 +52,12 @@ class SceneDevice(Device, DeviceOrchestratorMixin):
     async def on_referenced_device_status(self, _, __):
         # we don't care if the device is on or off
         pass
+
+    async def poll(self):
+        if all(device.scene == self.scene for device in self.devices):
+            await self.set_new_state(DeviceStatus.ON)
+        else:
+            await self.set_new_state(DeviceStatus.OFF)
 
     async def _turn_on(self):
         await self.__apply(self.__start_scene)
