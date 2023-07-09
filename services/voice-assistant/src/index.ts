d5e0b092600d091a05c60700b0df9bdd1c001933
TWilkin <TWilkin@users.noreply.github.com>
2023-07-09 16:21:04 +0100

    #111 Add health endpoint to voice-assistant

--- services/voice-assistant/src/index.ts
+++ services/voice-assistant/src/index.ts
@@ -1,12 +1,15 @@
 import { ExpressJs, Request, Response, Webhook } from "@jovotech/server-express";
 import { LoggerService, PowerPiService } from "@powerpi/common";
+import HttpStatus from "http-status-codes";
 import app from "./app";
 import Container from "./container";
 import ConfigService from "./services/ConfigService";
+import HealthService from "./services/HealthService";
 
 async function start() {
     const config = Container.get(ConfigService);
     const logger = Container.get(LoggerService);
+    const health = Container.get(HealthService);
 
     await app.initialize();
 
@@ -22,6 +25,16 @@ async function start() {
     Webhook.post("/webhook_alexa", async (request: Request, response: Response) => {
         await app.handle(new ExpressJs(request, response));
     });
+
+    Webhook.get("/health", async (_, response: Response) => {
+        response.setHeader("Cache-Control", "no-cache, no-store, must-revalidate");
+
+        const status = health.execute();
+
+        response
+            .status(status ? HttpStatus.OK : HttpStatus.INTERNAL_SERVER_ERROR)
+            .send(status);
+    })
 }
 
 const service = Container.get(PowerPiService);
