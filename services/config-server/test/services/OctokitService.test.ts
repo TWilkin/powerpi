934f38cf973b328cfb8db39b3559ce12180dd408
TWilkin <TWilkin@users.noreply.github.com>
2023-07-13 22:41:32 +0100

    #373 Test without having to create dependencies

--- services/config-server/test/services/OctokitService.test.ts
+++ services/config-server/test/services/OctokitService.test.ts
@@ -1,8 +1,8 @@
 import octokit, { Octokit } from "@octokit/rest";
-import { FileService } from "@powerpi/common";
 import ConfigService from "../../src/services/ConfigService";
 import OctokitService, { NoUserError } from "../../src/services/OctokitService";
 
+jest.mock("../../src/services/ConfigService");
 jest.mock("@octokit/rest", () => {
     return {
         Octokit: jest.fn().mockImplementation(() => ({
@@ -17,33 +17,26 @@ jest.mock("@octokit/rest", () => {
     };
 });
 
-describe("OctokitService", () => {
-    let subject: OctokitService | undefined;
+const ConfigServiceMock = <jest.Mock<ConfigService>>ConfigService;
 
+describe("OctokitService", () => {
     beforeEach(() => {
         jest.mocked(Octokit).mockClear();
-
-        jest.spyOn(ConfigService.prototype, "gitHubUser", "get").mockReturnValue("user");
-        jest.spyOn(ConfigService.prototype, "repo", "get").mockReturnValue("repo");
-        jest.spyOn(ConfigService.prototype, "branch", "get").mockReturnValue("branch");
-        jest.spyOn(ConfigService.prototype, "path", "get").mockReturnValue("path/to");
-        jest.spyOn(ConfigService.prototype, "gitHubToken", "get").mockResolvedValue("token");
-
-        subject = new OctokitService(new ConfigService(new FileService()));
     });
 
     describe("getContent", () => {
         test("no user", () => {
-            jest.spyOn(ConfigService.prototype, "gitHubUser", "get").mockReturnValue(undefined);
+            const subject = createSubject(undefined);
 
-            const action = () => subject?.getContent();
+            const action = () => subject.getContent();
 
             expect(action).rejects.toThrow(NoUserError);
         });
 
         [undefined, "devices.json"].forEach((fileName) =>
-            test(`gets content(${fileName})`, async () => {
-                const result = await subject?.getContent(fileName);
+            test(`gets content '${fileName}'`, async () => {
+                const subject = createSubject("user");
+                const result = await subject.getContent(fileName);
 
                 expect(octokit.Octokit).toHaveBeenCalledTimes(1);
 
@@ -53,8 +46,10 @@ describe("OctokitService", () => {
         );
 
         test("no second login", async () => {
-            await subject?.getContent("devices.json");
-            await subject?.getContent("devices.json");
+            const subject = createSubject("user");
+
+            await subject.getContent("devices.json");
+            await subject.getContent("devices.json");
 
             expect(octokit.Octokit).toHaveBeenCalledTimes(1);
         });
@@ -62,17 +57,38 @@ describe("OctokitService", () => {
 
     describe("getUrl", () => {
         test("no user", () => {
-            jest.spyOn(ConfigService.prototype, "gitHubUser", "get").mockReturnValue(undefined);
+            const subject = createSubject(undefined);
 
             expect(subject?.getUrl()).toBe("github://unknown/repo/branch/path/to");
         });
 
-        test("with fileName", () =>
-            expect(subject?.getUrl("devices.json")).toBe(
-                "github://user/repo/branch/path/to/devices.json",
-            ));
+        test("with fileName", () => {
+            const subject = createSubject("user");
 
-        test("without fileName", () =>
-            expect(subject?.getUrl()).toBe("github://user/repo/branch/path/to"));
+            expect(subject.getUrl("devices.json")).toBe(
+                "github://user/repo/branch/path/to/devices.json",
+            );
+        });
+
+        test("without fileName", () => {
+            const subject = createSubject("user");
+
+            expect(subject.getUrl()).toBe("github://user/repo/branch/path/to");
+        });
     });
 });
+
+function createSubject(user: string | undefined) {
+    ConfigServiceMock.mockImplementation(
+        () =>
+            ({
+                gitHubUser: user,
+                repo: "repo",
+                branch: "branch",
+                path: "path/to",
+                gitHubToken: Promise.resolve("token"),
+            }) as ConfigService,
+    );
+
+    return new OctokitService(new ConfigServiceMock());
+}
