188433b466d1f39d9b7f8df34644c66089ffd81d
TWilkin <TWilkin@users.noreply.github.com>
2023-07-16 16:53:08 +0100

    #373 Add full test for GitHubConfigService

--- services/config-server/test/services/GitHubConfigService.test.ts
+++ services/config-server/test/services/GitHubConfigService.test.ts
@@ -1,5 +1,5 @@
-import { LoggerService } from "@powerpi/common";
-import { capture, instance, mock, resetCalls, when } from "ts-mockito";
+import { ConfigFileType, IDeviceConfigFile, LoggerService } from "@powerpi/common";
+import { anything, capture, instance, mock, resetCalls, verify, when } from "ts-mockito";
 import ConfigPublishService from "../../src/services/ConfigPublishService";
 import ConfigService from "../../src/services/ConfigService";
 import ConfigServiceArgumentService from "../../src/services/ConfigServiceArgumentService";
@@ -7,6 +7,9 @@ import GitHubConfigService from "../../src/services/GitHubConfigService";
 import OctokitService from "../../src/services/OctokitService";
 import ValidatorService from "../../src/services/ValidatorService";
 import HandlerFactory from "../../src/services/handlers/HandlerFactory";
+import IHandler from "../../src/services/handlers/IHandler";
+
+type OctokitContent = Awaited<ReturnType<OctokitService["getContent"]>>;
 
 const mockedConfigPublishService = mock<ConfigPublishService>();
 const mockedHandlerFactory = mock<HandlerFactory>();
@@ -15,18 +18,24 @@ const mockedOctokitService = mock<OctokitService>();
 const mockedConfigService = mock<ConfigService>();
 const mockedConfigServiceArgumentService = mock<ConfigServiceArgumentService>();
 const mockedLoggerService = mock<LoggerService>();
+const mockedHandler = mock<IHandler<IDeviceConfigFile>>();
 
 describe("GitHubConfigService", () => {
     let subject: GitHubConfigService | undefined;
 
     beforeEach(() => {
         jest.spyOn(process, "exit").mockImplementation(() => {
-            throw new Error("terminated");
+            throw new ProcessExitCalled();
         });
 
         when(mockedConfigServiceArgumentService.options).thenReturn({ daemon: false });
         when(mockedConfigService.pollFrequency).thenReturn(300);
         when(mockedOctokitService.getUrl()).thenReturn("github://");
+
+        resetCalls(mockedLoggerService);
+        resetCalls(mockedOctokitService);
+        resetCalls(mockedValidatorService);
+        resetCalls(mockedConfigPublishService);
 
         subject = new GitHubConfigService(
             instance(mockedConfigPublishService),
@@ -65,8 +74,230 @@ describe("GitHubConfigService", () => {
             test("off", async () => {
                 const action = subject?.start();
 
-                await expect(action).rejects.toThrow(Error);
+                await expect(action).rejects.toThrow(ProcessExitCalled);
+            });
+        });
+
+        describe("downloads files", () => {
+            test("no files", async () => {
+                when(mockedConfigService.configFileTypes).thenReturn([
+                    ConfigFileType.Devices,
+                    ConfigFileType.Events,
+                ]);
+
+                when(mockedOctokitService.getContent()).thenResolve([
+                    {
+                        name: "other.thing",
+                    },
+                ] as unknown as OctokitContent);
+
+                const action = subject?.start();
+
+                await expect(action).rejects.toThrow(ProcessExitCalled);
+
+                const logs = capture(mockedLoggerService.warn);
+
+                expect(logs.first()).toContainLogMessage("No file found for devices");
+                expect(logs.second()).toContainLogMessage("No file found for events");
+            });
+
+            test("error", async () => {
+                when(mockedConfigService.configFileTypes).thenReturn([ConfigFileType.Users]);
+
+                when(mockedOctokitService.getContent()).thenResolve([
+                    {
+                        name: "users.yml",
+                    },
+                ] as unknown as OctokitContent);
+
+                when(mockedOctokitService.getContent("users.yml")).thenReject(new Error("Nope"));
+
+                const action = subject?.start();
+
+                await expect(action).rejects.toThrow(ProcessExitCalled);
+
+                const logs = capture(mockedLoggerService.error);
+
+                expect(logs.second()).toContainLogMessage(
+                    "Could not retrieve users.yml from GitHub",
+                );
+            });
+
+            test("changes", async () => {
+                when(mockedConfigService.configFileTypes).thenReturn([
+                    ConfigFileType.Devices,
+                    ConfigFileType.Events,
+                    ConfigFileType.Users,
+                ]);
+
+                // devices.yaml is changed
+                when(mockedConfigService.getConfig(ConfigFileType.Devices)).thenReturn({
+                    data: {},
+                    checksum: "checksum_old_d",
+                });
+
+                // events.json is unchanged
+                when(mockedConfigService.getConfig(ConfigFileType.Events)).thenReturn({
+                    data: {},
+                    checksum: "checksum_e",
+                });
+
+                // users.yml is changed
+                when(mockedConfigService.getConfig(ConfigFileType.Users)).thenReturn({
+                    data: {},
+                    checksum: "checksum_old_u",
+                });
+
+                when(mockedOctokitService.getContent()).thenResolve([
+                    { name: "devices.yaml" },
+                    { name: "events.json" },
+                    { name: "users.yml" },
+                ] as unknown as OctokitContent);
+
+                when(mockedOctokitService.getContent("devices.yaml")).thenResolve({
+                    content: Buffer.from("devices:[]\nsensors:[]", "binary").toString("base64"),
+                    sha: "checksum_d",
+                } as unknown as OctokitContent);
+
+                when(mockedOctokitService.getContent("events.json")).thenResolve({
+                    content: Buffer.from('{"listeners": []}', "binary").toString("base64"),
+                    sha: "checksum_e",
+                } as unknown as OctokitContent);
+
+                when(mockedOctokitService.getContent("users.yml")).thenResolve({
+                    content: Buffer.from("users:[]", "binary").toString("base64"),
+                    sha: "checksum_u",
+                } as unknown as OctokitContent);
+
+                when(mockedValidatorService.validate(anything(), anything())).thenResolve(true);
+
+                when(mockedHandlerFactory.build(ConfigFileType.Devices)).thenReturn(
+                    instance(mockedHandler),
+                );
+
+                const action = subject?.start();
+
+                await expect(action).rejects.toThrow(ProcessExitCalled);
+
+                verify(mockedOctokitService.getContent("devices.yaml")).once();
+                verify(mockedOctokitService.getContent("events.json")).once();
+                verify(mockedOctokitService.getContent("users.yml")).once();
+
+                verify(mockedValidatorService.validate(ConfigFileType.Devices, anything())).once();
+                verify(mockedValidatorService.validate(ConfigFileType.Events, anything())).once();
+                verify(mockedValidatorService.validate(ConfigFileType.Users, anything())).once();
+
+                // only the device and user file was changed
+                verify(
+                    mockedConfigPublishService.publishConfigChange(
+                        ConfigFileType.Devices,
+                        anything(),
+                        "checksum_d",
+                    ),
+                ).once();
+                verify(
+                    mockedConfigPublishService.publishConfigChange(
+                        ConfigFileType.Events,
+                        anything(),
+                        "checksum_e",
+                    ),
+                ).never();
+                verify(
+                    mockedConfigPublishService.publishConfigChange(
+                        ConfigFileType.Users,
+                        anything(),
+                        "checksum_u",
+                    ),
+                ).once();
+
+                // we call the handler but only for device
+                verify(mockedHandlerFactory.build(anything())).twice();
+                verify(mockedHandler.handle(anything())).once();
+            });
+
+            test("changes, unseen file", async () => {
+                when(mockedConfigService.configFileTypes).thenReturn([ConfigFileType.Devices]);
+
+                // devices.yaml is changed
+                when(mockedConfigService.getConfig(ConfigFileType.Devices)).thenReturn(undefined);
+
+                when(mockedOctokitService.getContent()).thenResolve([
+                    { name: "devices.yaml" },
+                ] as unknown as OctokitContent);
+
+                when(mockedOctokitService.getContent("devices.yaml")).thenResolve({
+                    content: Buffer.from("devices:[]\nsensors:[]", "binary").toString("base64"),
+                    sha: "checksum_d",
+                } as unknown as OctokitContent);
+
+                when(mockedValidatorService.validate(anything(), anything())).thenResolve(true);
+
+                const action = subject?.start();
+
+                await expect(action).rejects.toThrow(ProcessExitCalled);
+
+                verify(mockedOctokitService.getContent("devices.yaml")).once();
+
+                verify(mockedValidatorService.validate(ConfigFileType.Devices, anything())).once();
+
+                verify(
+                    mockedConfigPublishService.publishConfigChange(
+                        ConfigFileType.Devices,
+                        anything(),
+                        "checksum_d",
+                    ),
+                ).once();
+            });
+        });
+
+        describe("validation", () => {
+            test("fails", async () => {
+                when(mockedConfigService.configFileTypes).thenReturn([ConfigFileType.Events]);
+
+                // events.json is changed
+                when(mockedConfigService.getConfig(ConfigFileType.Events)).thenReturn({
+                    data: {},
+                    checksum: "checksum_old_e",
+                });
+
+                when(mockedOctokitService.getContent()).thenResolve([
+                    { name: "events.json" },
+                ] as unknown as OctokitContent);
+
+                when(mockedOctokitService.getContent("events.json")).thenResolve({
+                    content: Buffer.from('{"listeners": []}', "binary").toString("base64"),
+                    sha: "checksum_e",
+                } as unknown as OctokitContent);
+
+                when(mockedValidatorService.validate(anything(), anything())).thenResolve(false);
+
+                const action = subject?.start();
+
+                await expect(action).rejects.toThrow(ProcessExitCalled);
+
+                verify(mockedOctokitService.getContent("events.json")).once();
+
+                verify(mockedValidatorService.validate(ConfigFileType.Events, anything())).once();
+
+                // validation so only errors are published
+                verify(
+                    mockedConfigPublishService.publishConfigChange(
+                        ConfigFileType.Events,
+                        anything(),
+                        "checksum_e",
+                    ),
+                ).never();
+
+                verify(
+                    mockedConfigPublishService.publishConfigError(
+                        ConfigFileType.Events,
+                        anything(),
+                        anything(),
+                    ),
+                ).once();
             });
         });
     });
 });
+
+class ProcessExitCalled extends Error {}
