445602892ea175a68cb98ee1e2fa81701a83365a
TWilkin <TWilkin@users.noreply.github.com>
2023-10-19 22:33:19 +0100

    #437 fix: Listeners should have abstract methods

--- controllers/snapcast/snapcast_controller/snapcast/listener/snapcast_client_listener.py
+++ controllers/snapcast/snapcast_controller/snapcast/listener/snapcast_client_listener.py
@@ -1,13 +1,19 @@
-from abc import ABC
+from abc import ABC, abstractmethod
 
 from snapcast_controller.snapcast.typing import Client
 
 
 class SnapcastClientListener(ABC):
+    '''
+    Listener for Snapcast client connect and disconnect events.
+    '''
+
+    @abstractmethod
     async def on_client_connect(self, client: Client):
         '''Override to be notified when a client connects to the Snapcast server.'''
         raise NotImplementedError
 
+    @abstractmethod
     async def on_client_disconnect(self, client: Client):
         '''Override to be notified when a client disconnects to the Snapcast server.'''
         raise NotImplementedError
