83f325305a536031b1b491466194da4922ea081b
TWilkin <TWilkin@users.noreply.github.com>
2023-10-22 13:53:39 +0100

    #437 feat: Use ReactSelect for the drop-down

--- services/ui/src/components/Components/Controls/StreamSelect/StreamSelect.tsx
+++ services/ui/src/components/Components/Controls/StreamSelect/StreamSelect.tsx
@@ -1,41 +1,68 @@
-import { ChangeEvent, useCallback, useEffect, useMemo, useState } from "react";
+import classNames from "classnames";
+import { useCallback, useEffect, useMemo, useState } from "react";
+import Select, { SingleValue } from "react-select";
 import _ from "underscore";
 import AdditionalStateControlsProps from "../AdditionalStateControlProps";
+import styles from "./StreamSelect.module.scss";
 
 type StreamSelectProps = {
     streams: string[];
     stream: string | undefined;
 } & AdditionalStateControlsProps;
 
+type Option = {
+    label: string;
+    value: string;
+};
+
 const StreamSelect = ({ streams, stream, disabled, onChange }: StreamSelectProps) => {
-    const [currentValue, setCurrentValue] = useState(stream);
+    const [currentValue, setCurrentValue] = useState<Option>();
 
     const onValueChange = useCallback(
-        (event: ChangeEvent<HTMLSelectElement>) => {
-            event.preventDefault();
-
-            const newValue = event.target.value;
-            if (newValue !== stream) {
-                const message = { stream: newValue };
+        (newValue: SingleValue<Option>) => {
+            if (newValue && newValue.value !== stream) {
+                const message = { stream: newValue.value };
                 onChange(message);
             }
         },
         [onChange, stream],
     );
 
-    useEffect(() => setCurrentValue(stream), [stream]);
+    const options: Option[] = useMemo(
+        () =>
+            _(streams)
+                .sortBy((stream) => stream)
+                .map((stream) => ({
+                    label: stream,
+                    value: stream,
+                })),
+        [streams],
+    );
 
-    const sortedStreams = useMemo(() => _(streams).sortBy((stream) => stream), [streams]);
+    useEffect(
+        () => setCurrentValue(options.find((option) => option.value === stream)),
+        [options, stream],
+    );
 
     return (
         <div title="Select the stream for this device to play">
-            <select disabled={disabled} onChange={onValueChange}>
-                {sortedStreams.map((stream) => (
-                    <option key={stream} selected={currentValue === stream}>
-                        {stream}
-                    </option>
-                ))}
-            </select>
+            <Select
+                isDisabled={disabled}
+                isSearchable
+                options={options}
+                onChange={onValueChange}
+                value={currentValue}
+                classNames={{
+                    container: () => styles.container,
+                    control: () => styles.control,
+                    input: () => styles.input,
+                    singleValue: () => styles.value,
+                    menu: () => styles.menu,
+                    option: (state) =>
+                        classNames(styles.option, { [styles.focus]: state.isFocused }),
+                    noOptionsMessage: () => styles.option,
+                }}
+            />
         </div>
     );
 };
