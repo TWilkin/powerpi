78492464e1282c76d54470bbccb8e637ece6b0d2
TWilkin <TWilkin@users.noreply.github.com>
2024-01-18 17:45:23 +0000

    #186 feat: Add the change endpoint to the DeviceController

--- services/api/src/controllers/DeviceController.ts
+++ services/api/src/controllers/DeviceController.ts
@@ -1,11 +1,28 @@
-import { Controller, Get } from "@tsed/common";
+import { MqttService, OutgoingMessage } from "@powerpi/common";
+import { DeviceChangeMessage } from "@powerpi/common-api";
+import { BodyParams, Controller, Get, PathParams, Post, Res } from "@tsed/common";
+import { Required } from "@tsed/schema";
+import { Response } from "express";
+import HttpStatus from "http-status-codes";
 import _ from "underscore";
 import Authorize from "../middleware/AuthorizeMiddleware";
 import DeviceStateService from "../services/DeviceStateService";
 
 @Controller("/device")
 export default class DeviceController {
-    constructor(private readonly deviceService: DeviceStateService) {}
+    private static readonly allowedChangeMessageKeys = [
+        "state",
+        "brightness",
+        "temperature",
+        "hue",
+        "saturation",
+        "stream",
+    ];
+
+    constructor(
+        private readonly deviceService: DeviceStateService,
+        private readonly mqttService: MqttService,
+    ) {}
 
     @Get("/")
     @Authorize()
@@ -13,5 +30,32 @@ export default class DeviceController {
         return _(this.deviceService.devices).sortBy((device) =>
             (device.display_name ?? device.name).toLocaleLowerCase(),
         );
+    }
+
+    @Post("/:device")
+    @Authorize()
+    change(
+        @PathParams("device") device: string,
+        @Required() @BodyParams() body: DeviceChangeMessage,
+        @Res() response: Response,
+    ) {
+        // fail if there is no message or it contains disallowed keys
+        if (
+            !body ||
+            _(Object.keys(body)).difference(DeviceController.allowedChangeMessageKeys).length > 0
+        ) {
+            response.sendStatus(HttpStatus.BAD_REQUEST);
+            return;
+        }
+
+        // generate the message
+        const message: OutgoingMessage = {
+            ...body,
+        };
+
+        // publish to MQTT
+        this.mqttService.publish("device", device, "change", message);
+
+        response.sendStatus(HttpStatus.CREATED);
     }
 }
