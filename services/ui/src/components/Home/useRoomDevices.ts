64eefbaf68012f2316e35e8b5b400652c9c96df4
TWilkin <TWilkin@users.noreply.github.com>
2024-04-25 23:17:25 +0100

    #108 refactor: Massively simplify useRoomDevice hook

--- services/ui/src/components/Home/useRoomDevices.ts
+++ services/ui/src/components/Home/useRoomDevices.ts
@@ -1,34 +1,41 @@
+import { BaseDevice } from "@powerpi/common-api";
 import { useMemo } from "react";
 import { chain as _ } from "underscore";
 import { useGetDevices } from "../../hooks/devices";
 import useGetSensors from "../../hooks/sensors";
+
+type DeviceList =
+    | {
+          deviceType: "device" | "sensor";
+          device: BaseDevice;
+      }[]
+    | undefined;
 
 export default function useRoomDevices(room: string) {
     const { devices } = useGetDevices();
     const { sensors } = useGetSensors();
 
     return useMemo(() => {
-        const deviceList = _(devices)
-            .filter((device) => device.location === room && device.visible)
-            .groupBy((device) => device.type)
-            .map((group) => ({
-                deviceType: "device",
-                type: group[0].type,
-                count: group.length,
-            }))
-            .value();
+        const deviceList: DeviceList = devices?.map((device) => ({ deviceType: "device", device }));
 
-        const sensorList = _(sensors)
-            .filter((sensor) => sensor.location === room && sensor.visible)
-            .groupBy((sensor) => sensor.type)
-            .map((group) => ({
-                deviceType: "sensor",
-                type: group[0].type,
-                count: group.length,
-            }))
-            .value();
+        const sensorList: DeviceList = sensors?.map((sensor) => ({
+            deviceType: "sensor",
+            device: sensor,
+        }));
 
-        return _(deviceList.concat(sensorList))
+        // get all the devices and sensors and group by type, and get the count
+        const all = _(deviceList)
+            .concat(sensorList ?? [])
+            .filter(({ device }) => device.location === room && device.visible)
+            .groupBy(({ deviceType, device }) => `${deviceType}_${device.type}`)
+            .map((group) => ({
+                deviceType: group[0].deviceType,
+                type: group[0].device.type,
+                count: group.length,
+            }));
+
+        // sort by count descending, then type alphabetically
+        return all
             .sortBy((device) => device.type)
             .reverse()
             .sortBy((device) => device.count)
