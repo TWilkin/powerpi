0a782a4baef9761e01c45cdc9802dc018a119237
TWilkin <TWilkin@users.noreply.github.com>
2024-05-26 10:04:54 +0100

    #119 feat: Add socket.io listener for config change

---
+++ common/node/common-api/src/ConfigStatus.ts
@@ -0,0 +1,8 @@
+export enum ConfigFileType {
+    Devices = "devices",
+    Floorplan = "floorplan",
+}
+
+export type ConfigStatusMessage = { type: ConfigFileType };
+
+export type ConfigStatusCallback = (message: ConfigStatusMessage) => void;
