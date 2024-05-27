4bd8065bef3d273e5e18cd4b31e6accf088c3d66
TWilkin <TWilkin@users.noreply.github.com>
2024-05-27 21:10:38 +0100

    #119 fix: Actually disconnect MQTT when force closing the API

--- services/api/src/app.ts
+++ services/api/src/app.ts
@@ -7,11 +7,14 @@ import Server from "./Server";
 async function bootstrap() {
     try {
         const platform = await PlatformExpress.bootstrap(Server);
-        await platform.listen();
 
-        process.on("SIGINT", () => {
+        process.on("SIGINT", async () => {
+            console.log("SIGINT");
             platform.stop();
+            await service.stop();
         });
+
+        await platform.listen();
     } catch (er) {
         $log.error(er);
     }
