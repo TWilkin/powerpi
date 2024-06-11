d22d81826ce438ad543cc2a53a8f97a96417e77e
TWilkin <TWilkin@users.noreply.github.com>
2024-06-11 22:13:19 +0100

    #119 feat: UserService also needs to respond to config changes

--- services/api/src/services/UserService.test.ts
+++ services/api/src/services/UserService.test.ts
@@ -1,14 +1,23 @@
-import { instance, mock, when } from "ts-mockito";
+import { ConfigFileType, ConfigRetrieverService } from "@powerpi/common";
+import { capture, instance, mock, resetCalls, when } from "ts-mockito";
 import ConfigService from "./ConfigService";
 import UserService from "./UserService";
 
 const mockedConfigService = mock<ConfigService>();
+const mockedConfigRetrieverService = mock<ConfigRetrieverService>();
 
 describe("UserService", () => {
     const user1 = { email: "user1@gmail.com", subject: "1" };
     const user2 = { email: "user2@gmail.com", subject: "2" };
 
     let subject: UserService | undefined;
+
+    function getConfigListener() {
+        const listeners = capture(mockedConfigRetrieverService.addListener);
+
+        const listener = listeners.byCallIndex(0);
+        return listener[1];
+    }
 
     beforeEach(() => {
         when(mockedConfigService.users).thenReturn({
@@ -18,7 +27,12 @@ describe("UserService", () => {
             ],
         });
 
-        subject = new UserService(instance(mockedConfigService));
+        resetCalls(mockedConfigRetrieverService);
+
+        subject = new UserService(
+            instance(mockedConfigService),
+            instance(mockedConfigRetrieverService),
+        );
 
         subject.$onInit();
     });
@@ -27,7 +41,10 @@ describe("UserService", () => {
         test("some", () => expect(subject?.users).toHaveLength(2));
 
         test("none", () => {
-            subject = new UserService(instance(mockedConfigService));
+            subject = new UserService(
+                instance(mockedConfigService),
+                instance(mockedConfigRetrieverService),
+            );
 
             expect(subject?.users).toHaveLength(0);
         });
@@ -51,5 +68,34 @@ describe("UserService", () => {
 
         result = subject?.popUser("1234");
         expect(result).toBeUndefined();
+    });
+
+    test("onConfigChange", () => {
+        const listener = getConfigListener();
+
+        when(mockedConfigService.users).thenReturn({
+            users: [
+                { email: "user1@gmail.com", role: "USER" },
+                { email: "user3@gmail.com", role: "USER" },
+            ],
+        });
+
+        const user1Code = "1234";
+        subject?.pushUser(user1Code, user1);
+
+        const user2Code = "4321";
+        subject?.pushUser(user2Code, user2);
+
+        listener.onConfigChange(ConfigFileType.Users);
+
+        expect(subject?.users).toHaveLength(2);
+        expect(subject?.users[0].email).toBe("user1@gmail.com");
+        expect(subject?.users[1].email).toBe("user3@gmail.com");
+
+        // this user is still present so is left alone
+        expect(subject?.popUser(user1Code)).toBe(user1);
+
+        // this user was removed so the code was removed
+        expect(subject?.popUser(user2Code)).toBeUndefined();
     });
 });
