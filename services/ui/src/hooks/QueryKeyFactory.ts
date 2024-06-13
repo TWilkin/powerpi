8cf13d873e9feaef565093af6390f0bf11d3a069
TWilkin <TWilkin@users.noreply.github.com>
2024-06-13 17:56:04 +0100

    #119 refactor: Make the QueryKeyFactory methods readonly

--- services/ui/src/hooks/QueryKeyFactory.ts
+++ services/ui/src/hooks/QueryKeyFactory.ts
@@ -1,27 +1,33 @@
 export default class QueryKeyFactory {
-    static base = () => ["powerpi"];
+    static readonly base = () => ["powerpi"];
 
-    static config = () => [...this.base(), "config"];
+    static readonly config = () => [...this.base(), "config"];
 
-    static devices = () => [...this.base(), "devices"];
+    static readonly devices = () => [...this.base(), "devices"];
 
-    static floorplan = () => [...this.base(), "floorplan"];
+    static readonly floorplan = () => [...this.base(), "floorplan"];
 
-    static history = () => HistoryQueryKeyFactory;
+    static readonly history = () => HistoryQueryKeyFactory;
 
-    static sensors = () => [...this.base(), "sensors"];
+    static readonly sensors = () => [...this.base(), "sensors"];
 }
 
 export class HistoryQueryKeyFactory {
-    static base = () => [...QueryKeyFactory.base(), "history"];
+    static readonly base = () => [...QueryKeyFactory.base(), "history"];
 
-    static actions = () => [...this.base(), "actions"];
+    static readonly actions = () => [...this.base(), "actions"];
 
-    static entities = () => [...this.base(), "entities"];
+    static readonly entities = () => [...this.base(), "entities"];
 
-    static types = () => [...this.base(), "types"];
+    static readonly types = () => [...this.base(), "types"];
 
-    static page = (type?: string, entity?: string, action?: string, start?: Date, end?: Date) => [
+    static readonly page = (
+        type?: string,
+        entity?: string,
+        action?: string,
+        start?: Date,
+        end?: Date,
+    ) => [
         ...this.base(),
         "page",
         [type, entity, action, start, end].filter((option) => option !== undefined),
