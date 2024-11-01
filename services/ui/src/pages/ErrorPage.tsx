4b0082a06fb33fa0426c430a141fa2534d293e78
TWilkin <TWilkin@users.noreply.github.com>
2024-11-01 19:45:30 +0000

    #541 feat: Add default error page

---
+++ services/ui/src/pages/ErrorPage.tsx
@@ -0,0 +1,42 @@
+import { isRouteErrorResponse, useNavigate, useRouteError } from "react-router-dom";
+
+function errorMessage(error: unknown) {
+    let status: number | undefined;
+    let message: string;
+
+    if (isRouteErrorResponse(error)) {
+        status = error.status;
+        message = `${error.status} ${error.statusText}`;
+    } else if (error instanceof Error) {
+        message = error.message;
+    } else if (typeof error === "string") {
+        message = error;
+    } else {
+        console.error(error);
+        message = "Unknown error";
+    }
+
+    return {
+        status,
+        message,
+    };
+}
+
+const ErrorPage = () => {
+    const navigate = useNavigate();
+
+    const error = useRouteError();
+    const { status, message } = errorMessage(error);
+
+    if (status === 401) {
+        navigate("/login");
+    }
+
+    return (
+        <div role="alert">
+            <h1>An unexpected error has occurred.</h1>
+            <p>{message}</p>
+        </div>
+    );
+};
+export default ErrorPage;
--- services/ui/src/router.tsx
+++ services/ui/src/router.tsx
@@ -1,5 +1,6 @@
 import { lazy } from "react";
 import { createBrowserRouter } from "react-router-dom";
+import ErrorPage from "./pages/ErrorPage";
 import Layout from "./pages/Layout";
 import { api, queryClient } from "./queries/client";
 import { configLoader } from "./queries/useConfig";
@@ -11,6 +12,7 @@ const router = createBrowserRouter([
     {
         path: "/",
         element: <Layout />,
+        errorElement: <ErrorPage />,
         loader: configLoader(queryClient, api),
         children: [
             {
