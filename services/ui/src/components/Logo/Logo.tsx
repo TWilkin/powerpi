e78b696dddcfcc835bd6a24a73874b0ad3c45a1f
TWilkin <TWilkin@users.noreply.github.com>
2024-11-05 17:36:10 +0000

    #541 refactor: Add writing mode as tailwind utility

--- services/ui/src/components/Logo/Logo.tsx
+++ services/ui/src/components/Logo/Logo.tsx
@@ -1,7 +1,6 @@
 import { useIsFetching } from "@tanstack/react-query";
 import RouteBuilder from "../../routing/RouteBuilder";
 import Icon from "../Icon";
-import "./Logo.css";
 
 const Logo = () => {
     const fetching = useIsFetching();
@@ -9,7 +8,7 @@ const Logo = () => {
     return (
         <a
             href={RouteBuilder.build()}
-            className="flex flex-row gap-1 items-center py-2 bg-black text-white text-xs rotate-180 logo"
+            className="flex flex-row gap-1 items-center py-2 bg-black text-white text-xs rotate-180 vertical-writing-lr"
         >
             <Icon icon="logo" spin={fetching > 0} className="rotate-90" />
             PowerPi
