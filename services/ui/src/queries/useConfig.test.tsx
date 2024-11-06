b106df1eb06288ddcdaff78cc4f0f0e343bb74cd
TWilkin <TWilkin@users.noreply.github.com>
2024-11-06 21:45:49 +0000

    #541 tests: Fix useConfig/useDevice test after Suspense change

--- services/ui/src/queries/useConfig.test.tsx
+++ services/ui/src/queries/useConfig.test.tsx
@@ -1,6 +1,7 @@
 import { Config, PowerPiApi } from "@powerpi/common-api";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { renderHook, waitFor } from "@testing-library/react";
+import { PropsWithChildren, Suspense } from "react";
 import { vi } from "vitest";
 import useConfig, { configLoader } from "./useConfig";
 
@@ -11,6 +12,15 @@ const mocks = vi.hoisted(() => ({
 }));
 
 vi.mock("./useAPI", () => ({ default: () => mocks.api }));
+
+const Wrapper = ({ children }: PropsWithChildren<unknown>) => {
+    const queryClient = new QueryClient();
+    return (
+        <QueryClientProvider client={queryClient}>
+            <Suspense>{children}</Suspense>
+        </QueryClientProvider>
+    );
+};
 
 const data: Config = {
     hasDevices: true,
@@ -25,8 +35,7 @@ describe("configLoader", () =>
 
         const loader = configLoader(new QueryClient(), mocks.api as unknown as PowerPiApi);
 
-        const result = await loader();
-
+        const result = await loader().data.data;
         expect(result).toBe(data);
     }));
 
@@ -35,7 +44,7 @@ describe("useConfig", () =>
         mocks.api.getConfig.mockImplementation(async () => data);
 
         const { result } = renderHook(useConfig, {
-            wrapper: (props) => QueryClientProvider({ ...props, client: new QueryClient() }),
+            wrapper: Wrapper,
         });
 
         await waitFor(() => expect(result.current.data).toBe(data));
--- services/ui/src/queries/useDevices.test.tsx
+++ services/ui/src/queries/useDevices.test.tsx
@@ -1,6 +1,7 @@
 import { Device, DeviceState, PowerPiApi } from "@powerpi/common-api";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { renderHook, waitFor } from "@testing-library/react";
+import { PropsWithChildren, Suspense } from "react";
 import { vi } from "vitest";
 import useDevices, { devicesLoader } from "./useDevices";
 
@@ -11,6 +12,15 @@ const mocks = vi.hoisted(() => ({
 }));
 
 vi.mock("./useAPI", () => ({ default: () => mocks.api }));
+
+const Wrapper = ({ children }: PropsWithChildren<unknown>) => {
+    const queryClient = new QueryClient();
+    return (
+        <QueryClientProvider client={queryClient}>
+            <Suspense>{children}</Suspense>
+        </QueryClientProvider>
+    );
+};
 
 const data: Device[] = [
     {
@@ -29,8 +39,7 @@ describe("devicesLoader", () =>
 
         const loader = devicesLoader(new QueryClient(), mocks.api as unknown as PowerPiApi);
 
-        const result = await loader();
-
+        const result = await loader().data.data;
         expect(result).toBe(data);
     }));
 
@@ -39,8 +48,11 @@ describe("useDevices", () =>
         mocks.api.getDevices.mockImplementation(async () => data);
 
         const { result } = renderHook(useDevices, {
-            wrapper: (props) => QueryClientProvider({ ...props, client: new QueryClient() }),
+            wrapper: Wrapper,
         });
 
-        await waitFor(() => expect(result.current.data).toBe(data));
+        await waitFor(() => {
+            console.log(result.current);
+            expect(result.current?.data).toBe(data);
+        });
     }));
