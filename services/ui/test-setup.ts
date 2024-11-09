7447fb942edbe986ec447611cafe73fe3bd42050
TWilkin <TWilkin@users.noreply.github.com>
2024-11-09 08:50:29 +0000

    #541 tests: Fix tests not finding translations

--- services/ui/test-setup.ts
+++ services/ui/test-setup.ts
@@ -1,9 +1,24 @@
 import * as matchers from "@testing-library/jest-dom/matchers";
 import { cleanup } from "@testing-library/react";
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
 import { afterEach, expect } from "vitest";
+import translation from "./public/locales/en-GB/translation.json";
 
 expect.extend(matchers);
 
-afterEach(() => {
-    cleanup();
+beforeAll(() => {
+    // setup i18next with en-GB translation
+    i18n.use(initReactI18next).init({
+        lng: "en-GB",
+        debug: false,
+        resources: {
+            "en-GB": { translation },
+        },
+        interpolation: {
+            escapeValue: false,
+        },
+    });
 });
+
+afterEach(() => cleanup());
