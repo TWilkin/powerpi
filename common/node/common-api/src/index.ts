5c1d0eafa97f20006ee1699c9aeaca7573c34a8a
TWilkin <TWilkin@users.noreply.github.com>
2024-11-11 17:20:44 +0000

    #541 fix: Actually export ConfigFileType

--- common/node/common-api/src/index.ts
+++ common/node/common-api/src/index.ts
@@ -5,7 +5,7 @@ import { BatteryStatusMessage } from "./BatteryStatus";
 import Capability from "./Capability";
 import { CapabilityStatusCallback, CapabilityStatusMessage } from "./CapabilityStatus";
 import Config from "./Config";
-import { ConfigStatusCallback, ConfigStatusMessage } from "./ConfigStatus";
+import { ConfigFileType, ConfigStatusCallback, ConfigStatusMessage } from "./ConfigStatus";
 import Device from "./Device";
 import DeviceChangeMessage from "./DeviceChangeMessage";
 import DeviceState from "./DeviceState";
@@ -27,6 +27,7 @@ export {
     CapabilityStatusCallback,
     CapabilityStatusMessage,
     Config,
+    ConfigFileType,
     ConfigStatusCallback,
     ConfigStatusMessage,
     Device,
