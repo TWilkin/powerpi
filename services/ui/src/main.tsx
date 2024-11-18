c2dd047a48e26587fb50eb547c888baf2619ed2e
TWilkin <TWilkin@users.noreply.github.com>
2024-11-18 07:57:49 +0000

    #541 chore: Add the i18next linter

--- services/ui/src/main.tsx
+++ services/ui/src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import App from "./App";
-import "./i8n";
+import "./localisation";
 import "./main.css";
 import router from "./routing/router";
 
