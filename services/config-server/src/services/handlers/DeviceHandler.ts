413c10b17f6666375020012abf3831600ed74c49
TWilkin <TWilkin@users.noreply.github.com>
2025-01-31 18:26:59 +0000

    #572 refactor(config-server): Make it clear what the DeviceHandler is operating on

--- services/config-server/src/services/handlers/DeviceHandler.ts
+++ services/config-server/src/services/handlers/DeviceHandler.ts
@@ -10,16 +10,16 @@ export default class DeviceHandler implements IHandler<IDeviceConfigFile> {
     constructor(private readonly publishService: ConfigPublishService) {}
 
     public async handle(config: IDeviceConfigFile) {
-        const sensors = config.sensors?.filter((sensor) => sensor.type === "powerpi");
+        const powerpiSensors = config.sensors?.filter((sensor) => sensor.type === "powerpi");
 
-        if (sensors) {
-            for (const sensor of sensors) {
-                await this.generateConfigMessage(sensor);
+        if (powerpiSensors) {
+            for (const sensor of powerpiSensors) {
+                await this.generatePowerPiSensorConfigMessage(sensor);
             }
         }
     }
 
-    private async generateConfigMessage(sensor: ISensorConfig) {
+    private async generatePowerPiSensorConfigMessage(sensor: ISensorConfig) {
         const props = _(sensor)
             .omit("name", "type", "display_name", "location", "metrics", "visible")
             .value();
