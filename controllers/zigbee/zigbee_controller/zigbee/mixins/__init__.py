c40943d2b44249b96cc717b86e9478eecb151552
TWilkin <TWilkin@users.noreply.github.com>
2025-02-10 15:47:58 +0000

    #585 feat(zigbee): Add ZigbeeRemoteMixin which can be used for all remote/switches

--- controllers/zigbee/zigbee_controller/zigbee/mixins/__init__.py
+++ controllers/zigbee/zigbee_controller/zigbee/mixins/__init__.py
@@ -1,2 +1,3 @@
 from .on_off import ZigbeeOnOffMixin
+from .remote import ZigbeeRemoteMixin
 from .report import ZigbeeReportMixin
---
+++ controllers/zigbee/zigbee_controller/zigbee/mixins/remote.py
@@ -0,0 +1,73 @@
+from abc import ABC, abstractmethod
+from dataclasses import dataclass
+from enum import StrEnum, unique
+from typing import List
+
+from zigpy.zcl.clusters import Cluster
+
+from zigbee_controller.zigbee import ClusterCommandListener
+
+
+@unique
+class Button(StrEnum):
+    BUTTON = 'button'
+
+
+@unique
+class PressType(StrEnum):
+    SHORT = 'short'
+    LONG = 'long'
+    DOUBLE = 'double'
+
+
+@dataclass
+class ButtonConfig:
+    cluster_id: int
+    endpoint_id: int
+    button: Button
+
+
+@dataclass
+class RemoteConfig:
+    buttons: List[ButtonConfig]
+    press_types: List[PressType]
+
+
+class ZigbeeRemoteMixin(ABC):
+    '''
+    Mixin to be used to configure a sensor to be a remote, where button presses are broadcast
+    to the message queue so the event service can handle them.
+    Expected to be used alongside ZigbeeMixin.
+    '''
+
+    async def initialise(self):
+        device = self._zigbee_device
+
+        config = self._remote_config()
+
+        for button in config.buttons:
+            cluster: Cluster = device[button.endpoint_id].out_clusters[button.cluster_id]
+
+            cluster.add_listener(
+                ClusterCommandListener(lambda _, press_type, __: self._button_press_handler(
+                    button.button,
+                    config.press_types[press_type]
+                ))
+            )
+
+    @abstractmethod
+    def _remote_config(self) -> RemoteConfig:
+        '''
+        Override to supply the remote configuration.
+        '''
+        raise NotImplementedError
+
+    def _button_press_handler(self, button: Button, press_type: PressType):
+        self.log_info(f'Received {press_type} press of {button}')
+
+        message = {
+            'button': button,
+            'type': press_type
+        }
+
+        self._broadcast('press', message)
