eff79258b19d3c1899e7c25888adaf2cfa810adc
TWilkin <TWilkin@users.noreply.github.com>
2025-02-18 17:15:13 +0000

    #297 tests(shutdown): Add test for SetBrightness

--- services/shutdown/src/additional/brightness/brightness_test.go
+++ services/shutdown/src/additional/brightness/brightness_test.go
@@ -1,6 +1,7 @@
 package brightness
 
 import (
+	"os"
 	"testing"
 	"testing/fstest"
 
@@ -33,6 +34,38 @@ func TestGetBrightness(t *testing.T) {
 
 			if result != test.expected {
 				t.Errorf("Brightness incorrect, got: %d, expected: %d", result, test.expected)
+			}
+		})
+	}
+}
+
+func TestSetBrightness(t *testing.T) {
+	var tests = []struct {
+		name       string
+		brightness int
+		min        float64
+		max        float64
+		expected   string
+	}{
+		{"0 == 0", 0, 0.0, 100.0, "0\n"},
+		{"50 == 50", 50, 0.0, 100.0, "50\n"},
+		{"100 == 100", 100, 0.0, 100.0, "100\n"},
+		{"50 == 60", 50, 10.0, 110.0, "60\n"},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			fs := fstest.MapFS{}
+			dir := t.TempDir()
+			file := dir + "/brightness"
+
+			config := flags.BrightnessConfig{Device: file, Min: test.min, Max: test.max}
+
+			New(config, fs).SetBrightness(test.brightness)
+			result, _ := os.ReadFile(file)
+
+			if string(result) != test.expected {
+				t.Errorf("Brightness incorrect, got: %s, expected: %s", result, test.expected)
 			}
 		})
 	}
