8eafc393d283bc3c4be20f0d3535452f5f40fd96
TWilkin <TWilkin@users.noreply.github.com>
2025-03-02 11:50:20 +0000

    #606 fix(pipeline): Check if the main dependency should be included

--- .github/scripts/test_python.sh
+++ .github/scripts/test_python.sh
@@ -4,8 +4,12 @@ project=$1
 
 echo Testing $project
 
-# not every project has the powerpi dependency group
-groups=main,test
+# not every project has the main and powerpi dependency group
+groups=test
+if grep -q "dependencies = [" pyproject.toml
+then
+    groups=$groups,main
+fi
 if grep -q "tool.poetry.group.powerpi.dependencies" pyproject.toml
 then
     groups=$groups,powerpi
