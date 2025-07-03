808b864c178b90d71490fc1b445d9ddcf69527c9
TWilkin <TWilkin@users.noreply.github.com>
2025-07-03 08:27:03 +0100

    #551 feat(energy-monitor): Start adding the logic for reading from Octopus

---
+++ services/energy-monitor/services/energyretriever/octopus/consumption.go
@@ -0,0 +1,14 @@
+package octopus
+
+import "time"
+
+type ConsumptionResult struct {
+	Consumption   float64   `json:"consumption"`
+	IntervalStart time.Time `json:"interval_start"`
+	IntervalEnd   time.Time `json:"interval_end"`
+}
+
+type ConsumptionResponse struct {
+	Count   int                 `json:"count"`
+	Results []ConsumptionResult `json:"results"`
+}
--- services/energy-monitor/services/energyretriever/octopus/octopus_energy_retriever.go
+++ services/energy-monitor/services/energyretriever/octopus/octopus_energy_retriever.go
@@ -1,6 +1,10 @@
 package octopus
 
 import (
+	"fmt"
+	"net/url"
+	"path"
+
 	"powerpi/common/services/logger"
 	"powerpi/common/services/mqtt"
 	"powerpi/energy-monitor/models"
@@ -10,6 +14,8 @@ import (
 type OctopusEnergyRetriever struct {
 	*energyRetriever.BaseEnergyRetriever
 }
+
+const baseURL = "https://api.octopus.energy/v1"
 
 func NewOctopusEnergyRetriever(mqttService mqtt.MqttService, logger logger.LoggerService, meter models.MeterSensor) *OctopusEnergyRetriever {
 	return &OctopusEnergyRetriever{
@@ -23,4 +29,54 @@ func NewOctopusEnergyRetriever(mqttService mqtt.MqttService, logger logger.Logge
 
 func (retriever *OctopusEnergyRetriever) Read() {
 	retriever.Logger.Info("Reading energy data from Octopus meter", "meter", retriever.Meter.Name)
+
+	retriever.readConsumption()
+}
+
+func (retriever *OctopusEnergyRetriever) readConsumption() {
+	// identify the type of meter
+	var meterType string
+	_, success := retriever.Meter.Metrics[models.MeterMetricElectricity]
+	if success {
+		meterType = "electricity"
+	} else {
+		meterType = "gas"
+	}
+
+	// the electricity MPAN or gas MPRN
+	meterId := "mpan-or-mprn"
+	serialNumber := "sn"
+
+	url, err := retriever.generateURL(
+		[]string{fmt.Sprintf("%s-meter-points", meterType), meterId, "meters", serialNumber, "consumption"},
+		map[string]string{
+			"page_size":   "100",
+			"period_from": "2025-07-01T00:00:00",
+			"period_to":   "2025-07-02T00:00:00",
+		},
+	)
+	if err != nil {
+		retriever.Logger.Error("Failed to generate URL for consumption data", "error", err)
+		return
+	}
+
+	retriever.Logger.Info("Generated URL for consumption data", "url", url)
+}
+
+// TODO put this in a helper in common
+func (retriever *OctopusEnergyRetriever) generateURL(paths []string, params map[string]string) (string, error) {
+	url, err := url.Parse(baseURL)
+	if err != nil {
+		return "", err
+	}
+
+	url.Path = path.Join(append([]string{url.Path}, paths...)...)
+
+	query := url.Query()
+	for key, value := range params {
+		query.Set(key, value)
+	}
+	url.RawQuery = query.Encode()
+
+	return url.String(), nil
 }
