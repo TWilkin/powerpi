8ea709c795b9cf2cb2a9f3cccb4a93d819c06939
TWilkin <TWilkin@users.noreply.github.com>
2025-07-11 18:44:24 +0100

    #551 feat: Add the conversion from kWh to m3 for gas

--- services/ui/src/services/UnitConverter/converters/gas.ts
+++ services/ui/src/services/UnitConverter/converters/gas.ts
@@ -4,7 +4,7 @@ import { volume } from "./volume";
 
 const calorific = 38; // MJ/m3
 
-export const gas: ConverterDefinition[] = [
+const gas: ConverterDefinition[] = [
     {
         unit: "m3",
         key: "metres cubed",
@@ -17,3 +17,20 @@ export const gas: ConverterDefinition[] = [
     // and the energy converters
     ...energy,
 ];
+
+// add a conversion from kWh to m3, but only for the gas energy converter
+const kWhIndex = energy.findIndex((energy) => energy.unit === "kWh");
+
+if (kWhIndex !== -1) {
+    const kWh = energy[kWhIndex];
+
+    gas[kWhIndex] = {
+        ...kWh,
+        convert: {
+            ...kWh.convert,
+            m3: (value: number) => (value * 3.6) / (calorific * 1.02264),
+        },
+    };
+}
+
+export { gas };
