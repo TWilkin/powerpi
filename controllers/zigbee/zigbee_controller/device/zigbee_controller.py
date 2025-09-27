265621bbbc2a7ac3bfc58471b2839ac255a78dcb
TWilkin <TWilkin@users.noreply.github.com>
2025-09-27 09:19:48 +0100

    #664 feat(zigbee): Get the library start-up to work correctly

--- controllers/zigbee/zigbee_controller/device/zigbee_controller.py
+++ controllers/zigbee/zigbee_controller/device/zigbee_controller.py
@@ -21,6 +21,8 @@ class ZigbeeController(LogMixin):
         self.__controller: ControllerApplication | None = None
 
     def get_device(self, ieee: EUI64, nwk: int) -> DeviceType:
+        self._ensure_controller_running()
+
         return self.__controller.get_device(ieee, nwk)
 
     async def startup(self):
@@ -30,24 +32,43 @@ class ZigbeeController(LogMixin):
                 'path': self.__config.zigbee_device,
                 'baudrate': self.__config.baudrate,
                 'flow_control': self.__config.flow_control,
-            }
+            },
+            'backup_enabled': False
         }
 
-        # pylint: disable=broad-except,protected-access
         try:
             app_type = self.__library_factory().get_library()
-            self.__controller = await app_type.new(config, auto_form=True)
+            controller: ControllerApplication = await app_type.new(config, auto_form=True)
+
+            self.__controller = controller
+
+            self.log_info('ZigBee controller started')
         except Exception as ex:
-            self.log_error('Could not initialise ZigBee device')
+            self.log_error('Could not initialise ZigBee controller')
             self.log_exception(ex)
             os._exit(-1)
 
     async def shutdown(self):
-        self.log_info('Shutting down ZigBee device')
-        await self.__controller.shutdown()
+        self.log_info('Shutting down ZigBee controller')
+        if self.__controller:
+            await self.__controller.shutdown()
+            self.__controller = None
 
     async def pair(self, time: int):
+        self._ensure_controller_running()
+
         await self.__controller.permit(time)
 
     def add_listener(self, listener):
+        self._ensure_controller_running()
+
         self.__controller.add_listener(listener)
+
+    def _ensure_controller_running(self):
+        if not self.__controller:
+            raise ZigbeeControllerNotRunningError()
+
+
+class ZigbeeControllerNotRunningError(RuntimeError):
+    def __init__(self):
+        super().__init__('ZigBee controller is not running')
