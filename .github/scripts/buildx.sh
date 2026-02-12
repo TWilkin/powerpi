971fa9ff9cbd4ed214ac2b8e7020a752f0678d0c
TWilkin <TWilkin@users.noreply.github.com>
2026-02-12 21:17:24 +0000

    #693 feat: Remove old version pull from buildx.sh

--- .github/scripts/buildx.sh
+++ .github/scripts/buildx.sh
@@ -11,17 +11,6 @@ help() {
     exit
 }
 
-get_old_version() {
-    local url=https://raw.githubusercontent.com/TWilkin/powerpi/main/kubernetes/charts/$1/Chart.yaml
-    local path=Chart.yaml
-
-    curl $url --output $path
-
-    appVersion=`yq --raw-output .appVersion $path`
-
-    rm $path
-}
-
 get_version() {
     local path=$scriptPath/../../kubernetes/charts/$1/Chart.yaml
 
@@ -38,36 +27,21 @@ repo=$2
 
 powerpiPath=$scriptPath/../../
 
-get_old_version $service
-oldVersion=$appVersion
-
-get_version $service
-version=$appVersion
-
-name=powerpi-$service
-
 if ! get_service_by_chart "$service"
 then
     echo "Service $service not found in services.yaml"
     exit 1
 fi
 
+get_version $service
+version=$appVersion
+
+name=powerpi-$service
 path=$SERVICE_DIR
 platform=linux/arm64
 
-# retrieve the previous image
-image=twilkin/$name:$oldVersion
-echo "Pulling previous image $image"
-docker pull $image
-echo
-
-# attempt to retrieve the new image, in case it's an update
-image=$repo/$name:$version
-echo "Pulling current image $image"
-docker pull $image
-echo
-
 # build the image
+image=$repo/$name:$version
 echo "Building image $name:$version"
 docker buildx build --load --platform $platform -t $image -t twilkin/$name:$version -f $powerpiPath/$path/Dockerfile $powerpiPath
 echo
