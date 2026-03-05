aa64f9ed68626afba1bd1cecf363d533350f78fa
TWilkin <TWilkin@users.noreply.github.com>
2026-03-05 20:20:38 +0000

    #710 docs(zigbee): Document the hold/release for Stybar

--- controllers/zigbee/zigbee_controller/sensor/ikea/stybar.py
+++ controllers/zigbee/zigbee_controller/sensor/ikea/stybar.py
@@ -39,12 +39,13 @@ class IKEAStyrbarSensor(Sensor, ZigbeeMixin, ZigbeeSleepyMixin, ZigbeeRemoteMixi
     /event/NAME/press:{"button": "left", "type": "single"}
 
     Long press generates an event pair, one when the button is pressed and
-    the other when it's released:
+    the other when it's released with the latter including the duration of
+    the press:
     /event/NAME/press:{"button": "up", "type": "hold"}
-    /event/NAME/press:{"button": "up", "type": "release"}
+    /event/NAME/press:{"button": "up", "type": "release", "value": 500, "unit": "ms"}
 
     /event/NAME/press:{"button": "down", "type": "hold"}
-    /event/NAME/press:{"button": "down", "type": "release"}
+    /event/NAME/press:{"button": "down", "type": "release", "value": 500, "unit": "ms"}
     '''
 
     BUTTON_MAP = {
