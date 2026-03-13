c442ae5445b8523190168ec54277b0391bb90721
TWilkin <TWilkin@users.noreply.github.com>
2026-03-13 08:27:24 +0000

    #720 chore: Remove old config-server

--- services/config-server.old/src/schema/devices/harmony/index.ts
+++
@@ -1,9 +0,0 @@
-import * as HarmonyActivity from "./HarmonyActivity.schema.json" with { type: "json" };
-import * as HarmonyHub from "./HarmonyHub.schema.json" with { type: "json" };
-
-export default function loadHarmonySchema() {
-    return {
-        HarmonyActivity,
-        HarmonyHub,
-    };
-}
--- services/config-server.old/src/schema/devices/lifx/index.ts
+++
@@ -1,6 +0,0 @@
-import * as LIFXLight from "./LIFXLight.schema.json" with { type: "json" };
-export default function loadLIFXSchema() {
-    return {
-        LIFXLight,
-    };
-}
