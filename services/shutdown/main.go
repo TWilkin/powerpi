3f08555ff1ee18d65a79fb943f2a5d6ebdda946f
TWilkin <TWilkin@users.noreply.github.com>
2026-03-13 19:58:18 +0000

    #720 refactor(shutdown): Should close the channel and unsubscribe

--- services/shutdown/main.go
+++ services/shutdown/main.go
@@ -53,7 +53,10 @@ func main() {
 
 	// subscribe to the change event
 	channel := make(chan *messagequeue.DeviceMessage, 1)
+	defer close(channel)
+
 	deviceService.SubscribeChange(hostname, channel)
+	defer deviceService.UnsubscribeChange(hostname)
 
 	// loop waiting for messages
 	clockService := services.GetService[clock.ClockService](container)
