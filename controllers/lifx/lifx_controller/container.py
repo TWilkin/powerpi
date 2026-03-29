2f3b09cbf7a82451f3f7e3b361ba2dd371df33d4
TWilkin <TWilkin@users.noreply.github.com>
2026-03-29 12:36:35 +0100

    #720 feat(lifx-controller): Remove config-retriever

--- controllers/lifx/lifx_controller/container.py
+++ controllers/lifx/lifx_controller/container.py
@@ -1,5 +1,4 @@
 from dependency_injector import containers, providers
-from powerpi_common.config import ControllerConfig
 from powerpi_common.container import Container as CommonContainer
 
 from lifx_controller.__version__ import __app_name__, __version__
@@ -14,15 +13,10 @@ class ApplicationContainer(containers.DeclarativeContainer):
         __self__
     )
 
-    config = providers.Singleton(
-        ControllerConfig
-    )
-
     common = providers.Container(
         CommonContainer,
         app_name=__app_name__,
-        version=__version__,
-        config=config
+        version=__version__
     )
 
     device = providers.Container(
