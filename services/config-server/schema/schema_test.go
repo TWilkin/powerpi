4dbdf63e00fc416ef48729c72c44c0e37f55a440
TWilkin <TWilkin@users.noreply.github.com>
2026-04-07 18:22:11 +0100

    tests: Add some reusable functions

--- services/config-server/schema/schema_test.go
+++ services/config-server/schema/schema_test.go
@@ -44,24 +44,38 @@ func Test(t *testing.T) {
 			}
 
 			// patch the test into the wrapper
-			patch, err := jsonpatch.DecodePatch([]byte(
-				fmt.Sprintf(`[{"op": "add", "path": "%s", "value": %s}]`, suite.path, buffer.String()),
-			))
-			if err != nil {
-				t.Fatalf("%s", err.Error())
-			}
-			result, err := patch.Apply(base)
-			if err != nil {
-				t.Fatalf("%s", err.Error())
-			}
+			result := patch(t, base, suite.path, "add", buffer.String())
 
 			// first we run the positive test
-			t.Run("positive", func(t *testing.T) {
-				_, err := service.Validate(suite.configType, string(result))
-				if err != nil {
-					t.Errorf("Failed validation %s", err.Error())
-				}
+			t.Run("complete", func(t *testing.T) {
+				validate(t, service, suite.configType, result, true)
 			})
 		})
 	}
+}
+
+func validate(t *testing.T, service validator.ValidatorService, configType models.ConfigType, config []byte, expected bool) {
+	_, err := service.Validate(configType, string(config))
+	if expected && err != nil {
+		t.Errorf("Failed validation %s", err.Error())
+	}
+	if !expected && err == nil {
+		t.Errorf("Unexpectedly passed validation")
+	}
+}
+
+func patch(t *testing.T, base []byte, path string, operand string, data string) []byte {
+	patch, err := jsonpatch.DecodePatch(
+		fmt.Appendf(nil, `[{"op": "%s", "path": "%s", "value": %s}]`, operand, path, data),
+	)
+	if err != nil {
+		t.Fatalf("%s", err.Error())
+	}
+
+	result, err := patch.Apply(base)
+	if err != nil {
+		t.Fatalf("%s", err.Error())
+	}
+
+	return result
 }
