a4a2da138eb287348e9bf3a44bf342aca3b97886
TWilkin <TWilkin@users.noreply.github.com>
2026-04-12 18:22:02 +0100

    #757 tests(common): Add test for the presence variable

---
+++ common/python/tests/variable/test_presence.py
@@ -0,0 +1,27 @@
+import pytest
+from powerpi_common_test.variable.variable import VariableTestBase
+
+from powerpi_common.variable.presence import PresenceVariable
+
+
+class TestPresenceVariable(VariableTestBase):
+
+    def test_topic(self, subject: PresenceVariable):
+        assert subject.topic == 'presence/TestPresence/status'
+
+    @pytest.mark.asyncio
+    async def test_on_message_updates(self, subject: PresenceVariable):
+        message = {'state': 'absent'}
+
+        assert subject.state is None
+
+        await subject.on_message(message, 'TestPresence', 'status')
+
+        assert subject.state == 'absent'
+
+    @pytest.fixture
+    def subject(self, powerpi_config, powerpi_logger, powerpi_mqtt_client):
+        return PresenceVariable(
+            powerpi_config, powerpi_logger, powerpi_mqtt_client,
+            name='TestPresence'
+        )
