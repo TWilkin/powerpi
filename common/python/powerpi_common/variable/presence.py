55ed18d1461c0b4598ba3fc389f69e77ff75b2d7
TWilkin <TWilkin@users.noreply.github.com>
2026-04-12 18:04:54 +0100

    #757 feat(common): Add the variable implementation

--- common/python/powerpi_common/variable/presence.py
+++ common/python/powerpi_common/variable/presence.py
@@ -1,16 +1,36 @@
+from powerpi_common.config import Config
+from powerpi_common.logger import Logger
+from powerpi_common.mqtt import MQTTClient, MQTTTopic
+from powerpi_common.sensor.consumers import SensorEventConsumer
 from powerpi_common.variable.types import VariableType
 from powerpi_common.variable.variable import Variable
 
 
-class PresenceVariable(Variable):
+class PresenceVariable(Variable, SensorEventConsumer):
+    '''
+    Variable implementation that will receive presence detection events from the message queue.
+    '''
+
     def __init__(
         self,
+        config: Config,
+        logger: Logger,
+        mqtt_client: MQTTClient,
         name: str,
         **kwargs
     ):
         Variable.__init__(self, name, **kwargs)
+        SensorEventConsumer.__init__(
+            self,
+            f'{MQTTTopic.PRESENCE}/{name}/status',
+            self,
+            config,
+            logger
+        )
 
         self.__state = None
+
+        mqtt_client.add_consumer(self)
 
     @property
     def variable_type(self):
