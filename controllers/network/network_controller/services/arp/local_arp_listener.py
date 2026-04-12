f0770878d094719bb0ce8ff8debc44c387d6e2f7
TWilkin <TWilkin@users.noreply.github.com>
2026-04-12 09:16:52 +0100

    #183 feat(network): Expire devices when they haven't checked in for a while

--- controllers/network/network_controller/services/arp/local_arp_listener.py
+++ controllers/network/network_controller/services/arp/local_arp_listener.py
@@ -1,13 +1,20 @@
 from asyncio import ensure_future, sleep, Future
+from dataclasses import dataclass
 from dpkt import ethernet
-from netifaces import gateways, interfaces, ifaddresses, AF_INET
+from netifaces import interfaces, ifaddresses, AF_INET
 from pcap import pcap
 from socket import inet_ntoa
+from time import time
 from threading import RLock
 
 from powerpi_common.logger import Logger
 
 from .arp import ARPReader, HostAddress
+
+
+@dataclass
+class ARPEntry(HostAddress):
+    timestamp: int = 0
 
 
 class LocalARPListener(ARPReader):
@@ -25,7 +32,7 @@ class LocalARPListener(ARPReader):
         self.__running = False
         self.__future: Future | None
 
-        self.__table: list[HostAddress] = []
+        self.__table: list[ARPEntry] = []
         self.__lock = RLock()
 
     async def start(self):
@@ -53,14 +60,24 @@ class LocalARPListener(ARPReader):
             sniffer.setfilter('arp')
             sniffer.setnonblock()
 
+            counter = 0
+
             while self.__running:
+                counter += 1
+
                 sniffer.dispatch(-1, self.__handle_packet)
+
+                # every 120s we prune anything too old
+                if counter >= 120:
+                    counter = 0
+
+                    self.__prune()
 
                 await sleep(1)
         except Exception as ex:
             self.log_error(ex)
 
-    def __handle_packet(self, _, raw):
+    def __handle_packet(self, timestamp: int, raw):
         eth = ethernet.Ethernet(raw)
         arp = eth.data
         src_ip = inet_ntoa(arp.spa)
@@ -72,7 +89,12 @@ class LocalARPListener(ARPReader):
         with self.__lock:
             entry = self.find(host_address)
             if entry is None:
-                self.__table.append(host_address)
+                self.__table.append(ARPEntry(
+                    host_address.mac_address,
+                    host_address.ip_address,
+                    host_address.hostname,
+                    timestamp
+                ))
             else:
                 entry.mac_address = src_mac
                 entry.ip_address = src_ip
@@ -86,3 +108,22 @@ class LocalARPListener(ARPReader):
                 return iface
 
         raise RuntimeError('No suitable network interface found')
+
+    def __prune(self):
+        now = int(time())
+        expiry = now - 120
+
+        with self.__lock:
+            count = len(self.__table)
+
+            self.__table = [
+                entry for entry in self.__table
+                if entry.timestamp > expiry
+            ]
+
+            new_count = len(self.__table)
+            if new_count != count:
+                self.log_debug(
+                    'Removed %d expired ARP records',
+                    count - new_count
+                )
