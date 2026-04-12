d3c39d60bf2883e9770b600caef81683d89e713b
TWilkin <TWilkin@users.noreply.github.com>
2026-04-12 09:57:44 +0100

    #183 feat(network): Add the grace period to the presence sensor

--- controllers/network/network_controller/sensor/presence.py
+++ controllers/network/network_controller/sensor/presence.py
@@ -1,6 +1,8 @@
 from dataclasses import dataclass
 from enum import StrEnum, unique
+from time import time
 
+from icmplib import async_ping
 from powerpi_common.config import Config
 from powerpi_common.logger import Logger
 from powerpi_common.mqtt import MQTTClient
@@ -15,6 +17,12 @@ class PresenceStatus(StrEnum):
     PRESENT = 'present'
     ABSENT = 'absent'
     UNKNOWN = 'unknown'
+
+
+@dataclass
+class GracePeriod:
+    delay: int
+    timer: int = 0
 
 
 class PresenceSensor(Sensor, PollableMixin):
@@ -32,8 +40,10 @@ class PresenceSensor(Sensor, PollableMixin):
         mac: str | None = None,
         ip: str | None = None,
         hostname: str | None = None,
+        absent_delay: int = 3 * 60,
         **kwargs
     ):
+        # pylint: disable=too-many-arguments,too-many-positional-arguments
         Sensor.__init__(self, mqtt_client, **kwargs)
         PollableMixin.__init__(self, config, **kwargs)
 
@@ -45,6 +55,8 @@ class PresenceSensor(Sensor, PollableMixin):
 
         # we cache the values we get from the service, and use them as fallbacks
         self.__cache = HostAddress()
+
+        self.__grace_period = GracePeriod(absent_delay)
 
         self.__state = PresenceStatus.UNKNOWN
 
@@ -63,12 +75,41 @@ class PresenceSensor(Sensor, PollableMixin):
     async def poll(self):
         entry = self.__arp_service.find(self)
         if entry is None:
-            self.__set_new_state(PresenceStatus.ABSENT)
+            # apply the grace period before marking the device as absent
+            await self.__check_grace_period()
         else:
-            self.__cache = entry
-            self.__set_new_state(PresenceStatus.PRESENT)
+            self.log_debug('Device present in ARP cache')
 
-    def _broadcast(self, state: PresenceStatus):
+            # update the cache of MAC, IP and hostname
+            self.__cache = entry
+
+            self.__set_present()
+
+    async def __check_grace_period(self):
+        # first has the period already expired?
+        now = int(time())
+        expiry = now - self.__grace_period.delay
+
+        if self.__grace_period.timer != 0 and self.__grace_period.timer < expiry:
+            # it has expired
+            self.log_debug('Device absent after grace period')
+
+            self.__set_new_state(PresenceStatus.ABSENT)
+
+            return
+
+        # start the timer
+        self.__grace_period.timer = now
+
+        # try to ping
+        is_alive = await self.__is_alive()
+        if is_alive:
+            # it is actually present
+            self.log_debug('Device present after ping')
+
+            self.__set_present()
+
+    def __broadcast(self, state: PresenceStatus):
         topic = f'presence/{self.entity}/status'
 
         message = {'state': state}
@@ -79,7 +120,14 @@ class PresenceSensor(Sensor, PollableMixin):
         if self.__state != state:
             self.__state = state
 
-            self._broadcast(state)
+            self.__broadcast(state)
+
+    def __set_present(self):
+        # reset the grace period timer as the device is present
+        # if it disappears again we want to allow the grace period again
+        self.__grace_period.timer = 0
+
+        self.__set_new_state(PresenceStatus.PRESENT)
 
     @property
     def __arp_service(self):
@@ -94,3 +142,16 @@ class PresenceSensor(Sensor, PollableMixin):
             return value
 
         return getattr(self.__cache, prop)
+
+    async def __is_alive(self):
+        network_address = self.ip_address if self.ip_address else self.hostname
+
+        result = await async_ping(
+            network_address,
+            count=1,
+            interval=0.2,
+            timeout=2,
+            privileged=False
+        )
+
+        return result.is_alive
