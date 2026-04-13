9c47903442bdd107bdcf731ff784077fc3ababac
TWilkin <TWilkin@users.noreply.github.com>
2026-04-13 13:17:41 +0100

    #114 feat(virtual): Add the logic for when a message is received

--- controllers/virtual/virtual_controller/sensor/geofence.py
+++ controllers/virtual/virtual_controller/sensor/geofence.py
@@ -1,3 +1,5 @@
+from powerpi_common.condition import ConditionParser, Expression
+from powerpi_common.device import DeviceStatus
 from powerpi_common.logger import Logger
 from powerpi_common.mqtt import MQTTClient
 from powerpi_common.sensor import Sensor
@@ -7,6 +9,16 @@ from powerpi_common.variable import VariableManager
 class GeofenceSensor(Sensor):
     '''
     Sensor implementing the conditions to activate, or deactivate a Geofence.
+    A Geofence is a conditional sensor which outputs "on" or "off" depending
+    on whether the condition is true or false. It is expected that the condition
+    include presence sensors; but is otherwise user configurable.
+    e.g.:
+    {
+        'either': [
+            {'var': 'presence.Person1.state', 'present'},
+            {'var': 'presence.Person2.state', 'present'}
+        ]
+    }
     '''
 
     def __init__(
@@ -14,6 +26,7 @@ class GeofenceSensor(Sensor):
         logger: Logger,
         mqtt_client: MQTTClient,
         variable_manager: VariableManager,
+        condition: Expression,
         **kwargs
     ):
         Sensor.__init__(self, mqtt_client, **kwargs)
@@ -21,3 +34,32 @@ class GeofenceSensor(Sensor):
         self._logger = logger
 
         self.__variable_manager = variable_manager
+        self.__condition = condition
+
+        self.__state = DeviceStatus.UNKNOWN
+
+    @property
+    def state(self):
+        return self.__state
+
+    def on_message(self):
+        # first we check the condition to identify what state we are in
+        condition = self.__check_condition()
+        new_state = DeviceStatus.ON if condition else DeviceStatus.OFF
+
+        # if the state has changed, then we need to broadcast that
+        if new_state != self.__state:
+            self.__state = new_state
+
+            self.__broadcast(new_state)
+
+    def __check_condition(self):
+        parser = ConditionParser(self.__variable_manager, {})
+        return parser.conditional_expression(self.__condition)
+
+    def __broadcast(self, state: DeviceStatus):
+        topic = f'geofence/{self.entity}/status'
+
+        message = {'state': state}
+
+        self._producer(topic, message)
